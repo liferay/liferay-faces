70fdd7a4c39a04c4b98f5e31dcb24036ea9b670f
Neil Griffin <neil.griffin.scm@gmail.com>
2012-05-11 17:14:29 -0400

    FACES-1218 Failure in TCK TestPage084: encodeActionURLPoundCharTest

--- bridge-impl/src/main/java/com/liferay/faces/bridge/context/BridgeContextImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/BridgeContextImpl.java
@@ -256,7 +256,8 @@ public class BridgeContextImpl implements BridgeContext {
 				bridgeActionURL.removeParameter(Bridge.DIRECT_LINK);
 			}
 
-			if (!targetFacesView.isExtensionMapped() && !targetFacesView.isPathMapped()) {
+			if (!targetFacesView.isExtensionMapped() && !targetFacesView.isPathMapped() &&
+					!url.startsWith(BridgeConstants.CHAR_POUND)) {
 				bridgeActionURL.setParameter(Bridge.NONFACES_TARGET_PATH_PARAMETER, contextRelativeViewPath);
 			}
 
