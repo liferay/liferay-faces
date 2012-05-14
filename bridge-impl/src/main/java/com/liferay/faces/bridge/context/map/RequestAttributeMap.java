131eb5e30b4da77014fe09320690b3cd7500b543
Neil Griffin <neil.griffin.scm@gmail.com>
2012-05-14 15:11:47 -0400

    FACES-1222 Failure in TCK TestPage150: requestMapCoreTest

--- bridge-impl/src/main/java/com/liferay/faces/bridge/context/map/RequestAttributeMap.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/map/RequestAttributeMap.java
@@ -28,10 +28,6 @@ import com.liferay.faces.bridge.util.ManagedBeanUtil;
  */
 public class RequestAttributeMap extends AbstractPropertyMap<Object> {
 
-	// Private Constants
-	private static final String PATH_INFO_ATTRIBUTE = "javax.servlet.include.path_info";
-	private static final String SERVLET_PATH_ATTRIBUTE = "javax.servlet.include.servlet_path";
-
 	// Private Data Members
 	private PortletRequest portletRequest;
 	private boolean preferPreDestroy;
@@ -64,28 +60,9 @@ public class RequestAttributeMap extends AbstractPropertyMap<Object> {
 		portletRequest.removeAttribute(name);
 	}
 
-	/**
-	 * This method returns the value of the specified attribute name according to the current request. Liferay Portal's
-	 * portlet container uses a servlet dispatcher when executing the portlet lifecycle. This approach requires the
-	 * portal to save some standard Servlet-API request attributes like javax.servlet.include.path_info and
-	 * javax.servlet.include.servlet_path. Unfortunately, some JSF implementations expect that these attributes will
-	 * only exist in a JSF servlet-based (not portlet-based) webapp. Therefore this method will return null if one of
-	 * those request attributes is specified.
-	 *
-	 * @see  https://issues.apache.org/jira/browse/PORTLETBRIDGE-77
-	 */
 	@Override
 	protected Object getProperty(String name) {
-		Object value = null;
-
-		if (name != null) {
-
-			if (!(name.equals(PATH_INFO_ATTRIBUTE) || name.equals(SERVLET_PATH_ATTRIBUTE))) {
-				value = portletRequest.getAttribute(name);
-			}
-		}
-
-		return value;
+		return portletRequest.getAttribute(name);
 	}
 
 	@Override
