927553634a6faa4ea9405d55c12afd8d232a4660
Neil Griffin <neil.griffin.scm@gmail.com>
2012-06-30 12:11:10 -0400

    FACES-1317 Trinidad dependency in TCK preventing TestPage073 (scopeAfterRedisplayResourcePPRTest) from executing Ajax request

--- test/bridge-tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/application/view/ViewDeclarationLanguageJspTCKImpl.java
+++ test/bridge-tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/application/view/ViewDeclarationLanguageJspTCKImpl.java
@@ -14,8 +14,14 @@
 package com.liferay.faces.bridge.tck.application.view;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIOutput;
 import javax.faces.component.UIViewRoot;
+import javax.faces.component.html.HtmlHead;
+import javax.faces.component.html.HtmlInputHidden;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewDeclarationLanguage;
 import javax.portlet.faces.Bridge;
@@ -40,6 +46,60 @@ public class ViewDeclarationLanguageJspTCKImpl extends ViewDeclarationLanguageWr
 		this.wrappedViewDeclarationLanguage = viewDeclarationLanguage;
 	}
 
+	@Override
+	public void buildView(FacesContext facesContext, UIViewRoot uiViewRoot)
+			throws IOException {
+		super.buildView(facesContext, uiViewRoot);
+		
+		UIComponent coreForm = findCoreFormRecurse(uiViewRoot);
+
+		if (coreForm != null) {
+
+			// Since the JSF-1.2-based TCK does not have h:head tags in any of the JSPs, need to add one dynamically so
+			// that the jsf.js script resource can be rendered. This is necessary so that Trinidad Partial Page
+			// Rendering (PPR) will work properly.
+			HtmlHead htmlHead = new HtmlHead();
+			uiViewRoot.getChildren().add(htmlHead);
+
+			// Add the "jsf.js" script resource to the h:head component.
+			UIOutput uiOutput = new UIOutput();
+			uiOutput.setRendererType("javax.faces.resource.Script");
+			uiOutput.getAttributes().put("name", "jsf.js");
+			uiOutput.getAttributes().put("library", "javax.faces");
+			uiViewRoot.addComponentResource(facesContext, uiOutput, "head");
+
+			// Due to a bug in the Trinidad tr:form renderer, need to add the javax.faces.encodedURL hidden field
+			// dynamically. See: https://issues.apache.org/jira/browse/TRINIDAD-2284
+			String viewId = uiViewRoot.getViewId();
+			String actionURL = facesContext.getApplication().getViewHandler().getActionURL(facesContext, viewId);
+			ExternalContext externalContext = facesContext.getExternalContext();
+			String encodedPartialActionURL = externalContext.encodePartialActionURL(actionURL);
+			EncodedURLHiddenField encodedURLHiddenField = new EncodedURLHiddenField();
+			encodedURLHiddenField.setValue(encodedPartialActionURL);
+			coreForm.getChildren().add(encodedURLHiddenField);
+		}		
+	}
+
+	protected UIComponent findCoreFormRecurse(UIComponent uiComponent) {
+		UIComponent coreForm = null;
+
+		if (uiComponent.getClass().toString().endsWith("CoreForm")) {
+			coreForm = uiComponent;
+		}
+		else {
+			List<UIComponent> children = uiComponent.getChildren();
+
+			for (UIComponent child : children) {
+				coreForm = findCoreFormRecurse(child);
+
+				if (coreForm != null) {
+					break;
+				}
+			}
+		}
+
+		return coreForm;
+	}
 	@Override
 	public void renderView(FacesContext facesContext, UIViewRoot uiViewRoot) throws IOException {
 
@@ -67,4 +127,14 @@ public class ViewDeclarationLanguageJspTCKImpl extends ViewDeclarationLanguageWr
 		return wrappedViewDeclarationLanguage;
 	}
 
+	protected class EncodedURLHiddenField extends HtmlInputHidden {
+
+		@Override
+		public String getClientId(FacesContext facesContext) {
+			return "javax.faces.encodedURL";
+		}
+
+	}
+
 }
+
