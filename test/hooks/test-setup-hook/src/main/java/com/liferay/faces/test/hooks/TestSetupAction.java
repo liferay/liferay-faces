a57af9b6dda259522363e72f757c9e6c7438bc7e
Neil Griffin <neil.griffin.scm@gmail.com>
2012-06-30 12:41:47 -0400

    FACES-1319 Develop test/bridge-tck/bridge-tck-main-portlet project (auto setup of tests on portal pages)

--- test/hooks/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestSetupAction.java
+++ test/hooks/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestSetupAction.java
@@ -13,27 +13,39 @@
  */
 package com.liferay.faces.test.hooks;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.liferay.portal.NoSuchGroupException;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.xml.Attribute;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.DocumentException;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.model.LayoutTypePortlet;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
 
 
 /**
@@ -43,33 +55,38 @@ public class TestSetupAction extends SimpleAction {
 
 	private static final Log logger = LogFactory.getLog(TestSetupAction.class);
 
-	private static final List<PortalPage> PORTAL_PAGES;
+	private static final List<PortalPage> BRIDGE_DEMO_PAGES;
+	private static final List<PortalPage> PORTAL_DEMO_PAGES;
 
 	static {
-		PORTAL_PAGES = new ArrayList<PortalPage>();
-		PORTAL_PAGES.add(new PortalPage("JSF2", "1_WAR_jsf2portlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("JSF2-CDI", "1_WAR_jsf2cdiportlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("JSF2-JSP", "1_WAR_jsf2jspportlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("JSF2-PDF", "1_WAR_jsf2exportpdfportlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("JSF2-EVENTS",
+		BRIDGE_DEMO_PAGES = new ArrayList<PortalPage>();
+		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF2", "1_WAR_jsf2portlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF2-CDI", "1_WAR_jsf2cdiportlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF2-JSP", "1_WAR_jsf2jspportlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF2-PDF", "1_WAR_jsf2exportpdfportlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF2-EVENTS",
 				new String[] {
 					"customers_WAR_jsf2ipceventscustomersportlet", "bookings_WAR_jsf2ipceventsbookingsportlet"
 				}));
-		PORTAL_PAGES.add(new PortalPage("JSF2-PRP",
+		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF2-PRP",
 				new String[] {
 					"customersPortlet_WAR_jsf2ipcpubrenderparamsportlet",
 					"bookingsPortlet_WAR_jsf2ipcpubrenderparamsportlet"
 				}));
-		PORTAL_PAGES.add(new PortalPage("ICE3", "1_WAR_icefaces3portlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("ICE3-COMPAT", "1_WAR_icefaces3compatportlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("ICE3-CHAT", "1_WAR_icefaces3chatportlet"));
-		PORTAL_PAGES.add(new PortalPage("ICE3-CRUD", "1_WAR_icefaces3crudportlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("ICE3-DIR", "1_WAR_icefaces3directoryportlet"));
-		PORTAL_PAGES.add(new PortalPage("ICE3-DOC", "1_WAR_icefaces3documentsportlet"));
-		PORTAL_PAGES.add(new PortalPage("ICE3-IPC",
+		BRIDGE_DEMO_PAGES.add(new PortalPage("ICE3", "1_WAR_icefaces3portlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("ICE3-COMPAT", "1_WAR_icefaces3compatportlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("ICE3-CRUD", "1_WAR_icefaces3crudportlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("ICE3-IPC",
 				new String[] { "1_WAR_icefaces3ipcajaxpushportlet", "2_WAR_icefaces3ipcajaxpushportlet" }));
-		PORTAL_PAGES.add(new PortalPage("PRIME3", "1_WAR_primefaces3portlet_INSTANCE_"));
-		PORTAL_PAGES.add(new PortalPage("RICH4", "1_WAR_richfaces4portlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("PRIME3", "1_WAR_primefaces3portlet_INSTANCE_"));
+		BRIDGE_DEMO_PAGES.add(new PortalPage("RICH4", "1_WAR_richfaces4portlet_INSTANCE_"));
+	}
+
+	static {
+		PORTAL_DEMO_PAGES = new ArrayList<PortalPage>();
+		PORTAL_DEMO_PAGES.add(new PortalPage("ICE3-CHAT", "1_WAR_icefaces3chatportlet"));
+		PORTAL_DEMO_PAGES.add(new PortalPage("ICE3-DIR", "1_WAR_icefaces3directoryportlet"));
+		PORTAL_DEMO_PAGES.add(new PortalPage("ICE3-DOC", "1_WAR_icefaces3documentsportlet"));
 	}
 
 	@Override
@@ -77,8 +94,12 @@ public class TestSetupAction extends SimpleAction {
 
 		try {
 
-			for (String companyId : companyIds) {
-				setupCompanyPages(Long.parseLong(companyId));
+			for (String companyIdAsString : companyIds) {
+
+				long companyId = Long.parseLong(companyIdAsString);
+				Company company = CompanyLocalServiceUtil.getCompanyById(companyId);
+				long userId = company.getDefaultUser().getUserId();
+				setupSites(companyId, userId);
 			}
 		}
 		catch (Exception e) {
@@ -86,20 +107,64 @@ public class TestSetupAction extends SimpleAction {
 		}
 	}
 
-	protected void setupCompanyPages(long companyId) throws PortalException, SystemException {
+	protected void addAllUsersToSite(long companyId, long groupId) throws SystemException, PortalException {
 
-		Company company = CompanyLocalServiceUtil.getCompanyById(companyId);
+		List<User> users = UserLocalServiceUtil.getCompanyUsers(companyId, QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		ArrayList<Long> userIdList = new ArrayList<Long>();
 
-		for (PortalPage portalPage : PORTAL_PAGES) {
-			String defaultSiteName = PropsUtil.get(PropsKeys.VIRTUAL_HOSTS_DEFAULT_COMMUNITY_NAME);
-			long groupId = GroupLocalServiceUtil.getGroup(companyId, defaultSiteName).getGroupId();
-			setupPage(company.getDefaultUser().getUserId(), groupId, portalPage);
+		for (User user : users) {
+
+			if (!user.isDefaultUser()) {
+				userIdList.add(user.getUserId());
+			}
+		}
+
+		long[] userIds = new long[userIdList.size()];
+
+		for (int i = 0; i < userIds.length; i++) {
+			userIds[i] = userIdList.get(i);
+		}
+
+		UserLocalServiceUtil.addGroupUsers(groupId, userIds);
+	}
+
+	protected void setupBridgeDemosSite(long companyId, long userId) throws PortalException, SystemException {
+		Group site = getSite(companyId, userId, "Bridge Demos");
+		long groupId = site.getGroupId();
+		addAllUsersToSite(companyId, groupId);
+
+		for (PortalPage portalPage : BRIDGE_DEMO_PAGES) {
+			setupPage(userId, groupId, portalPage);
+		}
+	}
+
+	protected void setupBridgeTCKSite(long companyId, long userId) throws PortalException, SystemException, DocumentException {
+		Group site = getSite(companyId, userId, "Bridge TCK");
+		long groupId = site.getGroupId();
+		addAllUsersToSite(companyId, groupId);
+
+		URL configFileURL = getClass().getClassLoader().getResource("pluto-portal-driver-config.xml");
+		Document document = SAXReaderUtil.read(configFileURL);
+		Element rootElement = document.getRootElement();
+		Element renderConfigElement = rootElement.element("render-config");
+		Iterator<Element> pageElementIterator = renderConfigElement.elementIterator("page");
+		while (pageElementIterator.hasNext()) {
+			Element pageElement = pageElementIterator.next();
+			Attribute nameAttribute = pageElement.attribute("name");
+			String pageName = nameAttribute.getValue();
+			Element portletElement = pageElement.element("portlet");
+			nameAttribute = portletElement.attribute("name");
+			String portletName = nameAttribute.getValue();
+			String liferayPortletName = portletName.replaceAll(StringPool.DASH, StringPool.BLANK);
+			String liferayPortletId = liferayPortletName + "_WAR_bridgetckmainportlet";
+			PortalPage portalPage = new PortalPage(pageName, liferayPortletId);
+			setupPage(userId, groupId, portalPage);
 		}
 	}
 
 	protected void setupPage(long userId, long groupId, PortalPage portalPage) throws PortalException, SystemException {
 		String portalPageName = portalPage.getName();
-		String[] portletNames = portalPage.getPortletNames();
+		String[] portletIds = portalPage.getPortletIds();
 		Layout portalPageLayout = getPortalPageLayout(userId, groupId, portalPageName);
 		LayoutTypePortlet layoutTypePortlet = (LayoutTypePortlet) portalPageLayout.getLayoutType();
 
@@ -107,10 +172,9 @@ public class TestSetupAction extends SimpleAction {
 
 		int columnNumber = 1;
 
-		for (String portletName : portletNames) {
-			String portletId = portletName;
+		for (String portletId : portletIds) {
 
-			if (portletName.endsWith("_INSTANCE_")) {
+			if (portletId.endsWith("_INSTANCE_")) {
 				portletId = portletId + "ABCD";
 			}
 
@@ -120,7 +184,23 @@ public class TestSetupAction extends SimpleAction {
 
 		LayoutLocalServiceUtil.updateLayout(portalPageLayout);
 
-		logger.info("Updated page: " + portalPageName);
+		logger.info("Setup page: " + portalPageName);
+	}
+
+	protected void setupPortalDemosSite(long companyId, long userId) throws PortalException, SystemException {
+		Group site = getSite(companyId, userId, "Portal Demos");
+		long groupId = site.getGroupId();
+		addAllUsersToSite(companyId, groupId);
+
+		for (PortalPage portalPage : PORTAL_DEMO_PAGES) {
+			setupPage(userId, groupId, portalPage);
+		}
+	}
+
+	protected void setupSites(long companyId, long userId) throws PortalException, SystemException, DocumentException {
+		setupBridgeDemosSite(companyId, userId);
+		setupPortalDemosSite(companyId, userId);
+		setupBridgeTCKSite(companyId, userId);
 	}
 
 	protected Layout getPortalPageLayout(long userId, long groupId, String portalPageName) throws PortalException,
@@ -145,10 +225,30 @@ public class TestSetupAction extends SimpleAction {
 			serviceContext.setScopeGroupId(groupId);
 			portalPageLayout = LayoutLocalServiceUtil.addLayout(userId, groupId, privateLayout, parentLayoutId,
 					portalPageName, portalPageName, portalPageName, type, hidden, friendlyURL, serviceContext);
-			logger.info("Added page: " + portalPageName);
 		}
 
 		return portalPageLayout;
+	}
+
+	protected Group getSite(long companyId, long userId, String name) throws PortalException, SystemException {
+
+		Group site = null;
+
+		try {
+			site = GroupLocalServiceUtil.getGroup(companyId, name);
+		}
+		catch (NoSuchGroupException e) {
+			String description = name;
+			int type = GroupConstants.TYPE_SITE_OPEN;
+			String friendlyURL = StringPool.FORWARD_SLASH +
+				name.toLowerCase().replaceAll(StringPool.SPACE, StringPool.DASH);
+			boolean siteFlag = true;
+			boolean active = true;
+			site = GroupLocalServiceUtil.addGroup(userId, (String) null, 0L, name, description, type, friendlyURL,
+					siteFlag, active, new ServiceContext());
+		}
+
+		return site;
 	}
 
 }
