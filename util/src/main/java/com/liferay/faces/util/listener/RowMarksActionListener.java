af29cf8a83ce0a5406bd459353bc75a80f6c9a56
Neil Griffin <neil.griffin.scm@gmail.com>
2012-07-12 17:15:31 -0400

    FACES-1349 Develop liferay-faces-util project

--- util/src/main/java/com/liferay/faces/util/listener/RowMarksActionListener.java
+++ util/src/main/java/com/liferay/faces/util/listener/RowMarksActionListener.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
-import javax.faces.component.UIData;
 import javax.faces.component.UIParameter;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
@@ -25,8 +24,8 @@ import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
 import javax.faces.model.DataModel;
 
-import com.liferay.faces.util.context.ExtFacesContext;
 import com.liferay.faces.util.context.MessageContext;
+import com.liferay.faces.util.el.FaceletUtil;
 import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
 import com.liferay.faces.util.model.RowMarker;
@@ -67,7 +66,7 @@ public class RowMarksActionListener implements ActionListener {
 
 				if ((uiParameterName != null) && uiParameterName.equals(PARAM_NAME_FOR)) {
 					forClientId = (String) uiParameter.getValue();
-					dataModel = findDataModel(forClientId);
+					dataModel = FaceletUtil.findDataModel(forClientId);
 
 					break;
 				}
@@ -125,55 +124,4 @@ public class RowMarksActionListener implements ActionListener {
 		MessageContext.getInstance().newFacesMessage(FacesContext.getCurrentInstance(), FacesMessage.SEVERITY_INFO,
 			"an-unexpected-error-occurred");
 	}
-
-	/**
-	 * Returns the {@link javax.faces.model.DataModel} in the current ViewRoot associated with the specified
-	 * forClientId.
-	 */
-	protected DataModel<?> findDataModel(String forClientId) {
-
-		DataModel<?> dataModel = null;
-
-		if (forClientId != null) {
-			ExtFacesContext extFacesContext = ExtFacesContext.getInstance();
-			UIComponent forUIComponent = extFacesContext.matchComponentInViewRoot(forClientId);
-
-			if (forUIComponent != null) {
-
-				if (forUIComponent instanceof UIData) {
-					UIData uiData = (UIData) forUIComponent;
-					Object uiDataValue = uiData.getValue();
-
-					if (uiDataValue != null) {
-
-						if (uiDataValue instanceof DataModel) {
-							dataModel = (DataModel<?>) uiDataValue;
-						}
-						else {
-							logger.error("The 'value' attribute for the component [{}] is not a subclass of [{}]",
-								uiData.getClass().getName(), DataModel.class.getName());
-						}
-					}
-					else {
-						logger.error("The 'value' attribute is not set for the component [{}]",
-							uiData.getClass().getName());
-					}
-				}
-				else {
-					logger.error(
-						"Cannot determine javax.faces.DataModel for=[{0}] since corresponding component in the view=[{1}] is not a subclass of javax.faces.component.UIData",
-						forClientId, forUIComponent.getClass().getName());
-				}
-			}
-			else {
-				logger.error("Unable to find component in the view with clientId for=[{0}]", forClientId);
-			}
-		}
-		else {
-			logger.error("Must specifiy a value for the for parameter");
-		}
-
-		return dataModel;
-	}
-
 }
