0c8953183d8b3763a278081db4f0023468830f71
Neil Griffin <neil.griffin.scm@gmail.com>
2012-07-20 19:26:03 -0400

    FACES-1358 Develop JSF1 <-> JSF2 compatibility layer

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/BridgeContextCompatImpl.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.context;
+
+import javax.faces.context.FacesContext;
+import javax.faces.context.PartialViewContext;
+
+
+/**
+ * This class provides a compatibility layer that isolates differences between JSF1 and JSF2.
+ *
+ * @author  Neil Griffin
+ */
+public abstract class BridgeContextCompatImpl extends BridgeContext {
+
+	protected void partialViewContextRenderAll(FacesContext facesContext) {
+
+		PartialViewContext partialViewContext = facesContext.getPartialViewContext();
+
+		if (!partialViewContext.isRenderAll()) {
+			partialViewContext.setRenderAll(true);
+		}
+	}
+}
