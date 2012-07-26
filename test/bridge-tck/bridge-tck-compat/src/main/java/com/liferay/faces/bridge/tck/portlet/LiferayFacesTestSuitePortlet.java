655f0257a4fcd95a948e0a5a6adf6270a14d0e85
Neil Griffin <neil.griffin.scm@gmail.com>
2012-07-26 16:37:42 -0400

    https://issues.apache.org/jira/browse/PORTLETBRIDGE-226 requestProcessingNonFacesTest specifies charset in JSP (removed workaround from bridge-tck-compat project)

--- test/bridge-tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/portlet/LiferayFacesTestSuitePortlet.java
+++ test/bridge-tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/portlet/LiferayFacesTestSuitePortlet.java
@@ -13,27 +13,16 @@
  */
 package com.liferay.faces.bridge.tck.portlet;
 
-import java.io.IOException;
-
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 
 import org.apache.myfaces.portlet.faces.testsuite.common.portlet.GenericFacesTestSuitePortlet;
-
 
 
 /**
  * @author  Neil Griffin
  */
 public class LiferayFacesTestSuitePortlet extends GenericFacesTestSuitePortlet {
-
-	@Override
-	public void doDispatch(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException,
-		IOException {
-		super.doDispatch(renderRequest, new RenderResponseTCKImpl(renderResponse));
-	}
 
 	@Override
 	public void init(PortletConfig portletConfig) throws PortletException {
