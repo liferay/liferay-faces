999adab24624f728c5e927813c33e9254fcb109c
Neil Griffin <neil.griffin.scm@gmail.com>
2012-07-30 16:57:53 -0400

    Workaround for http://java.net/jira/browse/JAVASERVERFACES-1803 Mojarra 1.2_15 ManagedBeanELResolver.setValue() broken/regression

--- bridge-impl/src/main/java/com/liferay/faces/bridge/event/PublicRenderParameterCompatImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/event/PublicRenderParameterCompatImpl.java
@@ -13,11 +13,18 @@
  */
 package com.liferay.faces.bridge.event;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import javax.el.ELContext;
 import javax.el.PropertyNotFoundException;
 import javax.el.ValueExpression;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
+import com.liferay.faces.bridge.BridgeConstants;
+import com.liferay.faces.bridge.config.ProductMap;
 import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
 
@@ -32,19 +39,75 @@ public abstract class PublicRenderParameterCompatImpl extends PublicRenderParame
 	// Logger
 	private static final Logger logger = LoggerFactory.getLogger(PublicRenderParameterCompatImpl.class);
 
+	// Private Constants
+	private static final boolean MOJARRA_DETECTED = (ProductMap.getInstance().get(BridgeConstants.JSF).getTitle()
+			.toLowerCase().indexOf(BridgeConstants.MOJARRA.toLowerCase()) >= 0);
+
 	public PublicRenderParameterCompatImpl(String prefix, String originalRequestValue, String originalModelEL,
 		String portletName) {
 		super(prefix, originalRequestValue, originalModelEL, portletName);
 	}
 
+	/**
+	 * <p>This method provides a workaround for an issue in Mojarra 1.2 that prevents String-based managed-bean
+	 * instances from having their values set properly via EL-expressions. For more information, see:
+	 * http://java.net/jira/browse/JAVASERVERFACES-1803</p>
+	 */
 	public boolean injectIntoModel(FacesContext facesContext) {
 
 		try {
 
+			// Inject the value via the EL-expression.
 			ELContext elContext = facesContext.getELContext();
 			ValueExpression valueExpression = facesContext.getApplication().getExpressionFactory()
 				.createValueExpression(elContext, modelEL, String.class);
 			valueExpression.setValue(elContext, originalRequestValue);
+
+			// If Mojarra is detected, then
+			if (MOJARRA_DETECTED) {
+
+				// Get the value that Mojarra claims to have injected.
+				Object mojarraInjectedValue = valueExpression.getValue(elContext);
+
+				// If the injected value doesn't match up with the value that Mojarra claims to have injected, and the
+				// value is an empty String, then Mojarra simply resolved the EL-expression to a managed-bean and failed
+				// to actually set the value (replace the managed-bean instance with a new String instance).
+				if ((originalRequestValue != mojarraInjectedValue) && (mojarraInjectedValue != null) &&
+						(mojarraInjectedValue instanceof String) && (((String) mojarraInjectedValue).length() == 0)) {
+
+					// If the value that is to be injected is a String-based managed-bean in request scope, then
+					// update the value directly in the request map.
+					ExternalContext externalContext = facesContext.getExternalContext();
+					Map<String, Object> requestMap = externalContext.getRequestMap();
+					Entry<String, Object> mapEntry = getMapEntryByValue(requestMap, mojarraInjectedValue);
+
+					if (mapEntry != null) {
+						requestMap.put(mapEntry.getKey(), originalRequestValue);
+					}
+
+					// Otherwise, if the value that is to be injected is a managed-bean in session scope, then update
+					// the value directly in the session map.
+					else {
+						Map<String, Object> sessionMap = externalContext.getSessionMap();
+						mapEntry = getMapEntryByValue(sessionMap, mojarraInjectedValue);
+
+						if (mapEntry != null) {
+							sessionMap.put(mapEntry.getKey(), originalRequestValue);
+						}
+
+						// Otherwise, if the value that is to be injected is a managed-bean in application scope, then
+						// update the value directly in the application map.
+						else {
+							Map<String, Object> applicationMap = externalContext.getApplicationMap();
+							mapEntry = getMapEntryByValue(applicationMap, mojarraInjectedValue);
+
+							if (mapEntry != null) {
+								applicationMap.put(mapEntry.getKey(), originalRequestValue);
+							}
+						}
+					}
+				}
+			}
 
 			return true;
 		}
@@ -60,6 +123,24 @@ public abstract class PublicRenderParameterCompatImpl extends PublicRenderParame
 
 			return false;
 		}
+	}
+
+	protected Map.Entry<String, Object> getMapEntryByValue(Map<String, Object> map, Object value) {
+
+		Map.Entry<String, Object> mapEntry = null;
+		Set<Entry<String, Object>> entrySet = map.entrySet();
+
+		for (Map.Entry<String, Object> curMapEntry : entrySet) {
+			Object curValue = curMapEntry.getValue();
+
+			if ((curValue == value)) {
+				mapEntry = curMapEntry;
+
+				break;
+			}
+		}
+
+		return mapEntry;
 	}
 
 }
