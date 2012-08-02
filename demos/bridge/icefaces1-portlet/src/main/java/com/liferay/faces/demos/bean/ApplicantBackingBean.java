8ccfbdf588e3ea8971b438e8851e8394808f6c14
Neil Griffin <neil.griffin.scm@gmail.com>
2012-08-02 16:55:27 -0400

    FACES-1357 Develop 2.1.x branch in order to provide basic support for JSF 1.2

---
+++ demos/bridge/icefaces1-portlet/src/main/java/com/liferay/faces/demos/bean/ApplicantBackingBean.java
@@ -0,0 +1,242 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.bean;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.EventObject;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
+
+import com.icesoft.faces.component.inputfile.FileInfo;
+import com.icesoft.faces.component.inputfile.InputFile;
+
+import com.liferay.faces.bridge.model.UploadedFile;
+import com.liferay.faces.demos.dto.City;
+import com.liferay.faces.demos.dto.UploadedFileWrapper;
+import com.liferay.faces.demos.util.FacesMessageUtil;
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
+
+
+/**
+ * This is a JSF backing managed-bean for the applicant.xhtml composition.
+ *
+ * @author  "Neil Griffin"
+ */
+public class ApplicantBackingBean implements Serializable {
+
+	// serialVersionUID
+	private static final long serialVersionUID = 3047548873495692163L;
+
+	// Logger
+	private static final transient Logger logger = LoggerFactory.getLogger(ApplicantBackingBean.class);
+
+	// Injections
+	private transient ApplicantModelBean applicantModelBean;
+	private transient ListModelBean listModelBean;
+
+	// JavaBeans Properties for UI
+	private boolean commentsRendered;
+	private int percentComplete;
+	private boolean popupRendered;
+	private String uploadedFileId;
+
+	public void deleteUploadedFile(ActionEvent actionEvent) {
+
+		try {
+			List<UploadedFile> uploadedFiles = applicantModelBean.getUploadedFiles();
+
+			UploadedFile uploadedFileToDelete = null;
+
+			for (UploadedFile uploadedFile : uploadedFiles) {
+
+				if (uploadedFile.getId().equals(uploadedFileId)) {
+					uploadedFileToDelete = uploadedFile;
+
+					break;
+				}
+			}
+
+			if (uploadedFileToDelete != null) {
+				uploadedFileToDelete.delete();
+				uploadedFiles.remove(uploadedFileToDelete);
+				logger.debug("Deleted file=[{0}]", uploadedFileToDelete.getName());
+			}
+		}
+		catch (Exception e) {
+			logger.error(e);
+		}
+
+		popupRendered = false;
+	}
+
+	public void fileUploadActionListener(ActionEvent actionEvent) {
+
+		percentComplete = 0;
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		try {
+			InputFile inputFile = (InputFile) actionEvent.getSource();
+			FileInfo fileInfo = inputFile.getFileInfo();
+			int status = fileInfo.getStatus();
+
+			if (status == InputFile.INVALID) {
+				FacesMessageUtil.addGlobalErrorMessage(facesContext, "you-have-entered-invalid-data", null);
+			}
+			else if ((status == InputFile.INVALID_CONTENT_TYPE) || (status == InputFile.INVALID_NAME_PATTERN)) {
+				FacesMessageUtil.addGlobalErrorMessage(facesContext, "file-type-is-invalid", null);
+			}
+			else if (status == InputFile.SAVED) {
+				List<UploadedFile> uploadedFiles = applicantModelBean.getUploadedFiles();
+
+				UploadedFile uploadedFile = new UploadedFileWrapper(fileInfo);
+
+				synchronized (uploadedFiles) {
+
+					uploadedFiles.add(uploadedFile);
+				}
+			}
+			else if (status == InputFile.SIZE_LIMIT_EXCEEDED) {
+				FacesMessageUtil.addGlobalErrorMessage(facesContext, "please-enter-a-file-with-a-valid-file-size",
+					null);
+			}
+		}
+		catch (Exception e) {
+			logger.error(e);
+			FacesMessageUtil.addGlobalUnexpectedErrorMessage(facesContext);
+		}
+	}
+
+	public void fileUploadProgressListener(EventObject eventObject) {
+		InputFile inputFile = (InputFile) eventObject.getSource();
+		percentComplete = inputFile.getFileInfo().getPercent();
+	}
+
+	public void postalCodeListener(ValueChangeEvent valueChangeEvent) {
+
+		try {
+			String newPostalCode = (String) valueChangeEvent.getNewValue();
+			City city = listModelBean.getCityByPostalCode(newPostalCode);
+
+			if (city != null) {
+				applicantModelBean.setAutoFillCity(city.getCityName());
+				applicantModelBean.setAutoFillProvinceId(city.getProvinceId());
+			}
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			FacesMessageUtil.addGlobalUnexpectedErrorMessage(FacesContext.getCurrentInstance());
+		}
+	}
+
+	public String submit() {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("firstName=" + applicantModelBean.getFirstName());
+			logger.debug("lastName=" + applicantModelBean.getLastName());
+			logger.debug("emailAddress=" + applicantModelBean.getEmailAddress());
+			logger.debug("phoneNumber=" + applicantModelBean.getPhoneNumber());
+			logger.debug("dateOfBirth=" + applicantModelBean.getDateOfBirth());
+			logger.debug("city=" + applicantModelBean.getCity());
+			logger.debug("provinceId=" + applicantModelBean.getProvinceId());
+			logger.debug("postalCode=" + applicantModelBean.getPostalCode());
+			logger.debug("comments=" + applicantModelBean.getComments());
+
+			List<UploadedFile> uploadedFiles = applicantModelBean.getUploadedFiles();
+
+			for (UploadedFile uploadedFile : uploadedFiles) {
+				logger.debug("uploadedFile=[{0}]", uploadedFile.getName());
+			}
+		}
+
+		// Delete the uploaded files.
+		try {
+			List<UploadedFile> uploadedFiles = applicantModelBean.getUploadedFiles();
+
+			for (UploadedFile uploadedFile : uploadedFiles) {
+				File file = new File(uploadedFile.getAbsolutePath());
+				file.delete();
+				logger.debug("Deleted file=[{0}]", file);
+			}
+
+			applicantModelBean.clearProperties();
+
+			return "success";
+
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			FacesMessageUtil.addGlobalUnexpectedErrorMessage(FacesContext.getCurrentInstance());
+
+			return "failure";
+		}
+	}
+
+	public void toggleComments(ActionEvent actionEvent) {
+		commentsRendered = !commentsRendered;
+	}
+
+	public void togglePopup(ActionEvent actionEvent) {
+		popupRendered = !popupRendered;
+	}
+
+	public void setApplicantModelBean(ApplicantModelBean applicantModelBean) {
+
+		// Injected via WEB-INF/faces-config.xml managed-property
+		this.applicantModelBean = applicantModelBean;
+	}
+
+	public void setCommentsRendered(boolean commentsRendered) {
+		this.commentsRendered = commentsRendered;
+	}
+
+	public boolean isCommentsRendered() {
+		return commentsRendered;
+	}
+
+	public boolean isPopupRendered() {
+		return popupRendered;
+	}
+
+	public void setListModelBean(ListModelBean listModelBean) {
+
+		// Injected via WEB-INF/faces-config.xml managed-property
+		this.listModelBean = listModelBean;
+	}
+
+	public int getPercentComplete() {
+		return percentComplete;
+	}
+
+	public void setPercentComplete(int percentComplete) {
+		this.percentComplete = percentComplete;
+	}
+
+	public void setPopupRendered(boolean popupRendered) {
+		this.popupRendered = popupRendered;
+	}
+
+	public String getUploadedFileId() {
+		return uploadedFileId;
+	}
+
+	public void setUploadedFileId(String uploadedFileId) {
+		this.uploadedFileId = uploadedFileId;
+	}
+}
---
+++ demos/bridge/icefaces1-portlet/src/main/java/com/liferay/faces/demos/bean/ListModelBean.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
+import com.liferay.faces.demos.dto.City;
+import com.liferay.faces.demos.dto.Province;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class ListModelBean implements Serializable {
+
+	// serialVersionUID
+	private static final long serialVersionUID = 4533667773050051612L;
+
+	// Private Data Members
+	private List<City> cities;
+	private List<Province> provinces;
+	private List<SelectItem> provinceSelectItems;
+
+	public List<City> getCities() {
+
+		if (cities == null) {
+			long cityId = 1;
+			cities = new ArrayList<City>();
+
+			City city = new City(cityId++, getProvinceId("DE"), "Wilmington", "19806");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("GA"), "Atlanta", "30329");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("FL"), "Orlando", "32801");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("MD"), "Baltimore", "21224");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("NC"), "Charlotte", "28202");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("NJ"), "Hoboken", "07030");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("NY"), "Albany", "12205");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("SC"), "Columbia", "29201");
+			cities.add(city);
+			city = new City(cityId++, getProvinceId("VA"), "Roanoke", "24013");
+			cities.add(city);
+		}
+
+		return cities;
+	}
+
+	public City getCityByPostalCode(String postalCode) {
+		List<City> cities = getCities();
+
+		for (City city : cities) {
+
+			if (city.getPostalCode().equals(postalCode)) {
+				return city;
+			}
+		}
+
+		return null;
+	}
+
+	public long getProvinceId(String provinceName) {
+		long provinceId = 0;
+		List<Province> provinces = getProvinces();
+
+		for (Province province : provinces) {
+
+			if (province.getProvinceName().equals(provinceName)) {
+				provinceId = province.getProvinceId();
+
+				break;
+			}
+		}
+
+		return provinceId;
+	}
+
+	public List<Province> getProvinces() {
+
+		if (provinces == null) {
+			long provinceId = 1;
+			provinces = new ArrayList<Province>();
+
+			Province province = new Province(provinceId++, "DE");
+			provinces.add(province);
+			province = new Province(provinceId++, "GA");
+			provinces.add(province);
+			province = new Province(provinceId++, "FL");
+			provinces.add(province);
+			province = new Province(provinceId++, "MD");
+			provinces.add(province);
+			province = new Province(provinceId++, "NC");
+			provinces.add(province);
+			province = new Province(provinceId++, "NJ");
+			provinces.add(province);
+			province = new Province(provinceId++, "NY");
+			provinces.add(province);
+			province = new Province(provinceId++, "SC");
+			provinces.add(province);
+			province = new Province(provinceId++, "VA");
+			provinces.add(province);
+		}
+
+		return this.provinces;
+	}
+
+	public List<SelectItem> getProvinceSelectItems() {
+
+		if (provinceSelectItems == null) {
+			provinceSelectItems = new ArrayList<SelectItem>();
+
+			List<Province> provinces = getProvinces();
+
+			for (Province province : provinces) {
+				SelectItem selectItem = new SelectItem(province.getProvinceId(), province.getProvinceName());
+				provinceSelectItems.add(selectItem);
+			}
+		}
+
+		return provinceSelectItems;
+	}
+}
