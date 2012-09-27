c12261d0dbbb3e9e442b62083d9992ad29e116f3
Neil Griffin <neil.griffin.scm@gmail.com>
2012-09-27 16:14:03 -0400

    FACES-1427 Support Bulletin Board Code (bbcode) for liferay-ui:input-editor

---
+++ portal/src/main/java/com/liferay/portal/kernel/editor/EditorUtil.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.portal.kernel.editor;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+
+/**
+ * This class is a pseudo-backport of the EditorUtil class which was introduced in Liferay Portal 6.1.x. It's main
+ * purpose is to facilitate keeping the {@link InputEditorInternalRenderer} source the same on all branches of the
+ * Liferay Faces source.
+ *
+ * @author  Julio Camarero
+ * @author  Neil Griffin
+ */
+public class EditorUtil {
+
+	private static final String _EDITOR_WYSIWYG_DEFAULT = PropsUtil.get(PropsKeys.EDITOR_WYSIWYG_DEFAULT);
+
+	public static String getEditorValue(HttpServletRequest request, String editorImpl) {
+
+		if (Validator.isNull(editorImpl)) {
+			editorImpl = _EDITOR_WYSIWYG_DEFAULT;
+		}
+
+		return editorImpl;
+	}
+
+}
