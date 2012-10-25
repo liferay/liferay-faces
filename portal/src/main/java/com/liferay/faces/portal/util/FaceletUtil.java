d7ed8c1085050101c3761e5332f3e2af17b98203
Neil Griffin <neil.griffin.scm@gmail.com>
2012-10-25 15:15:33 -0400

    FACES-1422 liferay-ui:input-editor causes invalid DOM diff during Ajax postback when used with ICEfaces FACES-1439 liferay-ui:input-editor does not work in show/hide use case scenarios FACES-1440 Support f:ajax "change" ClientBehavior for liferay-ui:input-editor FACES-1441 Support f:ajax "blur" ClientBehavior for liferay-ui:input-editor FACES-1427 Support Bulletin Board Code (bbcode) for liferay-ui:input-editor

---
+++ portal/src/main/java/com/liferay/faces/portal/util/FaceletUtil.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portal.util;
+
+import javax.faces.context.FacesContext;
+import javax.portlet.PortletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import com.liferay.portal.kernel.editor.EditorUtil;
+import com.liferay.portal.util.PortalUtil;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class FaceletUtil {
+
+	public static String getEditorValue(String editorImpl) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		PortletRequest portletRequest = (PortletRequest) facesContext.getExternalContext().getRequest();
+		HttpServletRequest httpServletRequest = PortalUtil.getHttpServletRequest(portletRequest);
+		String editorValue = EditorUtil.getEditorValue(httpServletRequest, editorImpl);
+
+		return editorValue;
+	}
+}
