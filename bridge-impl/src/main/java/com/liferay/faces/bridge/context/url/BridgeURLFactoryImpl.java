eb5ddde35d13c516523ddc2d09efb4daca9e2ab8
Neil Griffin <neil.griffin.scm@gmail.com>
2013-02-22 18:52:32 -0500

    FACES-1479 Bridge is unable to handle PortletRequestWrapper created by a PortletFilter when running under Liferay Portal FACES-1328 Remove Java reflection for *LiferayImpl.java classes

--- bridge-impl/src/main/java/com/liferay/faces/bridge/context/url/BridgeURLFactoryImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/url/BridgeURLFactoryImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -40,7 +40,7 @@ public class BridgeURLFactoryImpl extends BridgeURLFactory {
 	@Override
 	public BridgeResourceURL getBridgeResourceURL(String url, String currentFacesViewId, BridgeContext bridgeContext) {
 
-		if (PortletContainerDetector.isLiferayObject(bridgeContext.getPortletConfig())) {
+		if (PortletContainerDetector.isLiferayPortletRequest(bridgeContext.getPortletRequest())) {
 			return new BridgeResourceURLLiferayImpl(url, currentFacesViewId, bridgeContext);
 		}
 		else {
