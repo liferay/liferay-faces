acc43c40982e4c66b9e8a69d91897d407ddc16f6
kylestiemann <kyle.stiemann@liferay.com>
2013-03-06 16:54:32 -0500

    FACES-1484 Refactor icefaces3-users-portlet so that it uses the newer ACE components and supports portrait image upload

---
+++ demos/portal/icefaces3-users-portlet/src/main/java/com/liferay/faces/demos/util/UploadedFileUtil.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.util;
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class UploadedFileUtil {
+
+	// Private Constants
+	private static final String JAVA_IO_TMPDIR = "java.io.tmpdir";
+	private static final String TMP = ".tmp";
+	private static final String USER_PORTRAIT = "user-portrait-";
+
+	public static String createFileName(String uploadedFileId) {
+		return USER_PORTRAIT + uploadedFileId + TMP;
+	}
+
+	public static String getTempDir() {
+		return System.getProperty(JAVA_IO_TMPDIR);
+	}
+}
