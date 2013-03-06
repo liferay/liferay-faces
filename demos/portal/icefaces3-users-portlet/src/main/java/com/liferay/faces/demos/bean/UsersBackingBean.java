425e03b3712cbd54209530809586dcc930b3b16e
Neil Griffin <neil.griffin.scm@gmail.com>
2013-03-06 17:22:14 -0500

    Merge remote-tracking branch 'refs/remotes/kylestiemann/FACES-1484' into 3.1.x

--- demos/portal/icefaces3-users-portlet/src/main/java/com/liferay/faces/demos/bean/UsersBackingBean.java
+++ demos/portal/icefaces3-users-portlet/src/main/java/com/liferay/faces/demos/bean/UsersBackingBean.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -16,21 +16,30 @@ package com.liferay.faces.demos.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
-import javax.faces.event.ActionListener;
 
-import com.liferay.faces.util.helper.LongHelper;
+import org.icefaces.ace.component.fileentry.FileEntry;
+import org.icefaces.ace.component.fileentry.FileEntryEvent;
+import org.icefaces.ace.component.fileentry.FileEntryResults;
+import org.icefaces.ace.event.SelectEvent;
+
+import com.liferay.faces.bridge.model.UploadedFile;
+import com.liferay.faces.demos.dto.UploadedFileWrapper;
+import com.liferay.faces.demos.util.UploadedFileUtil;
+import com.liferay.faces.portal.context.LiferayFacesContext;
 import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
-import com.liferay.faces.portal.context.LiferayFacesContext;
 
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.UserLocalServiceUtil;
 
 
 /**
+ * This class serves as a backing bean for the users.xhtml Facelet view. The bean is kept in request scope since it
+ * doesn't maintain any model data.
+ *
  * @author  Neil Griffin
+ * @author  Kyle Stiemann
  */
 @ManagedBean(name = "usersBackingBean")
 @RequestScoped
@@ -50,25 +59,101 @@ public class UsersBackingBean {
 	private LiferayFacesContext liferayFacesContext = LiferayFacesContext.getInstance();
 
 	// Private Data Members
-	private CancelUserActionListener cancelUserActionListener = new CancelUserActionListener();
-	private SaveUserActionListener saveUserActionListener = new SaveUserActionListener();
-	private SearchActionListener searchActionListener = new SearchActionListener();
-	private SelectUserActionListener selectUserActionListener = new SelectUserActionListener();
+	private String fileUploadAbsolutePath;
+	private String uploadedFileId;
+	private UploadedFile uploadedFile;
 
-	public CancelUserActionListener getCancelUserActionListener() {
-		return cancelUserActionListener;
+	public void cancel(ActionEvent actionEvent) {
+		usersViewBean.setFormRendered(false);
 	}
 
-	public SaveUserActionListener getSaveUserActionListener() {
-		return saveUserActionListener;
+	public void handleFileUpload(FileEntryEvent fileEntryEvent) {
+
+		try {
+			FileEntry fileEntry = (FileEntry) fileEntryEvent.getSource();
+			FileEntryResults results = fileEntry.getResults();
+			FileEntryResults.FileInfo fileInfo = results.getFiles().get(0);
+			UploadedFileWrapper uploadedFile = new UploadedFileWrapper(fileInfo);
+
+			if (uploadedFile.getStatus() == UploadedFile.Status.FILE_SAVED) {
+				usersModelBean.setUploadedFile(uploadedFile);
+			}
+			else {
+				logger.error("Uploaded file status=[" + uploadedFile.getStatus().toString() + "] " +
+					uploadedFile.getMessage());
+			}
+		}
+		catch (Exception e) {
+			logger.error(e);
+			liferayFacesContext.addGlobalUnexpectedErrorMessage();
+		}
 	}
 
-	public SearchActionListener getSearchActionListener() {
-		return searchActionListener;
+	public void save(ActionEvent actionEvent) {
+
+		try {
+
+			// Update the selected user in the Liferay database.
+			User user = usersModelBean.getSelectedUser();
+			long userId = user.getUserId();
+			UserLocalServiceUtil.updateStatus(userId, user.getStatus());
+			UserLocalServiceUtil.updateUser(user);
+
+			// If the end-user uploaded a portrait, then update the portrait in the Liferay database. Note that there is
+			// a bug in ICEfaces such that if the end-user forgot to click on the "Upload Portrait" button first, then
+			// the selected file will not be saved.
+			UploadedFile modelUploadedFile = usersModelBean.getUploadedFile();
+
+			if (modelUploadedFile != null) {
+				byte[] imageBytes = modelUploadedFile.getBytes();
+				UserLocalServiceUtil.updatePortrait(userId, imageBytes);
+			}
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			liferayFacesContext.addGlobalUnexpectedErrorMessage();
+		}
+
+		usersViewBean.setFormRendered(false);
+		usersModelBean.forceListReload();
 	}
 
-	public SelectUserActionListener getSelectUserActionListener() {
-		return selectUserActionListener;
+	public void search(ActionEvent actionEvent) {
+		usersModelBean.forceListReload();
+	}
+
+	public void selectUser(SelectEvent selectEvent) {
+
+		try {
+			User selectedUser = (User) selectEvent.getObject();
+			usersModelBean.setSelectedUser(selectedUser);
+			usersViewBean.setFormRendered(true);
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			liferayFacesContext.addGlobalUnexpectedErrorMessage();
+		}
+	}
+
+	public String getFileUploadAbsolutePath() {
+
+		if (fileUploadAbsolutePath == null) {
+			fileUploadAbsolutePath = UploadedFileUtil.getTempDir();
+		}
+
+		return fileUploadAbsolutePath;
+	}
+
+	public UploadedFile getUploadedFile() {
+		return uploadedFile;
+	}
+
+	public String getUploadedFileId() {
+		return uploadedFileId;
+	}
+
+	public void setUploadedFileId(String uploadedFileId) {
+		this.uploadedFileId = uploadedFileId;
 	}
 
 	public void setUsersModelBean(UsersModelBean usersModelBean) {
@@ -82,63 +167,4 @@ public class UsersBackingBean {
 		// Injected via ManagedProperty annotation
 		this.usersViewBean = usersViewBean;
 	}
-
-	protected class CancelUserActionListener implements ActionListener {
-
-		public void processAction(ActionEvent event) throws AbortProcessingException {
-
-			usersViewBean.setFormRendered(false);
-		}
-
-	}
-
-	protected class SaveUserActionListener implements ActionListener {
-
-		public void processAction(ActionEvent event) throws AbortProcessingException {
-
-			User user = usersModelBean.getSelectedUser();
-
-			try {
-				UserLocalServiceUtil.updateStatus(user.getUserId(), user.getStatus());
-				UserLocalServiceUtil.updateUser(user);
-			}
-			catch (Exception e) {
-				logger.error(e.getMessage(), e);
-				liferayFacesContext.addGlobalUnexpectedErrorMessage();
-			}
-
-			usersViewBean.setFormRendered(false);
-			usersModelBean.forceListReload();
-		}
-
-	}
-
-	protected class SearchActionListener implements ActionListener {
-
-		public void processAction(ActionEvent event) throws AbortProcessingException {
-
-			usersModelBean.forceListReload();
-		}
-
-	}
-
-	protected class SelectUserActionListener implements ActionListener {
-
-		public void processAction(ActionEvent event) throws AbortProcessingException {
-
-			try {
-				long userId = LongHelper.toLong(liferayFacesContext.getExternalContext().getRequestParameterMap().get(
-							"userId"), 0L);
-				User user = UserLocalServiceUtil.getUser(userId);
-				usersModelBean.setSelectedUser(user);
-				usersViewBean.setFormRendered(true);
-			}
-			catch (Exception e) {
-				logger.error(e.getMessage(), e);
-				liferayFacesContext.addGlobalUnexpectedErrorMessage();
-			}
-		}
-
-	}
-
 }
