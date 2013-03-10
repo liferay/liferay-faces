30ba025dc57f6393143891476bb7efb836e3b9c6
Vernon Singleton <vernon.singleton@liferay.com>
2013-03-09 21:21:46 -0500

    added tests for most of the elements on the jsf2-portlet

--- test/demos/bridge/jsf2-portlet/src/test/java/com/liferay/faces/test/Jsf2PortletTest.java
+++ test/demos/bridge/jsf2-portlet/src/test/java/com/liferay/faces/test/Jsf2PortletTest.java
@@ -6,6 +6,7 @@ import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.enricher.findby.FindBy;
 //import org.jboss.arquillian.graphene.*;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,9 +15,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 //import java.net.URL;
+
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -26,157 +29,180 @@ import static org.jboss.arquillian.graphene.Graphene.waitModel;
 
 @RunWith(Arquillian.class)
 public class Jsf2PortletTest {
-
+	
 	// private static final Logger logger;
-	private final static Logger logger = Logger.getLogger(Jsf2PortletTest.class
-			.getName());
+	private final static Logger logger = Logger.getLogger(Jsf2PortletTest.class.getName());
 	
-	// //*[@id="portlet_1_WAR_jsf2loginportlet"]/header/h1/span[2]
-	// @FindBy(xpath = "//*[@id='portlet_1_WAR_jsf2loginportlet']/header/h1/span[2]")
-	@FindBy(xpath = "//section[@id='portlet_1_WAR_jsf2loginportlet']/header/h1/span[2]")
-	private WebElement portletDisplayName;
-	
-	// //*[@id="A2677:j_idt3"]/ul/li
-	// Authentication failed. Please try again.
-	// //*[@id="A2677:j_idt4"]/ul/li
-	// //ul/li[@class="portlet-msg-error"]
-	// Authentication failed. Please try again.
-	// //*[@id="A2677:j_idt4"]/ul/li
-	// //form[@id='A2677:j_idt4']/ul/li
-	// //*[@id="A2677"]
-	// //*[@id="A2677:j_idt4"]/ul/li
-	// @FindBy(xpath = "//form[@id='A2677:j_idt4']/ul/li")
-	@FindBy(xpath = "//form[@method='post']/ul/li")
-	private WebElement messageError;
-
-	// @FindBy(id = "_58_login")
-	// //*[@id="A2677:j_idt6:j_idt8:j_idt9:handle"]
-	// //input[contains(@id,'buttonTargetNav2')]
-	@FindBy(xpath = "//input[contains(@id,':handle')]")
-	private WebElement emailField;
-
-	// @FindBy(id = "_58_password")
-	// //*[@id="A2677:j_idt6:j_idt8:j_idt9:password"]
-	@FindBy(xpath = "//input[contains(@id,':password')]")
-	private WebElement passwordField;
-
-	// @FindBy(id = "j_login")
-	// //*[@id="A2677:j_idt6:j_idt8"]/div/input
-	// //*[@id="A2677:j_idt4:j_idt6"]/div/input
-	// <input type="submit" name="A2677:j_idt4:j_idt6:j_idt9" value="Sign In">
-	// //input[@type='submit' and @value='Sign In']
-	@FindBy(xpath = "//input[@type='submit' and @value='Sign In']")
-	private WebElement signInButton;
-
-	// //*[@id="A2677"]
-	// <div class="portlet-body" id="aui_3_4_0_1_490">
-	//   <div id="A2677" class="liferay-faces-bridge-body">You are signed in as Test Test.</div> </div>
-	// //section[@id='portlet_1_WAR_jsf2loginportlet']
-	// //div[contains(text(),'You are signed in as')]
-	// @FindBy(xpath = "//*[@id='A2677']")
-	@FindBy(xpath = "//div[contains(text(),'You are signed in as')]")
-	private WebElement portletBody;
-
-	// By id = By.id("button");
-	// WebElement element = driver.findElement(id);
-
 	// @ArquillianResource
 	// URL portalURL;
+	String url = "http://localhost:8080/web/guest/signin";
 
 	@Drone
 	WebDriver browser;
+	
+	// elements for logging in
+	@FindBy(xpath = "//input[contains(@id,':handle')]")
+	private WebElement emailField;
+	@FindBy(xpath = "//input[contains(@id,':password')]")
+	private WebElement passwordField;
+	@FindBy(xpath = "//input[@type='submit' and @value='Sign In']")
+	private WebElement signInButton;
+	@FindBy(xpath = "//div[contains(text(),'You are signed in as')]")
+	private WebElement portletBody;
 
+	// elements for Job Applicants
+	// //*[@id="aui_3_4_0_1_1938"]
+	// //header[@class='portlet-topper']/h1/span
+	@FindBy(xpath = "//header[@class='portlet-topper']/h1/span")
+	private WebElement portletDisplayName;
+	
+	// <img src="http://localhost:8080/group/bridge-demos/jsf2?p_p_id=1_WAR_jsf2portlet_INSTANCE_ABCD&amp;p_p_lifecycle=2&amp;p_p_state=normal&amp;p_p_mode=view&amp;p_p_cacheability=cacheLevelPage&amp;p_p_col_id=column-1&amp;p_p_col_count=1&amp;_1_WAR_jsf2portlet_INSTANCE_ABCD_javax.faces.resource=liferay-logo.png&amp;_1_WAR_jsf2portlet_INSTANCE_ABCD_ln=example" title="liferay.com">
+	// //*[@id="aui_3_4_0_1_1914"]/div[2]/a/img
+	// //img[contains(@src,'liferay-logo.png')]
+	// @FindBy(xpath = "//img[@title='liferay.com']")
+	// <a href="http://www.liferay.com/community/liferay-projects/liferay-faces" target="_blank" id="aui_3_4_0_1_794"> <img src="http://localhost:8080/group/bridge-demos/jsf2?p_p_id=1_WAR_jsf2portlet_INSTANCE_ABCD&amp;p_p_lifecycle=2&amp;p_p_state=normal&amp;p_p_mode=view&amp;p_p_cacheability=cacheLevelPage&amp;p_p_col_id=column-1&amp;p_p_col_count=1&amp;_1_WAR_jsf2portlet_INSTANCE_ABCD_javax.faces.resource=liferay-logo.png&amp;_1_WAR_jsf2portlet_INSTANCE_ABCD_ln=example" title="liferay.com" id="aui_3_4_0_1_793"> </a>
+	// @FindBy(xpath = "//a[@src='http://www.liferay.com/community/liferay-projects/liferay-faces']/img")
+	@FindBy(xpath = "//*[@class='sponsor-button']")
+	private WebElement logo;
+	
+	@FindBy(xpath = "//input[contains(@id,':firstName')]")
+	private WebElement firstNameField;
+	@FindBy(xpath = "//input[contains(@id,':lastName')]")
+	private WebElement lastNameField;
+	@FindBy(xpath = "//input[contains(@id,':emailAddress')]")
+	private WebElement emailAddressField;
+	@FindBy(xpath = "//input[contains(@id,':phoneNumber')]")
+	private WebElement phoneNumberField;
+	
+	@FindBy(xpath = "//input[contains(@id,':dateOfBirth')]")
+	private WebElement dateOfBirthField;
+	@FindBy(xpath = "//input[contains(@id,':city')]")
+	private WebElement cityField;
+	@FindBy(xpath = "//input[contains(@id,':provinceId')]")
+	private WebElement provinceIdField;
+	@FindBy(xpath = "//input[contains(@id,':postalCode')]")
+	private WebElement postalCodeField;
+	// <img src="http://localhost:8080/group/bridge-demos/jsf2?p_p_id=1_WAR_jsf2portlet_INSTANCE_ABCD&amp;p_p_lifecycle=2&amp;p_p_state=normal&amp;p_p_mode=view&amp;p_p_cacheability=cacheLevelPage&amp;p_p_col_id=column-1&amp;p_p_col_count=1&amp;_1_WAR_jsf2portlet_INSTANCE_ABCD_javax.faces.resource=icon-help.png&amp;_1_WAR_jsf2portlet_INSTANCE_ABCD_ln=example" title="Type any of these ZIP codes: 19806, 30329, 32801, 21224, 28202, 07030, 12205, 29201, 24013">
+	// //*[@id="A5601:l1:c1:f1:fs1:c1b:postalCodeField"]/span/span/img
+	// //img[contains(@title,'Type any of these ZIP codes')]
+	@FindBy(xpath = "//img[contains(@title,'Type any of these ZIP codes')]")
+	private WebElement postalCodeToolTip;
+	
+	// <input id="A5601:l1:c2:c2a:f3:j_idt45" name="A5601:l1:c2:c2a:f3:j_idt45" type="file" multiple="multiple">
+	@FindBy(xpath = "//input[@type='file' and @multiple='multiple']")
+	private WebElement multiFileUploadButton;
+	// <input type="submit" name="A5601:l1:c2:c2a:f3:j_idt46" value="Submit" id="aui_3_4_0_1_571">
+	@FindBy(xpath = "//form[@enctype='multipart/form-data']/input[@type='submit' and @value='Submit']")
+	private WebElement submitFilesButton;
+	
+	@FindBy(xpath = "//a[contains(text(),'Show Comments')]")
+	private WebElement showCommentsLink;
+	
+	// //input[@type='submit' and @value='Submit']
+	@FindBy(xpath = "//input[@type='submit' and @value='Submit']")
+	private WebElement submitButton;
+	@FindBy(xpath = "//input[@type='submit' and @value='Edit Preferences']")
+	private WebElement editPreferencesButton;
+	
+	// //ul/li/em[contains(text(),'Mojarra 2.1.18')]
+	@FindBy(xpath = "//ul/li/em[contains(text(),'Mojarra')]")
+	private WebElement mojarraVersion;
+	// //ul/li/em[contains(text(),'Liferay Faces Alloy 3.1.2')]
+	@FindBy(xpath = "//ul/li/em[contains(text(),'Liferay Faces Alloy')]")
+	private WebElement alloyVersion;
+	// //ul/li/em[contains(text(),'Liferay Faces Bridge 3.1.2')]
+	@FindBy(xpath = "//ul/li/em[contains(text(),'Liferay Faces Bridge')]")
+	private WebElement bridgeVersion;
+	
 	@Before
 	public void getNewSession() {
-		browser.manage().deleteAllCookies();
 		// Shut its dirty mouth
 		java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
 	}
 
+	public void signIn() throws Exception {
+		
+		// browser.manage().deleteAllCookies();
+		// logger.log(Level.INFO, "browser.manage().deleteAllCookies() ...");
+		
+		url = "http://localhost:8080/web/guest/signin";
+		logger.log(Level.INFO, "Signing in using browser.navigate().to(url) ...");
+		browser.navigate().to(url);
+		waitModel(browser);
+		Thread.sleep(5000);
+		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+		
+		emailField.clear();
+		emailField.sendKeys("test@liferay.com");
+		passwordField.clear();
+		passwordField.sendKeys("test");
+		signInButton.click();
+		waitModel(browser);
+		Thread.sleep(5000);
+		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+
+		logger.log(Level.INFO, portletBody.getText());
+		// assertTrue("You are signed in", portletBody.getText().contains("You are signed in"));
+	}
+	
 	@Test
 	@RunAsClient
-	public void failToSignIn() throws Exception {
-
-		// String url =
-		// "http://localhost:8080/group/bridge-demos/jsf2?js_fast_load=0";
-		// String url = "http://localhost:8080/group/bridge-demos/jsf2";
-		String url = "http://localhost:8080/web/guest/signin";
+	@InSequence(1)
+	public void createJobApplicant() throws Exception {
+		
+		signIn();
+		
+		url = "http://localhost:8080/group/bridge-demos/jsf2";
 		logger.log(Level.INFO, "url = " + url);
 		
-		browser.get(url);
+		logger.log(Level.INFO, "second browser.navigate().to ...");
+		browser.navigate().to("http://localhost:8080/group/bridge-demos/jsf2");
+		waitModel(browser);
+		Thread.sleep(5000);
+		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+		logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
+
+		// logger.log(Level.INFO, "browser.manage().logs().toString() = " + browser.manage().logs().toString());
 		
 		logger.log(Level.INFO, "portletDisplayName.getText() = " + portletDisplayName.getText());
-		assertTrue("portletDisplayName.isDisplayed()", portletDisplayName.isDisplayed());
-		assertTrue("portlet is called JSF2 Sign-In", portletDisplayName.getText().contains("JSF2 Sign-In"));
+		assertTrue("portletDisplayName.isDisplayed()",portletDisplayName.isDisplayed());
 		
-		logger.log(Level.INFO, "emailField.isDisplayed() = " + emailField.isDisplayed());
-		assertTrue("emailField.isDisplayed()", emailField.isDisplayed());
-		logger.log(Level.INFO, "passwordField.isDisplayed() = " + passwordField.isDisplayed());
-		assertTrue("passwordField.isDisplayed()", passwordField.isDisplayed());
-		logger.log(Level.INFO, "signInButton.isDisplayed() = " + signInButton.isDisplayed());
-		assertTrue("signInButton.isDisplayed()", signInButton.isDisplayed());
-
-		logger.log(Level.INFO, "clearing email textbox");
-		emailField.clear();
-		logger.log(Level.INFO, "emailField.sendKeys ...");
-		emailField.sendKeys("test@liferay.com");
-		logger.log(Level.INFO, "emailField.getText() = " + emailField.getText());
-
-		logger.log(Level.INFO, "passwordField.sendKeys ...");
-		passwordField.sendKeys("not_test");
-		logger.log(Level.INFO, "passwordField.getText() = " + passwordField.getText());
-
-		logger.log(Level.INFO, "signInButton.click() ...");
-		signInButton.click();
-
-		// wait until the submit button is displayed
-		logger.log(Level.INFO, "starting to wait ...");
-		waitAjax(browser);
-		// Thread.sleep(3000);
-		// Graphene.waitModel(browser).until(Graphene.element(messageError).isPresent()).wait(5);
+		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+		logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
 		// logger.log(Level.INFO, "browser.getPageSource() = " + browser.getPageSource());
 		
-		logger.log(Level.INFO,
-				"messageError.getText() = " + messageError.getText());
-		assertTrue("messageError is displayed", messageError.isDisplayed());
-		assertTrue("Authentication failed",
-				messageError.getText().contains("Authentication failed"));
-
-	}
-
-	@Test
-	@RunAsClient
-	public void signIn() throws Exception {
-
-		String url = "http://localhost:8080/web/guest/signin";
-
-		logger.log(Level.INFO, "url = " + url);
-		browser.get(url);
-
-		assertTrue("emailField.isDisplayed()", emailField.isDisplayed());
-		assertTrue("passwordField.isDisplayed()", passwordField.isDisplayed());
-		assertTrue("signInButton.isDisplayed()", signInButton.isDisplayed());
-
-		logger.log(Level.INFO, "clearing emailField");
-		emailField.clear();
-		logger.log(Level.INFO, "emailField.sendKeys ...");
-		emailField.sendKeys("test@liferay.com");
-		logger.log(Level.INFO, "clearing passwordField");
-		passwordField.clear();
-		logger.log(Level.INFO, "passwordField.sendKeys ...");
-		passwordField.sendKeys("test");
+		assertTrue("logo.isDisplayed()",logo.isDisplayed());
 		
-		logger.log(Level.INFO, "signInButton.click() ...");
-		signInButton.click();
-		logger.log(Level.INFO, "starting to wait ...");
-		// Thread.sleep(3000);
-		waitModel(browser);
+		assertTrue("firstNameField.isDisplayed()",firstNameField.isDisplayed());
+		assertTrue("lastNameField.isDisplayed()",lastNameField.isDisplayed());
+		assertTrue("emailAddressField.isDisplayed()",emailAddressField.isDisplayed());
+		assertTrue("phoneNumberField.isDisplayed()",phoneNumberField.isDisplayed());
+		
+		assertTrue("dateOfBirthField.isDisplayed()",dateOfBirthField.isDisplayed());
+		assertTrue("cityField.isDisplayed()",cityField.isDisplayed());
+//		assertTrue("provinceIdField.isDisplayed()",provinceIdField.isDisplayed());
+		assertTrue("postalCodeField.isDisplayed()",postalCodeField.isDisplayed());
+		assertTrue("postalCodeToolTip.isDisplayed()",postalCodeToolTip.isDisplayed());
+		
+		assertTrue("multiFileUploadButton.isDisplayed()",multiFileUploadButton.isDisplayed());
+		assertTrue("submitFilesButton.isDisplayed()",submitFilesButton.isDisplayed());
+		
+		assertTrue("showCommentsLink.isDisplayed()",showCommentsLink.isDisplayed());
+		
+		assertTrue("submitButton.isDisplayed()",submitButton.isDisplayed());
+		assertTrue("editPreferencesButton.isDisplayed()",editPreferencesButton.isDisplayed());
+		
+		assertTrue("mojarraVersion.isDisplayed()",mojarraVersion.isDisplayed());
+		assertTrue("alloyVersion.isDisplayed()",alloyVersion.isDisplayed());
+		assertTrue("bridgeVersion.isDisplayed()",bridgeVersion.isDisplayed());
 
-		logger.log(Level.INFO,
-				"portletBody.getText() = " + portletBody.getText());
-		assertTrue("portletBody is displayed", portletBody.isDisplayed());
-		assertTrue("You are signed in",
-				portletBody.getText().contains("You are signed in"));
+		logger.log(Level.INFO, mojarraVersion.getText());
+		logger.log(Level.INFO, alloyVersion.getText());
+		logger.log(Level.INFO, bridgeVersion.getText());
+		
+		// signInButton.click();
+		// waitModel(browser);
+		
 	}
 
 }
