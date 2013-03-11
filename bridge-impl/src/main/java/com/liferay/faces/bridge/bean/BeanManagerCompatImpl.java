0783cd20cb589473e0e2bc1cf29ec570277fd4cc
Neil Griffin <neil.griffin.scm@gmail.com>
2013-03-11 16:50:57 -0400

    FACES-1494 Improve and enhance JSF1.2 <-> JSF2.0 compatibility layer (BeanManager)

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/bean/BeanManagerCompatImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.bean;
+
+import javax.faces.bean.ManagedBean;
+
+
+/**
+ * This class provides a compatibility layer that isolates differences between JSF1 and JSF2.
+ *
+ * @author  Neil Griffin
+ */
+public abstract class BeanManagerCompatImpl implements BeanManager {
+
+	/**
+	 * Determines whether or not the specified object is annotated as a JSF managed-bean.
+	 *
+	 * @param   obj  The object to check.
+	 *
+	 * @return  true if the specified object is annotated as a JSF managed-bean, otherwise false.
+	 */
+	protected boolean hasManagedBeanAnnotation(Object object) {
+
+		if ((object != null) && (object.getClass().getAnnotation(ManagedBean.class) != null)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
