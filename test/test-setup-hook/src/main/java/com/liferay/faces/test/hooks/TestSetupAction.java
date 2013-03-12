7994cc08907efd53c6b6b6e978bbf982bdfaee06
Neil Griffin <neil.griffin.scm@gmail.com>
2013-03-12 17:23:43 -0400

    FACES-1502 Introduce a compatibility layer for the test-setup-hook

--- test/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestSetupAction.java
+++ test/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestSetupAction.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -25,7 +25,6 @@ import org.apache.commons.logging.LogFactory;
 import com.liferay.portal.NoSuchGroupException;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.events.ActionException;
-import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.xml.Attribute;
 import com.liferay.portal.kernel.xml.Document;
@@ -47,34 +46,9 @@ import com.liferay.portal.service.UserLocalServiceUtil;
 /**
  * @author  Neil Griffin
  */
-public class TestSetupAction extends SimpleAction {
+public class TestSetupAction extends TestSetupCompatAction {
 
 	private static final Log logger = LogFactory.getLog(TestSetupAction.class);
-
-	private static final List<PortalPage> BRIDGE_DEMO_PAGES;
-	private static final List<PortalPage> PORTAL_DEMO_PAGES;
-
-	static {
-		BRIDGE_DEMO_PAGES = new ArrayList<PortalPage>();
-		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF1", "1_WAR_jsf1portlet_INSTANCE_"));
-		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF1-JSP", "1_WAR_jsf1jspportlet_INSTANCE_"));
-		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF1-EVENTS",
-				new String[] {
-					"customers_WAR_jsf1ipceventscustomersportlet", "bookings_WAR_jsf1ipceventsbookingsportlet"
-				}));
-		BRIDGE_DEMO_PAGES.add(new PortalPage("JSF1-PRP",
-				new String[] {
-					"customersPortlet_WAR_jsf1ipcpubrenderparamsportlet",
-					"bookingsPortlet_WAR_jsf1ipcpubrenderparamsportlet"
-				}));
-		BRIDGE_DEMO_PAGES.add(new PortalPage("ICE1", "1_WAR_icefaces1portlet_INSTANCE_"));
-		BRIDGE_DEMO_PAGES.add(new PortalPage("ICE1-IPC",
-				new String[] { "1_WAR_icefaces1ipcajaxpushportlet", "2_WAR_icefaces1ipcajaxpushportlet" }));
-	}
-
-	static {
-		PORTAL_DEMO_PAGES = new ArrayList<PortalPage>();
-	}
 
 	@Override
 	public void run(String[] companyIds) throws ActionException {
@@ -84,41 +58,18 @@ public class TestSetupAction extends SimpleAction {
 			for (String companyIdAsString : companyIds) {
 
 				long companyId = Long.parseLong(companyIdAsString);
+				setupPermissionChecker(companyId);
+
 				Company company = CompanyLocalServiceUtil.getCompanyById(companyId);
 				long userId = company.getDefaultUser().getUserId();
 				setupSites(companyId, userId);
 				setupUsers(companyId, userId);
+				clearPermissionChecker();
 			}
 		}
 		catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
-	}
-
-	protected void setupUsers(long companyId, long userId) throws Exception {
-		ServiceUtil.addUser(userId, companyId, "John", "Adams");
-		ServiceUtil.addUser(userId, companyId, "Samuel", "Adams");
-		ServiceUtil.addUser(userId, companyId, "Josiah", "Bartlett");
-		ServiceUtil.addUser(userId, companyId, "Carter", "Braxton");
-		ServiceUtil.addUser(userId, companyId, "Charles", "Carroll");
-		ServiceUtil.addUser(userId, companyId, "Benjamin", "Franklin");
-		ServiceUtil.addUser(userId, companyId, "Elbridge", "Gerry");
-		ServiceUtil.addUser(userId, companyId, "John", "Hancock");
-		ServiceUtil.addUser(userId, companyId, "Thomas", "Jefferson");
-		ServiceUtil.addUser(userId, companyId, "Francis", "Lewis");
-		ServiceUtil.addUser(userId, companyId, "Philip", "Livingston");
-		ServiceUtil.addUser(userId, companyId, "Thomas", "McKean");
-		ServiceUtil.addUser(userId, companyId, "Arthur", "Middleton");
-		ServiceUtil.addUser(userId, companyId, "John", "Penn");
-		ServiceUtil.addUser(userId, companyId, "George", "Read");
-		ServiceUtil.addUser(userId, companyId, "John", "Rutledge");
-		ServiceUtil.addUser(userId, companyId, "Roger", "Sherman");
-		ServiceUtil.addUser(userId, companyId, "Thomas", "Stone");
-		ServiceUtil.addUser(userId, companyId, "George", "Taylor");
-		ServiceUtil.addUser(userId, companyId, "George", "Washington");
-		ServiceUtil.addUser(userId, companyId, "John", "Witherspoon");
-		ServiceUtil.addUser(userId, companyId, "Oliver", "Wolcott");
-		ServiceUtil.addUser(userId, companyId, "George", "Wythe");
 	}
 
 	protected void addAllUsersToSite(long companyId, long groupId) throws Exception {
@@ -147,7 +98,7 @@ public class TestSetupAction extends SimpleAction {
 		long groupId = site.getGroupId();
 		addAllUsersToSite(companyId, groupId);
 
-		for (PortalPage portalPage : BRIDGE_DEMO_PAGES) {
+		for (PortalPage portalPage : TestPages.BRIDGE_DEMO_PAGES) {
 			setupPage(userId, groupId, portalPage);
 		}
 	}
@@ -177,12 +128,23 @@ public class TestSetupAction extends SimpleAction {
 			setupPage(userId, groupId, portalPage);
 		}
 
-		setupPage(userId, groupId, new PortalPage("Lifecycle Set", "chapter3TestslifecycleTestportlet_WAR_bridgetcklifecyclesetportlet"));
-		setupPage(userId, groupId, new PortalPage("Render Policy Always Delegate", "chapter3TestsrenderPolicyTestportlet_WAR_bridgetckrenderpolicy1portlet"));
-		setupPage(userId, groupId, new PortalPage("Render Policy Default", "chapter3TestsrenderPolicyTestportlet_WAR_bridgetckrenderpolicy2portlet"));
-		setupPage(userId, groupId, new PortalPage("Render Policy Never Delegate", "chapter3TestsrenderPolicyTestportlet_WAR_bridgetckrenderpolicy3portlet"));
-		setupPage(userId, groupId, new PortalPage("Render Response Wrapper", "chapter6_2_1TestsusesConfiguredRenderResponseWrapperTestportlet_WAR_bridgetckresponsewrapperportlet"));
-		setupPage(userId, groupId, new PortalPage("Resource Response Wrapper", "chapter6_2_1TestsusesConfiguredResourceResponseWrapperTestportlet_WAR_bridgetckresponsewrapperportlet"));
+		setupPage(userId, groupId,
+			new PortalPage("Lifecycle Set", "chapter3TestslifecycleTestportlet_WAR_bridgetcklifecyclesetportlet"));
+		setupPage(userId, groupId,
+			new PortalPage("Render Policy Always Delegate",
+				"chapter3TestsrenderPolicyTestportlet_WAR_bridgetckrenderpolicy1portlet"));
+		setupPage(userId, groupId,
+			new PortalPage("Render Policy Default",
+				"chapter3TestsrenderPolicyTestportlet_WAR_bridgetckrenderpolicy2portlet"));
+		setupPage(userId, groupId,
+			new PortalPage("Render Policy Never Delegate",
+				"chapter3TestsrenderPolicyTestportlet_WAR_bridgetckrenderpolicy3portlet"));
+		setupPage(userId, groupId,
+			new PortalPage("Render Response Wrapper",
+				"chapter6_2_1TestsusesConfiguredRenderResponseWrapperTestportlet_WAR_bridgetckresponsewrapperportlet"));
+		setupPage(userId, groupId,
+			new PortalPage("Resource Response Wrapper",
+				"chapter6_2_1TestsusesConfiguredResourceResponseWrapperTestportlet_WAR_bridgetckresponsewrapperportlet"));
 	}
 
 	protected void setupPage(long userId, long groupId, PortalPage portalPage) throws Exception {
@@ -201,7 +163,7 @@ public class TestSetupAction extends SimpleAction {
 				portletId = portletId + "ABCD";
 			}
 
-			layoutTypePortlet.setPortletIds("column-" + columnNumber, portletId);
+			addPortlet(layoutTypePortlet, userId, columnNumber, portletId);
 			columnNumber++;
 		}
 
@@ -215,7 +177,7 @@ public class TestSetupAction extends SimpleAction {
 		long groupId = site.getGroupId();
 		addAllUsersToSite(companyId, groupId);
 
-		for (PortalPage portalPage : PORTAL_DEMO_PAGES) {
+		for (PortalPage portalPage : TestPages.PORTAL_DEMO_PAGES) {
 			setupPage(userId, groupId, portalPage);
 		}
 	}
@@ -224,6 +186,32 @@ public class TestSetupAction extends SimpleAction {
 		setupBridgeDemosSite(companyId, userId);
 		setupPortalDemosSite(companyId, userId);
 		setupBridgeTCKSite(companyId, userId);
+	}
+
+	protected void setupUsers(long companyId, long userId) throws Exception {
+		ServiceUtil.addUser(userId, companyId, "John", "Adams");
+		ServiceUtil.addUser(userId, companyId, "Samuel", "Adams");
+		ServiceUtil.addUser(userId, companyId, "Josiah", "Bartlett");
+		ServiceUtil.addUser(userId, companyId, "Carter", "Braxton");
+		ServiceUtil.addUser(userId, companyId, "Charles", "Carroll");
+		ServiceUtil.addUser(userId, companyId, "Benjamin", "Franklin");
+		ServiceUtil.addUser(userId, companyId, "Elbridge", "Gerry");
+		ServiceUtil.addUser(userId, companyId, "John", "Hancock");
+		ServiceUtil.addUser(userId, companyId, "Thomas", "Jefferson");
+		ServiceUtil.addUser(userId, companyId, "Francis", "Lewis");
+		ServiceUtil.addUser(userId, companyId, "Philip", "Livingston");
+		ServiceUtil.addUser(userId, companyId, "Thomas", "McKean");
+		ServiceUtil.addUser(userId, companyId, "Arthur", "Middleton");
+		ServiceUtil.addUser(userId, companyId, "John", "Penn");
+		ServiceUtil.addUser(userId, companyId, "George", "Read");
+		ServiceUtil.addUser(userId, companyId, "John", "Rutledge");
+		ServiceUtil.addUser(userId, companyId, "Roger", "Sherman");
+		ServiceUtil.addUser(userId, companyId, "Thomas", "Stone");
+		ServiceUtil.addUser(userId, companyId, "George", "Taylor");
+		ServiceUtil.addUser(userId, companyId, "George", "Washington");
+		ServiceUtil.addUser(userId, companyId, "John", "Witherspoon");
+		ServiceUtil.addUser(userId, companyId, "Oliver", "Wolcott");
+		ServiceUtil.addUser(userId, companyId, "George", "Wythe");
 	}
 
 	protected Layout getPortalPageLayout(long userId, long groupId, String portalPageName) throws Exception {
---
+++ test/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestSetupCompatAction.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.test.hooks;
+
+import com.liferay.portal.kernel.events.SimpleAction;
+import com.liferay.portal.model.LayoutTypePortlet;
+import com.liferay.portal.security.permission.PermissionChecker;
+
+
+/**
+ * This class provides a compatibility layer that isolates differences between different versions of Liferay Portal. The
+ * purpose of this class is to minimize source code differences between different branches.
+ *
+ * @author  Neil Griffin
+ */
+public abstract class TestSetupCompatAction extends SimpleAction {
+
+	protected void addPortlet(LayoutTypePortlet layoutTypePortlet, long userId, int columnNumber, String portletId) {
+		layoutTypePortlet.setPortletIds("column-" + columnNumber, portletId);
+	}
+
+	/**
+	 * This method clears the {@link PermissionChecker} that was setup via the {@link #setupPermissionChecker(long)}
+	 * method.
+	 */
+	protected void clearPermissionChecker() {
+		// This is a no-op for Liferay Portal 6.1
+	}
+
+	/**
+	 * This method sets up the {@link PermissionChecker} {@link ThreadLocal} prior to performing additional test setup.
+	 */
+	protected void setupPermissionChecker(long companyId) {
+		// This is a no-op for Liferay Portal 6.1
+	}
+}
