f35a56b98496beb238d66419ea77bb3dba46997e
Vernon Singleton <vernon.singleton@liferay.com>
2013-03-15 22:30:32 -0400

    deeper testing passing with firefox

--- test/demos/bridge/jsf2-portlet/src/test/java/com/liferay/faces/test/Jsf2PortletTest.java
+++ test/demos/bridge/jsf2-portlet/src/test/java/com/liferay/faces/test/Jsf2PortletTest.java
@@ -48,9 +48,13 @@ public class Jsf2PortletTest {
 	@FindBy(xpath = "//input[@type='submit' and @value='Sign In']")
 	private WebElement signInButton;
 	@FindBy(xpath = "//div[contains(text(),'You are signed in as')]")
-	private WebElement portletBody;
-
-	// elements for Job Applicants
+	private WebElement signedInText;
+	
+	// <div class="portlet-body" id="aui_3_4_0_1_551"> <div id="A5601" class="jsf2-portlet liferay-faces-bridge-body">
+	@FindBy(xpath = "//form[@method='post']")
+	private WebElement formTag;
+	
+	// portlet topper and menu elements
 	@FindBy(xpath = "//header[@class='portlet-topper']/h1/span")
 	private WebElement portletDisplayName;
 	@FindBy(xpath = "//a[contains(@id,'jsf2portlet') and contains(@id,'menuButton')]")
@@ -58,26 +62,56 @@ public class Jsf2PortletTest {
 	@FindBy(xpath = "//a[contains(@id,'jsf2portlet') and contains(@id,'menu_preferences')]")
 	private WebElement menuPreferences;
 	
+	// preferences elements
+	@FindBy(xpath = "//input[contains(@id,':datePattern')]")
+	private WebElement datePatternField;
+	@FindBy(xpath = "//input[@type='submit' and @value='Reset']")
+	private WebElement resetButton;
+
+	// elements for Job Applicants
 	@FindBy(xpath = "//img[contains(@src,'liferay-logo.png')]")
 	private WebElement logo;
 	
 	@FindBy(xpath = "//input[contains(@id,':firstName')]")
 	private WebElement firstNameField;
+	@FindBy(xpath = "//input[contains(@id,':firstName')]/following-sibling::span[1]")
+	private WebElement firstNameFieldError;
+	
 	@FindBy(xpath = "//input[contains(@id,':lastName')]")
 	private WebElement lastNameField;
+	@FindBy(xpath = "//input[contains(@id,':lastName')]/following-sibling::span[1]")
+	private WebElement lastNameFieldError;
+	
 	@FindBy(xpath = "//input[contains(@id,':emailAddress')]")
 	private WebElement emailAddressField;
+	@FindBy(xpath = "//input[contains(@id,':emailAddress')]/following-sibling::span[1]")
+	private WebElement emailAddressFieldError;
+	
 	@FindBy(xpath = "//input[contains(@id,':phoneNumber')]")
 	private WebElement phoneNumberField;
+	@FindBy(xpath = "//input[contains(@id,':phoneNumber')]/following-sibling::span[1]")
+	private WebElement phoneNumberFieldError;
 	
 	@FindBy(xpath = "//input[contains(@id,':dateOfBirth')]")
 	private WebElement dateOfBirthField;
+	@FindBy(xpath = "//input[contains(@id,':dateOfBirth')]/following-sibling::span[1]")
+	private WebElement dateOfBirthFieldError;
+	
 	@FindBy(xpath = "//input[contains(@id,':city')]")
 	private WebElement cityField;
+	@FindBy(xpath = "//input[contains(@id,':city')]/following-sibling::span[1]")
+	private WebElement cityFieldError;
+	
 	@FindBy(xpath = "//select[contains(@id,':provinceId')]")
 	private WebElement provinceIdField;
+	@FindBy(xpath = "//select[contains(@id,':provinceId')]/following-sibling::span[1]")
+	private WebElement provinceIdFieldError;
+	
 	@FindBy(xpath = "//input[contains(@id,':postalCode')]")
 	private WebElement postalCodeField;
+	@FindBy(xpath = "//input[contains(@id,':postalCode')]/following-sibling::span[1]")
+	private WebElement postalCodeFieldError;
+	
 	@FindBy(xpath = "//img[contains(@title,'Type any of these ZIP codes')]")
 	private WebElement postalCodeToolTip;
 	
@@ -88,6 +122,10 @@ public class Jsf2PortletTest {
 	
 	@FindBy(xpath = "//a[contains(text(),'Show Comments')]")
 	private WebElement showCommentsLink;
+	@FindBy(xpath = "//a[contains(text(),'Hide Comments')]")
+	private WebElement hideCommentsLink;
+	@FindBy(xpath = "//textarea[contains(@id,':comments')]")
+	private WebElement comments;
 	
 	@FindBy(xpath = "//input[@type='submit' and @value='Submit']")
 	private WebElement submitButton;
@@ -105,11 +143,13 @@ public class Jsf2PortletTest {
 	private WebElement bridgeVersion;
 	
 	@Before
-	public void getNewSession() {
+	public void beforeEachTest() {
 		// Shut its dirty mouth
 		java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
-		browser.manage().deleteAllCookies();
-		logger.log(Level.INFO, "browser.manage().deleteAllCookies() ...");
+		
+//		browser.manage().deleteAllCookies();
+//		logger.log(Level.INFO, "browser.manage().deleteAllCookies() ...");
+		
 	}
 
 	public void signIn() throws Exception {
@@ -127,15 +167,15 @@ public class Jsf2PortletTest {
 		signInButton.click();
 		// waitModel(browser);
 		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle() + " after clicking the sign in button and waiting");
-		logger.log(Level.INFO, portletBody.getText());
-		// assertTrue("You are signed in", portletBody.getText().contains("You are signed in"));
+		logger.log(Level.INFO, signedInText.getText());
+		// assertTrue("You are signed in", signedInText.getText().contains("You are signed in"));
 		
 	}
 	
 	@Test
 	@RunAsClient
 	@InSequence(1)
-	public void jobApplicant() throws Exception {
+	public void jobApplicantFieldsRender() throws Exception {
 			
 		signIn();
 		url = "http://localhost:8080/group/bridge-demos/jsf2";
@@ -183,6 +223,250 @@ public class Jsf2PortletTest {
 		
 		// signInButton.click();
 		// waitModel(browser);
+		
+	}
+	
+	@Test
+	@RunAsClient
+	@InSequence(2)
+	public void validateEmail() throws Exception {
+		
+		int tags = 0;
+		int tagsWhileInvalid = 2;
+		int tagsWhileValid = 1;
+		
+		// checks an invalid email address
+		emailAddressField.sendKeys("test");
+		phoneNumberField.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "emailAddressField.getAttribute('value') = " + emailAddressField.getAttribute("value"));
+		logger.log(Level.INFO, "emailAddressFieldError.isDisplayed() = " + emailAddressFieldError.isDisplayed());
+		logger.log(Level.INFO, "emailAddressFieldError.getText() = " + emailAddressFieldError.getText());
+		tags = browser.findElements(By.xpath("//input[contains(@id,':emailAddress')]/../child::node()")).size();
+		logger.log(Level.INFO, "tags = " + tags);
+		assertTrue("Invalid e-mail address validation message displayed", 
+				emailAddressFieldError.getText().contains("Invalid e-mail address"));
+		assertTrue("one input tag plus one span tag = 2", tags == tagsWhileInvalid);
+		
+		// checks a valid email address
+		emailAddressField.clear();
+		Thread.sleep(500);
+		emailAddressField.sendKeys("test@liferay.com");
+		phoneNumberField.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "emailAddressField.getAttribute('value') = " + emailAddressField.getAttribute("value"));
+		tags = browser.findElements(By.xpath("//input[contains(@id,':emailAddress')]/../child::node()")).size();
+		logger.log(Level.INFO, "tags = " + tags);
+		assertTrue("one input tag plus zero span tags = 1", tags == tagsWhileValid);
+		
+	}
+	
+	@Test
+	@RunAsClient
+	@InSequence(3)
+	public void preferencesAreWorking() throws Exception {
+		
+		// test for both
+		int dateLengthAfterChange = 8;
+		int dateLengthAfterReset = 10;
+		
+		menuButton.click();
+		waitGui();
+		menuPreferences.click();
+		waitModel();
+		logger.log(Level.INFO, "datePatternField.getAttribute('value') = " + datePatternField.getAttribute("value"));
+		logger.log(Level.INFO, "resetButton.isDisplayed() = " + resetButton.isDisplayed());
+		// MM/dd/yyyy
+		datePatternField.clear();
+		datePatternField.sendKeys("MM/dd/yy");
+		submitButton.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "dateOfBirthField.getAttribute('value') = " + dateOfBirthField.getAttribute("value"));
+		logger.log(Level.INFO, "dateOfBirthField.getAttribute('value').length() = " + 
+				dateOfBirthField.getAttribute("value").length()
+			);
+		
+		assertTrue(
+				"date of birth has "+dateLengthAfterChange+" characters after changing preferences to MM/dd/yy", 
+				dateOfBirthField.getAttribute("value").length() == dateLengthAfterChange
+			);
+		
+		editPreferencesButton.click();
+		logger.log(Level.INFO, "editPreferencesButton.click() ...");
+		Thread.sleep(500);
+		resetButton.click();
+		logger.log(Level.INFO, "resetButton.click() ...");
+		Thread.sleep(500);
+		logger.log(Level.INFO, "dateOfBirthField.getAttribute('value') = " + dateOfBirthField.getAttribute("value"));
+		logger.log(Level.INFO, "dateOfBirthField.getAttribute('value').length() = " + dateOfBirthField.getAttribute("value").length());
+		
+		assertTrue(
+				"date of birth has "+dateLengthAfterReset+" characters after resetting preferences", 
+				dateOfBirthField.getAttribute("value").length() == dateLengthAfterReset
+			);
+	}
+	
+	@Test
+	@RunAsClient
+	@InSequence(4)
+	public void allFieldsRequiredUponSubmit() throws Exception {
+		
+		firstNameField.clear();
+		lastNameField.clear();
+		emailAddressField.clear();
+		phoneNumberField.clear();
+		dateOfBirthField.clear();
+		cityField.clear();
+		// provinceIdField.clear();
+//		logger.log(Level.INFO, "provinceIdField.getAttribute('value') = " + provinceIdField.getAttribute("value"));
+		postalCodeField.clear();
+		
+		submitButton.click();
+		waitAjax();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "firstNameFieldError.getText() = " + firstNameFieldError.getText());
+		logger.log(Level.INFO, "lastNameFieldError.getText() = " + lastNameFieldError.getText());
+		logger.log(Level.INFO, "emailAddressFieldError.getText() = " + emailAddressFieldError.getText());
+		logger.log(Level.INFO, "phoneNumberFieldError.getText() = " + phoneNumberFieldError.getText());
+		logger.log(Level.INFO, "dateOfBirthFieldError.getText() = " + dateOfBirthFieldError.getText());
+		logger.log(Level.INFO, "cityFieldError.getText() = " + cityFieldError.getText());
+		logger.log(Level.INFO, "provinceIdFieldError.getText() = " + provinceIdFieldError.getText());
+		logger.log(Level.INFO, "postalCodeFieldError.getText() = " + postalCodeFieldError.getText());
+		
+	}
+	
+	@Test
+	@RunAsClient
+	@InSequence(5)
+	public void cityAndStateAutoPopulate() throws Exception {
+		logger.log(Level.INFO, "before cityField.getAttribute('value') = " + cityField.getAttribute("value"));
+		logger.log(Level.INFO, "before provinceIdField.getAttribute('value') = " + provinceIdField.getAttribute("value"));
+		logger.log(Level.INFO, "before postalCodeField.getAttribute('value') = " + postalCodeField.getAttribute("value"));
+		assertTrue("cityField is empty", (cityField.getAttribute("value").length() == 0));
+		assertTrue("provinceIdField is empty", (provinceIdField.getAttribute("value").length() == 0));
+		assertTrue("postalCodeField is empty", (postalCodeField.getAttribute("value").length() == 0));
+		postalCodeField.sendKeys("32801");
+		phoneNumberField.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "after cityField.getAttribute('value') = " + cityField.getAttribute("value"));
+		logger.log(Level.INFO, "after provinceIdField.getAttribute('value') = " + provinceIdField.getAttribute("value"));
+		logger.log(Level.INFO, "after postalCodeField.getAttribute('value') = " + postalCodeField.getAttribute("value"));
+		assertTrue("cityField contains Orlando", cityField.getAttribute("value").contains("Orlando"));
+		assertTrue("provinceIdField contains 3", provinceIdField.getAttribute("value").contains("3"));
+		assertTrue("postalCodeField contains 32801", postalCodeField.getAttribute("value").contains("32801"));		
+	}
+	
+	@Test
+	@RunAsClient
+	@InSequence(6)
+	public void commentsFunctioning() throws Exception {
+		
+		String testing123 = "testing 1, 2, 3";
+		int tags = 0;
+		int tagsWhileHidden = 1;
+		int tagsWhileShowing = 2;
+		
+		showCommentsLink.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "hideCommentsLink.isDisplayed() = " + hideCommentsLink.isDisplayed());
+		assertTrue("hideCommentsLink is displayed", hideCommentsLink.isDisplayed());
+		logger.log(Level.INFO, "comments.isDisplayed() = " + comments.isDisplayed());
+		assertTrue("comments textarea is displayed", comments.isDisplayed());
+		tags = browser.findElements(By.xpath("//a[contains(text(),'Hide Comments')]/../../child::node()")).size();
+		logger.log(Level.INFO, "tags = " + tags);
+		assertTrue("tag for Hide and tag for textarea are showing", tags == tagsWhileShowing);
+		comments.sendKeys(testing123);
+		phoneNumberField.click();
+		Thread.sleep(500);
+		hideCommentsLink.click();
+		Thread.sleep(500);
+		tags = browser.findElements(By.xpath("//a[contains(text(),'Show Comments')]/../../child::node()")).size();
+		logger.log(Level.INFO, "tags = " + tags);
+		assertTrue("no textarea is showing", tags == tagsWhileHidden);
+		logger.log(Level.INFO, "showCommentsLink.isDisplayed() = " + showCommentsLink.isDisplayed());
+		showCommentsLink.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "after hide and show comments.getAttribute('value') = " + comments.getAttribute("value"));
+		assertTrue("comments are still there after hide and show", testing123.equals(comments.getAttribute("value")));
+	}
+	
+	@Test
+	@RunAsClient
+	@InSequence(7)
+	public void dateValidation() throws Exception {
+		
+		int tags = 0;
+		int tagsWhileInvalid = 2;
+		int tagsWhileValid = 1;
+		// "Value is required";
+		// "Invalid date format";
+		
+		// checks an invalid dateOfBirth
+		dateOfBirthField.clear();
+		Thread.sleep(500);
+		dateOfBirthField.sendKeys("12/34/5678");
+		Thread.sleep(500);
+		submitButton.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "dateOfBirthField.getAttribute('value') = " + dateOfBirthField.getAttribute("value"));
+		logger.log(Level.INFO, "dateOfBirthFieldError.isDisplayed() = " + dateOfBirthFieldError.isDisplayed());
+		logger.log(Level.INFO, "dateOfBirthFieldError.getText() = " + dateOfBirthFieldError.getText());
+		tags = browser.findElements(By.xpath("//input[contains(@id,':dateOfBirth')]/../child::node()")).size();
+		logger.log(Level.INFO, "tags = " + tags);
+		assertTrue("Invalid dateOfBirthField validation message displayed", 
+				dateOfBirthFieldError.getText().contains("Invalid date format"));
+		assertTrue("one input tag plus one span tag = 2", tags == tagsWhileInvalid);
+		
+		// checks with no dateOfBirth
+		dateOfBirthField.clear();
+		Thread.sleep(500);
+		phoneNumberField.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "dateOfBirthField.getAttribute('value') = " + dateOfBirthField.getAttribute("value"));
+		logger.log(Level.INFO, "dateOfBirthFieldError.isDisplayed() = " + dateOfBirthFieldError.isDisplayed());
+		logger.log(Level.INFO, "dateOfBirthFieldError.getText() = " + dateOfBirthFieldError.getText());
+		tags = browser.findElements(By.xpath("//input[contains(@id,':dateOfBirth')]/../child::node()")).size();
+		logger.log(Level.INFO, "tags = " + tags);
+		assertTrue("Value is required for dateOfBirthField message displayed", 
+				dateOfBirthFieldError.getText().contains("Value is required"));
+		assertTrue("one input tag plus one span tag = 2", tags == tagsWhileInvalid);
+		
+		// checks a valid dateOfBirth
+		dateOfBirthField.clear();
+		Thread.sleep(500);
+		dateOfBirthField.sendKeys("01/02/3456");
+		Thread.sleep(500);
+		phoneNumberField.click();
+		Thread.sleep(500);
+		logger.log(Level.INFO, "dateOfBirthField.getAttribute('value') = " + dateOfBirthField.getAttribute("value"));
+		tags = browser.findElements(By.xpath("//input[contains(@id,':dateOfBirth')]/../child::node()")).size();
+		logger.log(Level.INFO, "tags = " + tags);
+		assertTrue("one input tag plus zero span tags = 1", tags == tagsWhileValid);
+		
+	}
+	
+	@Test
+	@RunAsClient
+	@InSequence(8)
+	public void submitAndValidate() throws Exception {
+		
+		dateOfBirthField.clear();
+		emailAddressField.clear();
+		Thread.sleep(500);
+		
+		firstNameField.sendKeys("David");
+		lastNameField.sendKeys("Samuel");
+		emailAddressField.sendKeys("no_need@just.pray");
+		phoneNumberField.sendKeys("(way) too-good");
+		dateOfBirthField.sendKeys("01/02/3456");
+		postalCodeField.sendKeys("32801");
+		Thread.sleep(500);
+		comments.sendKeys("If as one people speaking the same language, they have begun to do this ...");
+		submitButton.click();
+		Thread.sleep(500);
+		
+		logger.log(Level.INFO, "formTag.getText() = " + formTag.getText());
+		assertTrue("Dear David", formTag.getText().contains("Dear David"));
 		
 	}
 
