e2ef96db3ce8e4ccdd6819e0cb47da5da1b834c0
Vernon Singleton <vernon.singleton@liferay.com>
2013-03-22 01:57:39 -0400

    portlet specific declarations and initialization

---
+++ test/demos/bridge/icefaces3-portlet/src/test/java/com/liferay/faces/test/Icefaces3.java
@@ -0,0 +1,190 @@
+package com.liferay.faces.test;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.Graphene;
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+// import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.jboss.arquillian.graphene.Graphene.waitGui;
+import static org.jboss.arquillian.graphene.Graphene.waitAjax;
+import static org.jboss.arquillian.graphene.Graphene.waitModel;
+
+public class Icefaces3 {
+	
+	private final static Logger logger = Logger.getLogger(Icefaces3.class.getName());
+	
+	// @ArquillianResource
+	// URL portalURL;
+	String signInUrl = "http://localhost:8080/web/guest/signin";
+	String url = "http://localhost:8080/group/bridge-demos/ice3";
+	
+	WebDriver browser;
+	
+	// elements for logging in
+	private static final String emailFieldXpath = "//input[contains(@id,':handle')]";
+	@FindBy(xpath = emailFieldXpath)
+	private WebElement emailField;
+	private static final String passwordFieldXpath = "//input[contains(@id,':password')]";
+	@FindBy(xpath = passwordFieldXpath)
+	private WebElement passwordField;
+	private static final String signInButtonXpath = "//input[@type='submit' and @value='Sign In']";
+	@FindBy(xpath = signInButtonXpath)
+	private WebElement signInButton;
+	private static final String signedInTextXpath = "//div[contains(text(),'You are signed in as')]";
+	@FindBy(xpath = signedInTextXpath)
+	private WebElement signedInText;
+	
+	// form tag found after submitting
+	private static final String formTagXpath = "//form[@method='post']";
+	@FindBy(xpath = formTagXpath)
+	private WebElement formTag;
+	
+	// portlet topper and menu elements
+	private static final String portletDisplayNameXpath = "//header[@class='portlet-topper']/h1/span";
+	@FindBy(xpath = portletDisplayNameXpath)
+	private WebElement portletDisplayName;
+	private static final String menuButtonXpath = "//a[contains(@id,'menuButton')]";
+	@FindBy(xpath = menuButtonXpath)
+	private WebElement menuButton;
+	private static final String menuPreferencesXpath = "//a[contains(@id,'menu_preferences')]";
+	@FindBy(xpath = menuPreferencesXpath)
+	private WebElement menuPreferences;
+	
+	// preferences elements
+	private static final String datePatternFieldXpath = "//input[contains(@id,':datePattern')]";
+	@FindBy(xpath = datePatternFieldXpath)
+	private WebElement datePatternField;
+	private static final String resetButtonXpath = "//input[@type='submit' and @value='Reset']";
+	@FindBy(xpath = resetButtonXpath)
+	private WebElement resetButton;
+	
+	// elements for Job Applicants
+	private static final String logoXpath = "//img[contains(@src,'liferay-logo.png')]";
+	@FindBy(xpath = logoXpath)
+	private WebElement logo;
+	
+	private static final String firstNameFieldXpath = "//input[contains(@id,':firstName_input')]";
+	@FindBy(xpath = firstNameFieldXpath)
+	private WebElement firstNameField;
+	private static final String firstNameFieldErrorXpath = "//input[contains(@id,':firstName_input')]/../../following-sibling::*[1]/child::node()";
+	@FindBy(xpath = firstNameFieldErrorXpath)
+	private WebElement firstNameFieldError;
+	
+	private static final String lastNameFieldXpath = "//input[contains(@id,':lastName_input')]";
+	@FindBy(xpath = lastNameFieldXpath)
+	private WebElement lastNameField;
+	private static final String lastNameFieldErrorXpath = "//input[contains(@id,':lastName_input')]/../../following-sibling::*[1]/child::node()";
+	@FindBy(xpath = lastNameFieldErrorXpath)
+	private WebElement lastNameFieldError;
+	
+	private static final String emailAddressFieldXpath = "//input[contains(@id,':emailAddress_input')]";
+	@FindBy(xpath = emailAddressFieldXpath)
+	private WebElement emailAddressField;
+	private static final String emailAddressFieldErrorXpath = "//input[contains(@id,':emailAddress_input')]/../../following-sibling::*[1]/child::node()";
+	@FindBy(xpath = emailAddressFieldErrorXpath)
+	private WebElement emailAddressFieldError;
+	
+	private static final String phoneNumberFieldXpath = "//input[contains(@id,':phoneNumber_input')]";
+	@FindBy(xpath = phoneNumberFieldXpath)
+	private WebElement phoneNumberField;
+	private static final String phoneNumberFieldErrorXpath = "//input[contains(@id,':phoneNumber_input')]/../../following-sibling::*[1]/child::node()";
+	@FindBy(xpath = phoneNumberFieldErrorXpath)
+	private WebElement phoneNumberFieldError;
+	
+	private static final String dateOfBirthFieldXpath = "//input[contains(@id,':dateOfBirth_input')]";
+	@FindBy(xpath = dateOfBirthFieldXpath)
+	private WebElement dateOfBirthField;
+	private static final String dateOfBirthFieldErrorXpath = "//input[contains(@id,':dateOfBirth_input')]/../following-sibling::*[1]/child::node()";
+	@FindBy(xpath = dateOfBirthFieldErrorXpath)
+	private WebElement dateOfBirthFieldError;
+	
+	private static final String cityFieldXpath = "//input[contains(@id,':city_input')]";
+	@FindBy(xpath = cityFieldXpath)
+	private WebElement cityField;
+	private static final String cityFieldErrorXpath = "//input[contains(@id,':city_input')]/../../following-sibling::*[1]";
+	@FindBy(xpath = cityFieldErrorXpath)
+	private WebElement cityFieldError;
+	
+	private static final String provinceIdFieldXpath = "//select[contains(@id,':provinceId')]";
+	@FindBy(xpath = provinceIdFieldXpath)
+	private WebElement provinceIdField;
+	private static final String provinceIdFieldErrorXpath = "//select[contains(@id,':provinceId')]/following-sibling::*[1]";
+	@FindBy(xpath = provinceIdFieldErrorXpath)
+	private WebElement provinceIdFieldError;
+	
+	private static final String postalCodeFieldXpath = "//input[contains(@id,':postalCode_input')]";
+	@FindBy(xpath = postalCodeFieldXpath)
+	private WebElement postalCodeField;
+	private static final String postalCodeFieldErrorXpath = "//input[contains(@id,':postalCode_input')]/../../following-sibling::*[1]/following-sibling::*[1]";
+	@FindBy(xpath = postalCodeFieldErrorXpath)
+	private WebElement postalCodeFieldError;
+	
+	private static final String postalCodeToolTipXpath = "//img[contains(@title,'Type any of these ZIP codes')]";
+	@FindBy(xpath = postalCodeToolTipXpath)
+	private WebElement postalCodeToolTip;
+	
+	private static final String showCommentsLinkXpath = "//a[contains(text(),'Show Comments')]";
+	@FindBy(xpath = showCommentsLinkXpath)
+	private WebElement showCommentsLink;
+	private static final String hideCommentsLinkXpath = "//a[contains(text(),'Hide Comments')]";
+	@FindBy(xpath = hideCommentsLinkXpath)
+	private WebElement hideCommentsLink;
+	private static final String commentsXpath = "//textarea[contains(@id,':comments')]";
+	@FindBy(xpath = commentsXpath)
+	private WebElement comments;
+	
+	private static final String submitButtonXpath = "//input[@type='submit' and @value='Submit']";
+	@FindBy(xpath = submitButtonXpath)
+	private WebElement submitButton;
+	private static final String preferencesSubmitButtonXpath = "//input[@type='submit' and @value='Submit']";
+	@FindBy(xpath = preferencesSubmitButtonXpath)
+	private WebElement preferencesSubmitButton;
+	private static final String editPreferencesButtonXpath = "//input[@type='submit' and @value='Edit Preferences']";
+	@FindBy(xpath = editPreferencesButtonXpath)
+	private WebElement editPreferencesButton;
+	private static final String returnLinkXpath = "//a[contains(text(),'Return to Full Page')]";
+	@FindBy(xpath = returnLinkXpath)
+	private WebElement returnLink;
+	
+	private static final String mojarraVersionXpath = "//*[contains(text(),'Mojarra')]";
+	@FindBy(xpath = mojarraVersionXpath)
+	private WebElement mojarraVersion;
+	private static final String componentLibraryVersionXpath = "//*[contains(text(),'ICEfaces ')]";
+	@FindBy(xpath = componentLibraryVersionXpath)
+	private WebElement componentLibraryVersion;
+	private static final String alloyVersionXpath = "//*[contains(text(),'Liferay Faces Alloy')]";
+	@FindBy(xpath = alloyVersionXpath)
+	private WebElement alloyVersion;
+	private static final String bridgeVersionXpath = "//*[contains(text(),'Liferay Faces Bridge')]";
+	@FindBy(xpath = bridgeVersionXpath)
+	private WebElement bridgeVersion;
+	
+	// xpath for specific tests
+	private static final String dateValidationXpath = "//input[contains(@id,':dateOfBirth')]/../following-sibling::*[1]/child::node()";
+	int dateValidationXpathModifier = 0;
+	
+}
