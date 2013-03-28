5b2539967d1fa7b2fb758033532481d249cb7969
Neil Griffin <neil.griffin.scm@gmail.com>
2013-03-28 14:46:32 -0400

    FACES-1521 Inconsistency between bridge demos in model bean

--- demos/bridge/icefaces1-portlet/src/main/java/com/liferay/faces/demos/bean/ApplicantModelBean.java
+++ demos/bridge/icefaces1-portlet/src/main/java/com/liferay/faces/demos/bean/ApplicantModelBean.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -15,6 +15,7 @@ package com.liferay.faces.demos.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -49,6 +50,7 @@ public class ApplicantModelBean implements Serializable {
 
 	public ApplicantModelBean() {
 		clearProperties();
+		this.dateOfBirth = Calendar.getInstance().getTime();
 	}
 
 	public void clearProperties() {
