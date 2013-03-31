052d3b9ac579e6a62fe200eb38473fe2bfbebc68
Vernon Singleton <vernon.singleton@liferay.com>
2013-03-31 15:53:05 -0400

    brought icefaces-compat into conformity with other PortletPreferencesBackingBean(s)

--- demos/bridge/icefaces3-compat-portlet/src/main/java/com/liferay/faces/demos/bean/PortletPreferencesBackingBean.java
+++ demos/bridge/icefaces3-compat-portlet/src/main/java/com/liferay/faces/demos/bean/PortletPreferencesBackingBean.java
@@ -19,9 +19,13 @@ import java.util.Map;
 import javax.el.ELResolver;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletMode;
 import javax.portlet.PortletPreferences;
 import javax.portlet.PortletRequest;
+import javax.portlet.WindowState;
 import javax.portlet.faces.preference.Preference;
 
 import com.liferay.faces.demos.util.FacesMessageUtil;
@@ -35,13 +39,14 @@ import com.liferay.faces.demos.util.FacesMessageUtil;
 public class PortletPreferencesBackingBean {
 
 	/**
-	 * Resets/restores the values in the portletPreferences.xhtml Facelet composition with portlet preference default
-	 * values.
+	 * Resets/restores the values in the portletPreferences.xhtml Facelet
+	 * composition with portlet preference default values.
 	 */
 	public void reset() {
 
 		FacesContext facesContext = FacesContext.getCurrentInstance();
-		PortletRequest portletRequest = (PortletRequest) facesContext.getExternalContext().getRequest();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		PortletRequest portletRequest = (PortletRequest) externalContext.getRequest();
 		PortletPreferences portletPreferences = portletRequest.getPreferences();
 
 		try {
@@ -54,6 +59,11 @@ public class PortletPreferencesBackingBean {
 
 			portletPreferences.store();
 
+			// Switch the portlet mode back to VIEW.
+			ActionResponse actionResponse = (ActionResponse) externalContext.getResponse();
+			actionResponse.setPortletMode(PortletMode.VIEW);
+			actionResponse.setWindowState(WindowState.NORMAL);
+
 			FacesMessageUtil.addGlobalSuccessInfoMessage(facesContext);
 		}
 		catch (Exception e) {
@@ -63,16 +73,22 @@ public class PortletPreferencesBackingBean {
 	}
 
 	/**
-	 * Saves the values in the portletPreferences.xhtml Facelet composition as portlet preferences.
+	 * Saves the values in the portletPreferences.xhtml Facelet composition as
+	 * portlet preferences.
 	 */
 	public void submit() {
 
-		// The JSR 329 specification defines an EL variable named mutablePortletPreferencesValues that is being used in
-		// the portletPreferences.xhtml Facelet composition. This object is of type Map<String, Preference> and is
-		// designed to be a model managed-bean (in a sense) that contain preference values. However the only way to
-		// access this from a Java class is to evaluate an EL expression (effectively self-injecting) the map into
+		// The JSR 329 specification defines an EL variable named
+		// mutablePortletPreferencesValues that is being used in
+		// the portletPreferences.xhtml Facelet composition. This object is of
+		// type Map<String, Preference> and is
+		// designed to be a model managed-bean (in a sense) that contain
+		// preference values. However the only way to
+		// access this from a Java class is to evaluate an EL expression
+		// (effectively self-injecting) the map into
 		// this backing bean.
 		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
 		String elExpression = "mutablePortletPreferencesValues";
 		ELResolver elResolver = facesContext.getApplication().getELResolver();
 		@SuppressWarnings("unchecked")
@@ -80,7 +96,7 @@ public class PortletPreferencesBackingBean {
 				facesContext.getELContext(), null, elExpression);
 
 		// Get a list of portlet preference names.
-		PortletRequest portletRequest = (PortletRequest) facesContext.getExternalContext().getRequest();
+		PortletRequest portletRequest = (PortletRequest) externalContext.getRequest();
 		PortletPreferences portletPreferences = portletRequest.getPreferences();
 		Enumeration<String> preferenceNames = portletPreferences.getNames();
 
@@ -101,6 +117,11 @@ public class PortletPreferencesBackingBean {
 
 			// Save the preference values.
 			portletPreferences.store();
+
+			// Switch the portlet mode back to VIEW.
+			ActionResponse actionResponse = (ActionResponse) externalContext.getResponse();
+			actionResponse.setPortletMode(PortletMode.VIEW);
+			actionResponse.setWindowState(WindowState.NORMAL);
 
 			// Report a successful message back to the user as feedback.
 			FacesMessageUtil.addGlobalSuccessInfoMessage(facesContext);
