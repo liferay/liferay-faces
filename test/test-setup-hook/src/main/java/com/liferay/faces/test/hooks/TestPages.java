15bf98c401b3ae6c552b15a7f37e3dbe9274e0db
Neil Griffin <neil.griffin.scm@gmail.com>
2013-04-06 17:16:00 -0400

    FACES-1535 Add issue portlets to test-setup-hook

--- test/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestPages.java
+++ test/test-setup-hook/src/main/java/com/liferay/faces/test/hooks/TestPages.java
@@ -25,7 +25,9 @@ import java.util.List;
 public class TestPages {
 
 	public static final List<PortalPage> BRIDGE_DEMO_PAGES;
+	public static final List<PortalPage> BRIDGE_ISSUE_PAGES;
 	public static final List<PortalPage> PORTAL_DEMO_PAGES;
+	public static final List<PortalPage> PORTAL_ISSUE_PAGES;
 
 	static {
 		BRIDGE_DEMO_PAGES = new ArrayList<PortalPage>();
@@ -46,6 +48,19 @@ public class TestPages {
 	}
 
 	static {
+		BRIDGE_ISSUE_PAGES = new ArrayList<PortalPage>();
+		BRIDGE_ISSUE_PAGES.add(new PortalPage("FACES-224", "1_WAR_FACES224portlet"));
+		BRIDGE_ISSUE_PAGES.add(new PortalPage("FACES-1470", "1_WAR_FACES1470portlet"));
+		BRIDGE_ISSUE_PAGES.add(new PortalPage("FACES-1478", "1_WAR_FACES1478portlet"));
+	}
+
+	static {
 		PORTAL_DEMO_PAGES = new ArrayList<PortalPage>();
+	}
+
+	static {
+		PORTAL_ISSUE_PAGES = new ArrayList<PortalPage>();
+		PORTAL_ISSUE_PAGES.add(new PortalPage("FACES-1427", "1_WAR_FACES1427portlet"));
+		PORTAL_ISSUE_PAGES.add(new PortalPage("FACES-1439", "1_WAR_FACES1439portlet"));
 	}
 }
