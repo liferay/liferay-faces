0b08514650de65e4856bee4a3f46722da1537b60
Neil Griffin <neil.griffin.scm@gmail.com>
2013-05-02 18:05:39 -0400

    FACES-1545 selenium tests for several demo and issue portlets FACES-1549 Add CDI portlet test, jsf2-events tests, and new signin page url for all tests

---
+++ test/integration/demos/bridge/jsf2-events/src/test/java/com/liferay/faces/test/Jsf2EventsTest.java
@@ -0,0 +1,264 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.test;
+
+import java.util.logging.Level;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.liferay.faces.test.util.TesterBase;
+
+/**
+ * @author  Liferay Faces Team
+ */
+@RunWith(Arquillian.class)
+public class Jsf2EventsTest extends TesterBase {
+
+	// portlet topper for customer
+	private static final String customerPortletDisplayNameXpath = "(//header[@class='portlet-topper']/h1/span)[1]";
+	private static final String briansInputXpath =
+		"//input[@type='image']/../following-sibling::td[1][contains(text(),'1')]/../td[1]/input";
+	private static final String briansFirstNameXpath =
+		"//input[@type='image']/../following-sibling::td[1][contains(text(),'1')]/following-sibling::*[1]";
+	private static final String briansLastNameXpath =
+		"//input[@type='image']/../following-sibling::td[1][contains(text(),'1')]/following-sibling::*[1]/following-sibling::*[1]";
+	private static final String lizsInputXpath =
+		"//input[@type='image']/../following-sibling::td[1][contains(text(),'2')]/../td[1]/input";
+	private static final String lizsFirstNameXpath =
+		"//input[@type='image']/../following-sibling::td[1][contains(text(),'2')]/following-sibling::*[1]";
+	private static final String lizsLastNameXpath =
+		"//input[@type='image']/../following-sibling::td[1][contains(text(),'2')]/following-sibling::*[1]/following-sibling::*[1]";
+	
+	// portlet topper for bookings
+	private static final String bookingsPortletDisplayNameXpath = "(//header[@class='portlet-topper']/h1/span)[2]";
+	// <input id="A8622:f1:firstName" type="text" name="A8622:f1:firstName" value="Brian" class="focus">
+	private static final String firstNameXpath = "//input[contains(@id,':firstName')]";
+	// <input id="A8622:f1:firstName" type="text" name="A8622:f1:firstName" value="Brian" class="focus">
+	private static final String lastNameXpath = "//input[contains(@id,':lastName')]";
+	private static final String bookingTypeIdXpath = "(//select[contains(@id,':bookingTypeId')])[1]";
+	private static final String startDateXpath = "(//input[contains(@id,':startDate')])[1]";
+	private static final String finishDateXpath = "(//input[contains(@id,':finishDate')])[1]";
+	// <input type="submit" name="A8622:f1:j_idt28" value="Submit" id="aui_3_4_0_1_2331">
+	private static final String submitXpath = "//input[@type='submit' and @value='Submit']";
+
+	static final String url = "http://localhost:8080/group/bridge-demos/jsf2-events";
+
+	@FindBy(xpath = customerPortletDisplayNameXpath)
+	private WebElement customerPortletDisplayName;
+	@FindBy(xpath = briansInputXpath)
+	private WebElement briansInput;
+	@FindBy(xpath = briansFirstNameXpath)
+	private WebElement briansFirstName;
+	@FindBy(xpath = briansLastNameXpath)
+	private WebElement briansLastName;
+	@FindBy(xpath = lizsInputXpath)
+	private WebElement lizsInput;
+	@FindBy(xpath = lizsFirstNameXpath)
+	private WebElement lizsFirstName;
+	@FindBy(xpath = lizsLastNameXpath)
+	private WebElement lizsLastName;
+	@FindBy(xpath = bookingsPortletDisplayNameXpath)
+	private WebElement bookingsPortletDisplayName;
+	@FindBy(xpath = firstNameXpath)
+	private WebElement firstName;
+	@FindBy(xpath = lastNameXpath)
+	private WebElement lastName;
+	@FindBy(xpath = bookingTypeIdXpath)
+	private WebElement bookingTypeId;
+	@FindBy(xpath = startDateXpath)
+	private WebElement startDate;
+	@FindBy(xpath = finishDateXpath)
+	private WebElement finishDate;
+	@FindBy(xpath = submitXpath)
+	private WebElement submit;
+
+	@Test
+	@RunAsClient
+	@InSequence(1000)
+	public void renderViewMode() throws Exception {
+
+		signIn();
+		logger.log(Level.INFO, "browser.navigate().to(" + url + ")");
+		browser.navigate().to(url);
+		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+		logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
+		logger.log(Level.INFO, "customerPortletDisplayName.getText() = " + customerPortletDisplayName.getText());
+		logger.log(Level.INFO, "bookingsPortletDisplayName.getText() = " + bookingsPortletDisplayName.getText());
+
+		assertTrue("customerPortletDisplayName.isDisplayed()", customerPortletDisplayName.isDisplayed());
+		assertTrue("bookingsPortletDisplayName.isDisplayed()", bookingsPortletDisplayName.isDisplayed());
+
+		logger.log(Level.INFO,
+			"browser.findElements(By.xpath(portletDisplayNameXpath)).size() = " +
+			browser.findElements(By.xpath(customerPortletDisplayNameXpath)).size());
+		logger.log(Level.INFO,
+			"browser.findElements(By.xpath(briansInputXpath)).size() = " +
+			browser.findElements(By.xpath(briansInputXpath)).size());
+		logger.log(Level.INFO,
+			"browser.findElements(By.xpath(lizsInputXpath)).size() = " +
+			browser.findElements(By.xpath(lizsInputXpath)).size());
+
+		logger.log(Level.INFO, "briansFirstName.getText() = " + briansFirstName.getText());
+		logger.log(Level.INFO, "briansLastName.getText() = " + briansLastName.getText());
+
+		logger.log(Level.INFO, "lizsFirstName.getText() = " + lizsFirstName.getText());
+		logger.log(Level.INFO, "lizsLastName.getText() = " + lizsLastName.getText());
+
+	}
+
+	@Test
+	@RunAsClient
+	@InSequence(2000)
+	public void checkBriansBookings() throws Exception {
+
+		briansInput.click();
+		Thread.sleep(500);
+
+		logger.log(Level.INFO, "firstName.getAttribute(value) = " + firstName.getAttribute("value"));
+		logger.log(Level.INFO, "lastName.getAttribute(value) = " + lastName.getAttribute("value"));
+		logger.log(Level.INFO, "startDate.getAttribute(value) = " + startDate.getAttribute("value"));
+		logger.log(Level.INFO, "finishDate.getAttribute(value) = " + finishDate.getAttribute("value"));
+
+		assertTrue("customer first name should be the same in the bookings but it is '" + briansFirstName.getText() +
+			"' in the customer module," + " and '" + firstName.getAttribute("value") + "' in bookings",
+			firstName.getAttribute("value").contains(briansFirstName.getText()));
+		assertTrue("customer last name should be the same in the bookings but it is '" + briansLastName.getText() +
+			"' in the customer module," + " and '" + lastName.getAttribute("value") + "' in bookings",
+			lastName.getAttribute("value").contains(briansLastName.getText()));
+
+	}
+
+	@Test
+	@RunAsClient
+	@InSequence(3000)
+	public void changeBriansBookings() throws Exception {
+
+		logger.log(Level.INFO, "lastName.clear() ...");
+		lastName.clear();
+		logger.log(Level.INFO, "lastName.sendKeys('Greeny') ...");
+		lastName.sendKeys("Greeny");
+		logger.log(Level.INFO, "finishDate.clear() ...");
+		finishDate.clear();
+		logger.log(Level.INFO, "finishDate.sendKeys('04/20/2099') ...");
+		finishDate.sendKeys("04/20/2099");
+		logger.log(Level.INFO, "submit.click() ...");
+		submit.click();
+		
+		waitForElement(briansLastNameXpath);
+		
+		logger.log(Level.INFO, "briansLastName.getText() = " + briansLastName.getText());
+		logger.log(Level.INFO, "lastName.getAttribute(value) = " + lastName.getAttribute("value"));
+		logger.log(Level.INFO, "finishDate.getAttribute(value) = " + finishDate.getAttribute("value"));
+
+		assertTrue("customer first name should be the same in the bookings but it is '" + briansLastName.getText() +
+			"' in the customer module," + " and '" + lastName.getAttribute("value") + "' in bookings",
+			lastName.getAttribute("value").contains(briansLastName.getText()));
+
+		assertTrue("Customer last name should contain 'Greeny', but it contains '" + briansLastName.getText() + "'",
+			briansLastName.getText().contains("Greeny"));
+		assertTrue("Bookings last name should contain 'Greeny', but it contains '" + lastName.getAttribute("value") +
+			"'", lastName.getAttribute("value").contains("Greeny"));
+	}
+
+	@Test
+	@RunAsClient
+	@InSequence(4000)
+	public void checkLizsBookings() throws Exception {
+
+		lizsInput.click();
+		
+		waitForElement(lizsFirstNameXpath);
+
+		logger.log(Level.INFO, "firstName.getAttribute(value) = " + firstName.getAttribute("value"));
+		logger.log(Level.INFO, "lastName.getAttribute(value) = " + lastName.getAttribute("value"));
+		logger.log(Level.INFO, "startDate.getAttribute(value) = " + startDate.getAttribute("value"));
+		logger.log(Level.INFO, "finishDate.getAttribute(value) = " + finishDate.getAttribute("value"));
+
+		assertTrue("customer first name should be the same in the bookings but it is '" + lizsFirstName.getText() +
+			"' in the customer module," + " and '" + firstName.getAttribute("value") + "' in bookings",
+			firstName.getAttribute("value").contains(lizsFirstName.getText()));
+		assertTrue("customer last name should be the same in the bookings but it is '" + lizsLastName.getText() +
+			"' in the customer module," + " and '" + lastName.getAttribute("value") + "' in bookings",
+			lastName.getAttribute("value").contains(lizsLastName.getText()));
+
+	}
+
+	@Test
+	@RunAsClient
+	@InSequence(5000)
+	public void changeLizsBookings() throws Exception {
+
+		logger.log(Level.INFO, "firstName.clear() ...");
+		firstName.clear();
+		logger.log(Level.INFO, "firstName.sendKeys('Lizzy') ...");
+		firstName.sendKeys("Lizzy");
+		finishDate.clear();
+		finishDate.sendKeys("12/25/2999");
+		submit.click();
+		
+		waitForElement(lizsFirstNameXpath);
+		
+		logger.log(Level.INFO, "lizsFirstName.getText() = " + lizsFirstName.getText());
+		logger.log(Level.INFO, "firstName.getAttribute(value) = " + firstName.getAttribute("value"));
+		logger.log(Level.INFO, "finishDate.getAttribute(value) = " + finishDate.getAttribute("value"));
+
+		assertTrue("customer first name should be the same in the bookings but it is '" + lizsFirstName.getText() +
+			"' in the customer module," + " and '" + firstName.getAttribute("value") + "' in bookings",
+			firstName.getAttribute("value").contains(lizsFirstName.getText()));
+
+		assertTrue("Customer first name should contain 'Lizzy', but it contains '" + lizsFirstName.getText() + "'",
+			lizsFirstName.getText().contains("Lizzy"));
+		assertTrue("Bookings first name should contain 'Lizzy', but it contains '" + firstName.getAttribute("value") +
+			"'", firstName.getAttribute("value").contains("Lizzy"));
+	}
+
+	@Test
+	@RunAsClient
+	@InSequence(6000)
+	public void checkBriansBookingsAgain() throws Exception {
+
+		briansInput.click();
+		
+		waitForElement(firstNameXpath);
+
+		logger.log(Level.INFO, "firstName.getAttribute(value) = " + firstName.getAttribute("value"));
+		logger.log(Level.INFO, "lastName.getAttribute(value) = " + lastName.getAttribute("value"));
+		logger.log(Level.INFO, "startDate.getAttribute(value) = " + startDate.getAttribute("value"));
+		logger.log(Level.INFO, "finishDate.getAttribute(value) = " + finishDate.getAttribute("value"));
+
+		assertTrue("customer first name should be the same in the bookings but it is '" + briansFirstName.getText() +
+			"' in the customer module," + " and '" + firstName.getAttribute("value") + "' in bookings",
+			firstName.getAttribute("value").contains(briansFirstName.getText()));
+		assertTrue("customer last name should be the same in the bookings but it is '" + briansLastName.getText() +
+			"' in the customer module," + " and '" + lastName.getAttribute("value") + "' in bookings",
+			lastName.getAttribute("value").contains(briansLastName.getText()));
+
+		assertTrue("Brian's first finishDate should be '04/20/2099', but it is '" + finishDate.getAttribute("value") +
+			"'", finishDate.getAttribute("value").contains("04/20/2099"));
+
+	}
+
+}
---
+++ test/integration/issues/bridge/FACES-224-portlet/src/test/java/com/liferay/faces/test/FACES224PortletTest.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+
+import org.openqa.selenium.WebElement;
+
+import com.liferay.faces.test.util.TesterBase;
+
+
+/**
+ * @author  Liferay Faces Team
+ */
+@RunWith(Arquillian.class)
+public class FACES224PortletTest extends TesterBase {
+
+	private static final Logger logger = Logger.getLogger(FACES224PortletTest.class.getName());
+
+	// portlet topper and menu elements
+	private static final String portletDisplayNameXpath = "//header[@class='portlet-topper']/h1/span";
+
+	private static final String formTagXpath = "//form[@method='post']";
+
+	// <input id="A2535:httpGetButton" type="button" value="Click me to render view2.xhtml via HTTP GET">
+	private static final String buttonXpath = "//input[contains(@value,'Click me to render view2.xhtml')]";
+
+	// <div class="portlet-body" id="aui_3_4_0_1_500"> <div id="A2535" class="liferay-faces-bridge-body">This is
+	// view2.xhtml <br>viewParam1='' (if the issue is fixed, the value should be equal to 'abc') <br>viewParam2='' (if
+	// the issue is fixed, the value should be equal to 'xyz')</div> </div>
+	private static final String view2DivXpath = "//div[@class='portlet-body']/div[1]";
+
+	static final String url = "http://localhost:8080/web/bridge-issues/faces-224";
+
+	@FindBy(xpath = portletDisplayNameXpath)
+	private WebElement portletDisplayName;
+	@FindBy(xpath = formTagXpath)
+	private WebElement formTag;
+	@FindBy(xpath = buttonXpath)
+	private WebElement button;
+	@FindBy(xpath = view2DivXpath)
+	private WebElement view2Div;
+
+	@Test
+	@RunAsClient
+	@InSequence(1000)
+	public void FACES224PortletViewMode() throws Exception {
+
+		logger.log(Level.INFO, "browser.navigate().to(" + url + ")");
+		browser.navigate().to(url);
+		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+		logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
+		logger.log(Level.INFO, "portletDisplayName.getText() = " + portletDisplayName.getText());
+
+		logger.log(Level.INFO, "button.isDisplayed() = " + button.isDisplayed());
+
+		assertTrue("button should be displayed but it is not, nothing found with buttonXpath = " + buttonXpath,
+			button.isDisplayed());
+
+	}
+
+	@Test
+	@RunAsClient
+	@InSequence(2000)
+	public void View2() throws Exception {
+
+		button.click();
+		Thread.sleep(500);
+
+		logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
+
+		logger.log(Level.INFO, "view2Div.isDisplayed() = " + view2Div.isDisplayed());
+		logger.log(Level.INFO, "view2Div.getText() = " + view2Div.getText());
+
+		logger.log(Level.INFO,
+			"view2Div.getText().contains('viewParam1') = " + view2Div.getText().contains("viewParam1"));
+		logger.log(Level.INFO,
+			"view2Div.getText().contains('viewParam2') = " + view2Div.getText().contains("viewParam2"));
+		Thread.sleep(500);
+
+		assertTrue("view2Div.getText() should contain viewParam1='abc', but instead it contains '" +
+			view2Div.getText() + "'", view2Div.getText().contains("viewParam1='abc'"));
+		assertTrue("view2Div.getText() should contain viewParam2='xyz', but instead it contains '" +
+			view2Div.getText() + "'", view2Div.getText().contains("viewParam2='xyz'"));
+
+	}
+
+}
