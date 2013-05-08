a689b3253b5ab2119f15df1b720df65eaedaca64
Vernon <vsingleton@gmail.com>
2013-05-07 23:17:02 -0400

    new jsf2-login-portlet for 3.0.x-legacy

---
+++ demos/portal/jsf2-login-portlet/src/main/java/com/liferay/portal/util/PortalUtilCompat.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.util;
+
+/**
+ *
+ * @author Liferay Faces Team
+ * 
+ */
+public class PortalUtilCompat {
+
+	public static String escapeRedirect(String url) {
+		// no-op for this version
+		return url;
+	}
+	
+}
