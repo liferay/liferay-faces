1e50e5d16c6b1e4970aa1c038129e5756b3db388
Neil Griffin <neil.griffin.scm@gmail.com>
2013-05-16 14:59:12 -0400

    Removed projects that are incompatible with Liferay 5.2

--- demos/bridge/richfaces4-portlet/src/main/webapp/resources/issues/RF-12182.js
+++
@@ -1,7 +0,0 @@
-var rf12182 = {
-	workaround : function (event, id) {
-		var v = RichFaces.$(id);
-		var domElement = RichFaces.getDomElement(v.id);
-		v.invokeEvent("change", domElement, event, v.selectedDate);
-	}
-};
