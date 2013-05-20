18efacb65727dbfefea2eea3d0756af4046a710a
Neil Griffin <neil.griffin.scm@gmail.com>
2013-05-20 18:11:03 -0400

    Merge remote-tracking branch 'refs/remotes/vsingleton/FACES-1570' into 3.2.x

--- test/integration/demos/bridge/jsf2-cdi-portlet/src/test/java/com/liferay/faces/test/Jsf2Cdi.java
+++ test/integration/demos/bridge/jsf2-cdi-portlet/src/test/java/com/liferay/faces/test/Jsf2Cdi.java
@@ -30,7 +30,8 @@ public class Jsf2Cdi {
 
 	// portlet topper and menu elements
 	private static final String portletDisplayNameXpath = "//header[@class='portlet-topper']/h1/span";
-	private static final String menuButtonXpath = "//a[contains(@id,'menuButton')]";
+	// <a class="dropdown-toggle direction-down max-display-items-15" href="javascript:;" id="_1_WAR_jsf2portlet_INSTANCE_ABCD_kldx_menu" title="Options">
+	private static final String menuButtonXpath = "//a[contains(@id,'_menu') and @title='Options']";
 	private static final String menuPreferencesXpath = "//a[contains(@id,'menu_preferences')]";
 
 	// preferences elements
