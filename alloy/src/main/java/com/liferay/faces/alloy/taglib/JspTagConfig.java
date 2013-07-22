a52a1d85c5add787fbb5d39238070c3c530ab0b9
Neil Griffin <neil.griffin.scm@gmail.com>
2013-07-22 18:19:06 -0400

    FACES-1576 DeploymentUnitProcessingException when deploying portlet with Liferay Faces Alloy dependency on JBoss AS

---
+++ alloy/src/main/java/com/liferay/faces/alloy/taglib/JspTagConfig.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.taglib;
+
+import javax.faces.view.facelets.FaceletHandler;
+import javax.faces.view.facelets.Tag;
+import javax.faces.view.facelets.TagConfig;
+
+
+/**
+ * This is a dummy class that exists in order to prevent an exception from being thrown during startup on JBoss AS. For
+ * more information, see <a href="http://issues.liferay.com/browse/FACES-1576">FACES-1576</a>.
+ *
+ * @author  Neil Griffin
+ */
+public class JspTagConfig implements TagConfig {
+
+	// Private Data Members
+	private FaceletHandler nextHandler;
+	private Tag tag;
+	private String tagId;
+
+	public FaceletHandler getNextHandler() {
+		return nextHandler;
+	}
+
+	public void setNextHandler(FaceletHandler nextHandler) {
+		this.nextHandler = nextHandler;
+	}
+
+	public Tag getTag() {
+		return tag;
+	}
+
+	public void setTag(Tag tag) {
+		this.tag = tag;
+	}
+
+	public String getTagId() {
+		return tagId;
+	}
+
+	public void setTagId(String tagId) {
+		this.tagId = tagId;
+	}
+
+}
