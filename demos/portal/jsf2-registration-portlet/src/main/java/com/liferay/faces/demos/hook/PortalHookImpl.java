17e3cd75b52adca06e8ad77506592e77229f6d15
Neil Griffin <neil.griffin.scm@gmail.com>
2013-08-03 17:03:33 -0400

    FACES-1665 Use an InvocationHandler in order to wrap PortalImpl.java in the jsf2-registration-portlet

---
+++ demos/portal/jsf2-registration-portlet/src/main/java/com/liferay/faces/demos/hook/PortalHookImpl.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.hook;
+
+import java.lang.reflect.Method;
+
+import javax.portlet.PortletMode;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+import javax.portlet.WindowState;
+import javax.servlet.http.HttpServletRequest;
+
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.Portal;
+
+import com.liferay.portlet.PortletURLFactoryUtil;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class PortalHookImpl extends PortalWrapper {
+
+	// Private Data Members
+	private Portal wrappedPortal;
+
+	public PortalHookImpl(Portal portal) {
+		wrappedPortal = portal;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+
+		Object returnValue = null;
+
+		if (method.getName().equals("getCreateAccountURL")) {
+			HttpServletRequest httpServletRequest = (HttpServletRequest) arguments[0];
+			ThemeDisplay themeDisplay = (ThemeDisplay) arguments[1];
+			returnValue = getCreateAccountURL(httpServletRequest, themeDisplay);
+		}
+		else {
+			returnValue = super.invoke(proxy, method, arguments);
+		}
+
+		return returnValue;
+	}
+
+	protected String getCreateAccountURL(HttpServletRequest request, ThemeDisplay themeDisplay) throws Exception {
+
+		String portletName = "1_WAR_jsf2registrationportlet";
+		PortletURL urlCreateAccount = PortletURLFactoryUtil.create(request, portletName, themeDisplay.getPlid(),
+				PortletRequest.ACTION_PHASE);
+
+		urlCreateAccount.setWindowState(WindowState.MAXIMIZED);
+		urlCreateAccount.setPortletMode(PortletMode.VIEW);
+
+		urlCreateAccount.setParameter("saveLastPath", "0");
+		urlCreateAccount.setParameter("struts_action", "/login/create_account");
+
+		return urlCreateAccount.toString();
+	}
+
+	@Override
+	public Portal getWrapped() {
+		return wrappedPortal;
+	}
+
+}
---
+++ demos/portal/jsf2-registration-portlet/src/main/java/com/liferay/faces/demos/hook/PortalWrapper.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.hook;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.faces.FacesWrapper;
+
+import com.liferay.portal.util.Portal;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class PortalWrapper implements InvocationHandler, FacesWrapper<Portal> {
+
+	public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+
+		try {
+			Object returnValue = method.invoke(getWrapped(), arguments);
+
+			return returnValue;
+		}
+		catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	public abstract Portal getWrapped();
+}
