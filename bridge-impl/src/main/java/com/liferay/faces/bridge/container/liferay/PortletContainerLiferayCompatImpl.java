9fd7f2579835bd357b966acad6a00c3bed9e8b75
Neil Griffin <neil.griffin.scm@gmail.com>
2013-08-19 14:20:08 -0400

    FACES-1633 The p_p_auth token is being removed by the bridge from ActionURLs and PartialActionURLs even if the <add-default-resource> parameter is set to true.

--- bridge-impl/src/main/java/com/liferay/faces/bridge/container/liferay/PortletContainerLiferayCompatImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/container/liferay/PortletContainerLiferayCompatImpl.java
@@ -17,21 +17,15 @@ import javax.faces.context.ResponseWriter;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.portlet.PortletRequest;
-import javax.portlet.faces.Bridge;
 
 import com.liferay.faces.bridge.config.BridgeConfig;
 import com.liferay.faces.bridge.container.PortletContainerImpl;
 import com.liferay.faces.bridge.context.BridgeContext;
 import com.liferay.faces.bridge.renderkit.html_basic.HeadResponseWriter;
 import com.liferay.faces.bridge.renderkit.html_basic.HeadResponseWriterLiferayImpl;
-import com.liferay.faces.util.logging.Logger;
-import com.liferay.faces.util.logging.LoggerFactory;
 
 import com.liferay.portal.SystemException;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.model.Portlet;
-import com.liferay.portal.service.PortletLocalServiceUtil;
-import com.liferay.portal.theme.ThemeDisplay;
 
 
 /**
@@ -40,12 +34,6 @@ import com.liferay.portal.theme.ThemeDisplay;
  * @author  Neil Griffin
  */
 public class PortletContainerLiferayCompatImpl extends PortletContainerImpl {
-
-	// Logger
-	private static final Logger logger = LoggerFactory.getLogger(PortletContainerLiferayCompatImpl.class);
-
-	// Private Constants
-	private static final String RENDER_PORTLET = "RENDER_PORTLET";
 
 	// serialVersionUID
 	private static final long serialVersionUID = 8713570232856573935L;
@@ -63,10 +51,10 @@ public class PortletContainerLiferayCompatImpl extends PortletContainerImpl {
 	@Override
 	public void afterPhase(PhaseEvent phaseEvent) {
 
-		// Remove duplicate resources from the LIFERAY_SHARED_PAGE_TOP request attribute. For more information, see:
-		// http://issues.liferay.com/browse/FACES-1216
 		BridgeContext bridgeContext = BridgeContext.getCurrentInstance();
 
+		// Remove duplicate resources from the LIFERAY_SHARED_PAGE_TOP request attribute. For more information, see:
+		// http://issues.liferay.com/browse/FACES-1216
 		if (liferaySharedPageTopLength > 0) {
 
 			PortletRequest portletRequest = bridgeContext.getPortletRequest();
@@ -81,16 +69,6 @@ public class PortletContainerLiferayCompatImpl extends PortletContainerImpl {
 				portletRequest.setAttribute(WebKeys.PAGE_TOP, stringBundler.getWrapped());
 			}
 		}
-
-		// Remove the "RENDER_PORTLET" attribute that may have been added by the {@link #beforePhase(PhaseEvent)}
-		// method.
-		if (bridgeContext.getPortletRequestPhase() == Bridge.PortletPhase.RESOURCE_PHASE) {
-
-			PortletRequest portletRequest = bridgeContext.getPortletRequest();
-
-			portletRequest.removeAttribute(RENDER_PORTLET);
-
-		}
 	}
 
 	/**
@@ -104,35 +82,11 @@ public class PortletContainerLiferayCompatImpl extends PortletContainerImpl {
 		liferaySharedPageTopLength = 0;
 
 		BridgeContext bridgeContext = BridgeContext.getCurrentInstance();
-
 		PortletRequest portletRequest = bridgeContext.getPortletRequest();
 		StringBundler stringBundler = new StringBundler(portletRequest.getAttribute(WebKeys.PAGE_TOP));
 
+		if (stringBundler != null) {
 		liferaySharedPageTopLength = stringBundler.length();
-
-		// If running in the RESOURCE_PHASE of the portlet lifecycle, then
-		if (bridgeContext.getPortletRequestPhase() == Bridge.PortletPhase.RESOURCE_PHASE) {
-
-			// During the RENDER_PHASE of the portlet lifecycle, the Liferay PortletImpl.renderPortlet(...) method adds
-			// the "RENDER_PORTLET" request attribute, which is consulted by the PortletURLImpl.addPortletAuthToken(...)
-			// method when URLs are being constructed. But during the RESOURCE_PHASE, Liferay does not add the
-			// "RENDER_PORTLET" attribute, and the PortletURLImpl.addPortletAuthToken(...) method behaves differently
-			// and ultimately causes the URL to have an additional "p_p_auth" parameter. Since the URL is different, it
-			// will cause ICEfaces to detect a DOM-diff, and will unnecessarily replace markup in the DOM.
-			//
-			// The workaround is to add the "RENDER_PORTLET" attribute here (before the/ RENDER_RESPONSE phase of the
-			// JSF lifecycle executes), and then remove the attribute after the RENDER_RESPONSE phase is complete.
-			// For more information, see: http://issues.liferay.com/browse/FACES-1435
-			String portletId = (String) portletRequest.getAttribute(WebKeys.PORTLET_ID);
-			ThemeDisplay themeDisplay = (ThemeDisplay) portletRequest.getAttribute(WebKeys.THEME_DISPLAY);
-
-			try {
-				Portlet portlet = PortletLocalServiceUtil.getPortletById(themeDisplay.getCompanyId(), portletId);
-				portletRequest.setAttribute(RENDER_PORTLET, portlet);
-			}
-			catch (SystemException e) {
-				logger.error(e);
-			}
 		}
 	}
 
