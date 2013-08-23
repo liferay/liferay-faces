6facfde74b7d957c96950ef4c4373f3d405c89b8
Neil Griffin <neil.griffin.scm@gmail.com>
2013-08-23 16:26:03 -0400

    FACES-1704 Move backported portal classes to new package com.liferay.faces.util.portal

--- util/src/main/java/com/liferay/faces/util/portal/EditorUtil.java
+++ util/src/main/java/com/liferay/faces/util/portal/EditorUtil.java
@@ -15,13 +15,48 @@ package com.liferay.faces.util.portal;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.liferay.portal.kernel.servlet.BrowserSnifferUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.Validator;
+
 
 /**
- * @author  Neil Griffin
+ * This class is a backport of the EditorUtil class which was introduced in Liferay Portal 6.0.12. It's main purpose is
+ * to facilitate keeping the {@link com.liferay.faces.portal.renderkit.InputEditorInternalRenderer} source the same on
+ * all branches of the Liferay Faces source.
+ *
+ * @author  Julio Camarero
  */
 public class EditorUtil {
 
+	private static final String _EDITOR_WYSIWYG_DEFAULT = PropsUtil.get(PropsKeys.EDITOR_WYSIWYG_DEFAULT);
+
 	public static String getEditorValue(HttpServletRequest request, String editorImpl) {
-		return com.liferay.portal.kernel.editor.EditorUtil.getEditorValue(request, editorImpl);
+
+		if (Validator.isNotNull(editorImpl)) {
+			editorImpl = PropsUtil.get(editorImpl);
+		}
+
+		if (!BrowserSnifferUtil.isRtf(request)) {
+
+			if (BrowserSnifferUtil.isSafari(request) && BrowserSnifferUtil.isMobile(request)) {
+
+				editorImpl = "simple";
+			}
+			else if (BrowserSnifferUtil.isSafari(request) && !editorImpl.contains("simple")) {
+
+				editorImpl = "tinymce_simple";
+			}
+			else {
+				editorImpl = "simple";
+			}
+		}
+
+		if (Validator.isNull(editorImpl)) {
+			editorImpl = _EDITOR_WYSIWYG_DEFAULT;
+		}
+
+		return editorImpl;
 	}
 }
