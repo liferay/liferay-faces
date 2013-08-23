72a72c6d1b6d9c83d89f71d6d0fe8cd77b9ba6f5
Neil Griffin <neil.griffin.scm@gmail.com>
2013-08-23 16:34:01 -0400

    FACES-1698 Implement methods inside of PageContextAdapter rather than throwing UnsupportedOperationException

---
+++ util/src/main/java/com/liferay/faces/util/jsp/PageAdapter.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.jsp;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class PageAdapter implements Servlet {
+
+	private ServletConfig servletConfig;
+
+	public PageAdapter(ServletConfig servletConfig) {
+		this.servletConfig = servletConfig;
+	}
+
+	public void destroy() {
+	}
+
+	public void init(ServletConfig servletConfig) throws ServletException {
+		this.servletConfig = servletConfig;
+	}
+
+	public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException,
+		IOException {
+		throw new UnsupportedOperationException();
+	}
+
+	public ServletConfig getServletConfig() {
+		return servletConfig;
+	}
+
+	public String getServletInfo() {
+		return PageAdapter.class.getName();
+	}
+
+}
---
+++ util/src/main/java/com/liferay/faces/util/jsp/SessionScope.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.jsp;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpSession;
+
+import com.liferay.faces.util.map.AbstractPropertyMap;
+import com.liferay.faces.util.map.AbstractPropertyMapEntry;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class SessionScope extends AbstractPropertyMap<Object> {
+
+	// Private Data Members
+	private HttpSession httpSession;
+
+	public SessionScope(HttpSession httpSession) {
+		this.httpSession = httpSession;
+	}
+
+	@Override
+	protected AbstractPropertyMapEntry<Object> createPropertyMapEntry(String name) {
+		return new SessionScopeEntry(httpSession, name);
+	}
+
+	@Override
+	protected void removeProperty(String name) {
+		httpSession.removeAttribute(name);
+	}
+
+	@Override
+	protected Object getProperty(String name) {
+		return httpSession.getAttribute(name);
+	}
+
+	@Override
+	protected void setProperty(String name, Object value) {
+		httpSession.setAttribute(name, value);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected Enumeration<String> getPropertyNames() {
+		return (Enumeration<String>) httpSession.getAttributeNames();
+	}
+
+}
