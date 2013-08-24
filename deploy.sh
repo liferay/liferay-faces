6af44e48ca8fc84fc5c63c80d2802d3f485d76b4
Neil Griffin <neil.griffin.scm@gmail.com>
2013-08-24 18:37:39 -0400

    FACES-1697 Add redeploy profile to parent-most pom in order to control when server deployments are cleaned FACES-1648 Unable to deploy multiple portlets to Pluto with the "pluto" maven profile FACES-1682 Consolidate duplicate profiles from child poms into parent pom

--- deploy.sh
+++ deploy.sh
@@ -121,6 +121,13 @@ until [ -z $1 ] ; do
 	fi
 	shift
 done
+if [ -e src/main/webapp/WEB-INF/portlet.xml ] ; then
+	rm -rf target
+	mkdir target
+	touch target/$SERVER_PROFILE_NAME-portlet-war-activation.tmp
+	touch target/$PORTAL_PROFILE_NAME-portlet-war-redeploy-activation.tmp
+	touch target/$PORTAL_PROFILE_NAME-portlet-war-activation.tmp
+fi
 PORTLET_MVN_CMD="mvn -P $PORTAL_PROFILE_NAME,$FACES_IMPL,$SERVER_PROFILE_NAME,redeploy,$EXTRA_PROFILE_NAMES help:active-profiles clean install"
 if [ "$PORTAL_PROFILE_NAME" = "liferay" ] ; then
 	if [ "$LIFERAY_FACES_VERSION" = "3.0.x-legacy" ] ; then
