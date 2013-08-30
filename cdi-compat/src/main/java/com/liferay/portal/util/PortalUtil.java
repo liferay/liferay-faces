70611daed3a2efeeb3fcc958e85576bdbd5e0613
Neil Griffin <neil.griffin.scm@gmail.com>
2013-08-30 17:25:46 -0400

    FACES-1589 jsf2-cdi-portlet demo does not deploy on Pluto

---
+++ cdi-compat/src/main/java/com/liferay/portal/util/PortalUtil.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.portal.util;
+
+import javax.portlet.PortletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import com.liferay.faces.bridge.filter.HttpServletRequestAdapter;
+
+
+/**
+ * Since the Liferay CDI Portlet Bridge has a dependency on the {@link PortalUtil#getHttpServletRequest(PortletRequest)}
+ * method, this class is necessary to provide a runtime compatibility layer.
+ *
+ * @author  Neil Griffin
+ */
+public class PortalUtil {
+
+	public static HttpServletRequest getHttpServletRequest(PortletRequest portletRequest) {
+		return new HttpServletRequestAdapter(portletRequest);
+	}
+}
