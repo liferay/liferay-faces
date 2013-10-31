11e33ca6f080a5f3a9ad86a6719e2a9427afe49b
Neil Griffin <neil.griffin.scm@gmail.com>
2013-10-31 17:23:48 -0400

    FACES-1776 Develop MessageContextWrapper class in order to support chain of delegation pattern

---
+++ util/src/main/java/com/liferay/faces/util/context/MessageContextWrapper.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.context;
+
+import java.util.Locale;
+
+import javax.faces.FacesWrapper;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.FacesContext;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class MessageContextWrapper extends MessageContext implements FacesWrapper<MessageContext> {
+
+	@Override
+	public FacesMessage newFacesMessage(Locale locale, Severity severity, String messageId) {
+		return getWrapped().newFacesMessage(locale, severity, messageId);
+	}
+
+	@Override
+	public FacesMessage newFacesMessage(FacesContext facesContext, Severity severity, String messageId) {
+		return getWrapped().newFacesMessage(facesContext, severity, messageId);
+	}
+
+	@Override
+	public FacesMessage newFacesMessage(Locale locale, Severity severity, String messageId, Object... arguments) {
+		return getWrapped().newFacesMessage(locale, severity, messageId, arguments);
+	}
+
+	@Override
+	public FacesMessage newFacesMessage(FacesContext facesContext, Severity severity, String messageId,
+		Object... arguments) {
+		return getWrapped().newFacesMessage(facesContext, severity, messageId, arguments);
+	}
+
+	@Override
+	public String getMessage(Locale locale, String messageId) {
+		return getWrapped().getMessage(locale, messageId);
+	}
+
+	@Override
+	public String getMessage(Locale locale, String messageId, Object... arguments) {
+		return getWrapped().getMessage(locale, messageId, arguments);
+	}
+
+	public abstract MessageContext getWrapped();
+}
