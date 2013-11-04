1fd65a90c3745f0cb4aab3c0c6a5443ae197362e
Neil Griffin <neil.griffin.scm@gmail.com>
2013-11-04 13:56:02 -0500

    FACES-1778 Develop Selenium test for jsf2-export-pdf-portlet

---
+++ test/integration/demos/bridge/jsf2-export-pdf-portlet/src/test/java/com/liferay/faces/test/Jsf2ExportPdfPortletTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.test;
+
+import java.util.Set;
+import java.util.logging.Level;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.liferay.faces.test.util.TesterBase;
+
+/**
+ * @author  Liferay Faces Team
+ */
+@RunWith(Arquillian.class)
+public class Jsf2ExportPdfPortletTest extends TesterBase {
+
+	// portlet topper
+	// <a href="http://localhost:8080/group/bridge-demos/jsf2-pdf?p_p_id=1_WAR_jsf2e...let_customerId=1" target="_blank" id="yui_patched_dev_3_x_1_1383368924455_492">Export</a>
+	private static final String brianExportXpath =
+		"//a[contains(text(),'Export')]/../following-sibling::td[1][contains(text(),'Green')]/preceding-sibling::td[1]/a";
+	private static final String LizExportXpath =
+			"//a[contains(text(),'Export')]/../following-sibling::td[1][contains(text(),'Kessler')]/preceding-sibling::td[1]/a";
+	private static final String richExportXpath =
+			"//a[contains(text(),'Export')]/../following-sibling::td[1][contains(text(),'Shearer')]/preceding-sibling::td[1]/a";
+
+	static final String url = baseUrl + webContext + "/jsf2-pdf";
+
+	@FindBy(xpath = brianExportXpath)
+	private WebElement brianExport;
+	@FindBy(xpath = LizExportXpath)
+	private WebElement LizExport;
+	@FindBy(xpath = richExportXpath)
+	private WebElement richExport;
+
+	@Test
+	@RunAsClient
+	@InSequence(1000)
+	public void renderViewMode() throws Exception {
+
+		signIn();
+		logger.log(Level.INFO, "browser.navigate().to(" + url + ")");
+		browser.navigate().to(url);
+		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+		logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
+		getPortletDisplayName();
+		logger.log(Level.INFO, "displayName.getText() = " + displayName.getText());
+
+		assertTrue("customerPortletDisplayName displayName.isDisplayed()", displayName.isDisplayed());
+		
+		if (isThere(brianExportXpath)) {
+			logger.log(Level.INFO, "isThere(brianExportXpath) = " + isThere(brianExportXpath));
+			richExport.click();
+			Thread.sleep(1000);
+			
+			Set<String> handles = browser.getWindowHandles();
+			for (String window : handles) {
+                browser.switchTo().window(window);
+                logger.log(Level.INFO, "window = " + window);
+                logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
+    			logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
+    			logger.log(Level.INFO, "browser.getPageSource() = " + browser.getPageSource());
+    			logger.log(Level.INFO, " -------------------------------------------------- ");
+            }
+			
+			logger.log(Level.INFO, "browser.getPageSource().contains('Shearer') = " + browser.getPageSource().contains("Shearer"));
+			logger.log(Level.INFO, " -------------------------------------------------- ");
+			
+			assertTrue("The pdf document should contain 'Shearer', but instead it contains: " + browser.getPageSource(),
+				browser.getPageSource().contains("Shearer")
+			);
+			
+		}
+
+	}
+
+}
