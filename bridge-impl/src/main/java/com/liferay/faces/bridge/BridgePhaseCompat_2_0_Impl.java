7cfb47af4e278e86de5a7dac0e13f9aeec458370
Neil Griffin <neil.griffin.scm@gmail.com>
2013-11-04 16:43:01 -0500

    FACES-1459 Support for JSF 2.2 (minimize diffs across branches)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/BridgePhaseCompat_2_0_Impl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/BridgePhaseCompat_2_0_Impl.java
@@ -14,13 +14,8 @@
 package com.liferay.faces.bridge;
 
 import java.io.IOException;
-import java.util.Map;
 
 import javax.faces.context.FacesContext;
-import javax.portlet.PortletConfig;
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletResponse;
-import javax.portlet.faces.Bridge.PortletPhase;
 
 
 /**
@@ -28,15 +23,7 @@ import javax.portlet.faces.Bridge.PortletPhase;
  *
  * @author  Neil Griffin
  */
-public abstract class BridgePhaseCompatImpl extends BridgePhaseBaseImpl {
-
-	// Private Constants
-	private static final String MOJARRA_REQUEST_STATE_MANAGER = "com.sun.faces.util.RequestStateManager";
-	private static final String MOJARRA_RENDER_KIT_IMPL_FOR_REQUEST = "com.sun.faces.renderKitImplForRequest";
-
-	public BridgePhaseCompatImpl(PortletConfig portletConfig) {
-		super(portletConfig);
-	}
+public abstract class BridgePhaseCompat_2_0_Impl {
 
 	public void handleJSF2ResourceRequest(FacesContext facesContext) throws IOException {
 		// no-op for JSF 1.x
@@ -44,22 +31,6 @@ public abstract class BridgePhaseCompatImpl extends BridgePhaseBaseImpl {
 
 	protected void clearHeadManagedBeanResources(FacesContext facesContext) {
 		// no-op for JSF 1.x
-	}
-
-	@Override
-	protected void init(PortletRequest portletRequest, PortletResponse portletResponse, PortletPhase portletPhase) {
-
-		super.init(portletRequest, portletResponse, portletPhase);
-
-		// Remove the render-kit attribute that Mojarra saves in the request map in order to prevent a
-		// ClassCastException during Portlet 2.0 Events IPC.
-		@SuppressWarnings("unchecked")
-		Map<String, Object> mojarraRequestStateManager = (Map<String, Object>) portletRequest.getAttribute(
-				MOJARRA_REQUEST_STATE_MANAGER);
-
-		if (mojarraRequestStateManager != null) {
-			mojarraRequestStateManager.remove(MOJARRA_RENDER_KIT_IMPL_FOR_REQUEST);
-		}
 	}
 
 	public Throwable getJSF2HandledException(FacesContext facesContext) {
