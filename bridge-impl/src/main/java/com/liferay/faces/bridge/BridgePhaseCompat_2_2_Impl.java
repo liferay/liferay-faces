dcaaeac8f2abe14a42fa1d97cf7ced774976f319
Neil Griffin <neil.griffin.scm@gmail.com>
2013-11-04 16:44:30 -0500

    FACES-1459 Support for JSF 2.2 (minimize diffs across branches)

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/BridgePhaseCompat_2_2_Impl.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge;
+
+import javax.faces.context.FacesContext;
+import javax.faces.lifecycle.Lifecycle;
+
+
+/**
+ * This class provides a compatibility layer that isolates differences related to JSF 2.2.
+ *
+ * @author  Neil Griffin
+ */
+public abstract class BridgePhaseCompat_2_2_Impl extends BridgePhaseCompat_2_0_Impl {
+
+	protected void attachClientWindowToLifecycle(FacesContext facesContext, Lifecycle lifecycle) {
+		// no-op for versions of JSF prior to 2.2
+	}
+}
