4c2f8f43f2218856ea84d4eaa3bc7375024714d0
Neil Griffin <neil.griffin.scm@gmail.com>
2013-11-04 17:16:54 -0500

    FACES-1459 Support for JSF 2.2 (Removed BridgeFlash and supporting classes)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/config/SAXHandlerFacesConfigPostImpl_2_0.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/config/SAXHandlerFacesConfigPostImpl_2_0.java
@@ -13,15 +13,6 @@
  */
 package com.liferay.faces.bridge.config;
 
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-
-import com.liferay.faces.bridge.bean.BeanManagerFactory;
-import com.liferay.faces.bridge.context.flash.BridgeFlashFactory;
-import com.liferay.faces.util.logging.Logger;
-import com.liferay.faces.util.logging.LoggerFactory;
-
-
 /**
  * This class provides a compatibility layer that isolates differences for JSF 2.0.
  *
@@ -29,51 +20,8 @@ import com.liferay.faces.util.logging.LoggerFactory;
  */
 public abstract class SAXHandlerFacesConfigPostImpl_2_0 extends SAXHandlerFacesConfigPostImpl_1_2 {
 
-	// Logger
-	private static final Logger logger = LoggerFactory.getLogger(SAXHandlerFacesConfigPostImpl_2_0.class);
-
-	// Private Constants
-	private static final String BRIDGE_FLASH_FACTORY = "bridge-flash-factory";
-
-	// Private Data Members
-	private boolean parsingBridgeFlashFactory;
-
 	public SAXHandlerFacesConfigPostImpl_2_0(boolean resolveEntities,
 		BridgeConfigAttributeMap bridgeConfigAttributeMap) {
 		super(resolveEntities, bridgeConfigAttributeMap);
 	}
-
-	@Override
-	public void endElement(String uri, String localName, String elementName) throws SAXException {
-
-		if (parsingBridgeFlashFactory) {
-			setupFactory(BridgeFlashFactory.class, content.toString().trim());
-		}
-		else {
-			super.endElement(uri, localName, elementName);
-		}
-
-		parsingBridgeFlashFactory = false;
-	}
-
-	@Override
-	public void logMissingElementErrors() {
-
-		if (bridgeConfigAttributeMap.get(BeanManagerFactory.class.getName()) == null) {
-			logger.error(FACTORY_NOT_FOUND_MSG, BRIDGE_FLASH_FACTORY);
-		}
-	}
-
-	@Override
-	public void startElement(String uri, String localName, String elementName, Attributes attributes)
-		throws SAXException {
-
-		if (localName.equals(BRIDGE_FLASH_FACTORY)) {
-			parsingBridgeFlashFactory = true;
-		}
-		else {
-			super.startElement(uri, localName, elementName, attributes);
-		}
-	}
-
 }
