ae64754bc13cab48561ceb2115e47e2d89bb7db3
Neil Griffin <neil.griffin.scm@gmail.com>
2013-11-06 15:02:37 -0500

    FACES-1459 Support for JSF 2.2 (minimize diffs across branches)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/context/ExternalContextCompat_1_2_Impl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/ExternalContextCompat_1_2_Impl.java
@@ -17,8 +17,13 @@ import java.util.Map;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
 
 import com.liferay.faces.bridge.component.primefaces.PrimeFacesFileUpload;
+import com.liferay.faces.bridge.config.BridgeConfigConstants;
+import com.liferay.faces.util.helper.BooleanHelper;
 
 
 /**
@@ -31,11 +36,27 @@ public abstract class ExternalContextCompat_1_2_Impl extends ExternalContext {
 
 	// Protected Data Members
 	protected BridgeContext bridgeContext;
+	protected IncongruityContext incongruityContext;
+	protected boolean manageIncongruities;
+	protected PortletContext portletContext;
+	protected PortletRequest portletRequest;
+	protected PortletResponse portletResponse;
 
-	public ExternalContextCompat_1_2_Impl() {
+	public ExternalContextCompat_1_2_Impl(PortletContext portletContext, PortletRequest portletRequest,
+		PortletResponse portletResponse) {
+
+		this.portletContext = portletContext;
+		this.portletRequest = portletRequest;
+		this.portletResponse = portletResponse;
 
 		// Get the BridgeContext.
 		this.bridgeContext = BridgeContext.getCurrentInstance();
+
+		this.incongruityContext = bridgeContext.getIncongruityContext();
+
+		// Determine whether or not lifecycle incongruities should be managed.
+		this.manageIncongruities = BooleanHelper.toBoolean(bridgeContext.getInitParameter(
+					BridgeConfigConstants.PARAM_MANAGE_INCONGRUITIES), true);
 	}
 
 	/**
