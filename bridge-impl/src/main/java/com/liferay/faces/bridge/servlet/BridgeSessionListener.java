b46b43a98f9629f45dc519bb423c2f766ed29dcd
Neil Griffin <neil.griffin.scm@gmail.com>
2013-12-19 18:04:59 -0500

    FACES-1401 Upgrade from Java 5 to Java 6 - eliminate warning in Eclipse (reverting previous commit)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/servlet/BridgeSessionListener.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/servlet/BridgeSessionListener.java
@@ -199,6 +199,7 @@ public class BridgeSessionListener implements HttpSessionListener, ServletContex
 
 				try {
 
+					@SuppressWarnings("unchecked")
 					Enumeration<String> attributeNames = (Enumeration<String>) httpSession.getAttributeNames();
 
 					while (attributeNames.hasMoreElements()) {
