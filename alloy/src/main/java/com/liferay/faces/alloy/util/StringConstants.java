5b132e063ebbd1774fb5546a7706ad69c5d0e58a
Neil Griffin <neil.griffin.scm@gmail.com>
2014-01-23 15:59:55 -0500

    FACES-1829 Deprecate AlloyUtil.java, GetterUtil.java, and StringConstants.java

--- alloy/src/main/java/com/liferay/faces/alloy/util/StringConstants.java
+++ alloy/src/main/java/com/liferay/faces/alloy/util/StringConstants.java
@@ -13,22 +13,73 @@
  */
 package com.liferay.faces.alloy.util;
 
+import com.liferay.faces.util.lang.StringPool;
+
+
 /**
- * @author  Neil Griffin
+ * @author      Neil Griffin
+ * @deprecated  Use {@link com.liferay.faces.util.lang.StringPool StringPool} instead.
  */
+@Deprecated
 public class StringConstants {
 
-	public static final String ATTRIBUTE_CLASS = "class";
-	public static final String ATTRIBUTE_ID = "id";
-	public static final String ATTRIBUTE_CSS_CLASS = "cssClass";
-	public static final String ATTRIBUTE_STYLE_CLASS = "styleClass";
-	public static final String BOOLEAN_TRUE = "true";
-	public static final String BOOLEAN_FALSE = "false";
-	public static final String CHAR_BLANK = "";
-	public static final String CHAR_COMMA = ",";
-	public static final String CHAR_SPACE = " ";
-	public static final String ELEMENT_DIV = "div";
-	public static final String ELEMENT_UL = "ul";
-	public static final String ELEMENT_LI = "li";
+	/**
+	 * @deprecated  Use {@link StringPool#CLASS} instead.
+	 */
+	public static final String ATTRIBUTE_CLASS = StringPool.CLASS;
 
+	/**
+	 * @deprecated  Use {@link StringPool#ID} instead.
+	 */
+	public static final String ATTRIBUTE_ID = StringPool.ID;
+
+	/**
+	 * @deprecated  Use "cssClass" String value instead.
+	 */
+	public static final String ATTRIBUTE_CSS_CLASS = "cssClass";
+
+	/**
+	 * @deprecated  Use "styleClass" String value instead.
+	 */
+	public static final String ATTRIBUTE_STYLE_CLASS = "styleClass";
+
+	/**
+	 * @deprecated  Use {@link StringPool#TRUE} instead.
+	 */
+	public static final String BOOLEAN_TRUE = StringPool.TRUE;
+
+	/**
+	 * @deprecated  Use {@link StringPool#FALSE} instead.
+	 */
+	public static final String BOOLEAN_FALSE = StringPool.FALSE;
+
+	/**
+	 * @deprecated  Use {@link StringPool#BLANK} instead.
+	 */
+	public static final String CHAR_BLANK = StringPool.BLANK;
+
+	/**
+	 * @deprecated  Use {@link StringPool#COMMA} instead.
+	 */
+	public static final String CHAR_COMMA = StringPool.COMMA;
+
+	/**
+	 * @deprecated  Use {@link StringPool#SPACE} instead.
+	 */
+	public static final String CHAR_SPACE = StringPool.SPACE;
+
+	/**
+	 * @deprecated  Use {@link StringPool#DIV} instead.
+	 */
+	public static final String ELEMENT_DIV = StringPool.DIV;
+
+	/**
+	 * @deprecated  Use {@link StringPool#UL} instead.
+	 */
+	public static final String ELEMENT_UL = StringPool.UL;
+
+	/**
+	 * @deprecated  Use {@link StringPool#LI} instead.
+	 */
+	public static final String ELEMENT_LI = StringPool.LI;
 }
