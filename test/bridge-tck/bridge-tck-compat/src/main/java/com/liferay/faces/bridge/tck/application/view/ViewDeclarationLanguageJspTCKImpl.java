f108257f63af942019a17163668fbfbd163bd2a4
Neil Griffin <neil.griffin.scm@gmail.com>
2014-02-14 15:23:03 -0500

    FACES-1876 InstantiationException thrown during execution of JSR 329 TCK on WebLogic 12c

--- test/bridge-tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/application/view/ViewDeclarationLanguageJspTCKImpl.java
+++ test/bridge-tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/application/view/ViewDeclarationLanguageJspTCKImpl.java
@@ -99,13 +99,4 @@ public class ViewDeclarationLanguageJspTCKImpl extends ViewDeclarationLanguageWr
 		return wrappedViewDeclarationLanguage;
 	}
 
-	protected class EncodedURLHiddenField extends HtmlInputHidden {
-
-		@Override
-		public String getClientId(FacesContext facesContext) {
-			return "javax.faces.encodedURL";
-		}
-
-	}
-
 }
