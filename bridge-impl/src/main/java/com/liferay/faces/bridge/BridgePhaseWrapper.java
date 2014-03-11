885d16d51b04989fdde783ece25d921e2c17159d
Neil Griffin <neil.griffin.scm@gmail.com>
2014-03-11 15:00:37 -0400

    FACES-1893 Develop wrappers for instances of classes/interfaces produced by bridge factories

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/BridgePhaseWrapper.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge;
+
+import javax.faces.FacesWrapper;
+import javax.portlet.faces.BridgeDefaultViewNotSpecifiedException;
+import javax.portlet.faces.BridgeException;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class BridgePhaseWrapper implements BridgePhase, FacesWrapper<BridgePhase> {
+
+	public void execute() throws BridgeDefaultViewNotSpecifiedException, BridgeException {
+		getWrapped().execute();
+	}
+
+	public abstract BridgePhase getWrapped();
+}
---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/bean/BeanManagerWrapper.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.bean;
+
+import javax.faces.FacesWrapper;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class BeanManagerWrapper implements BeanManager, FacesWrapper<BeanManager> {
+
+	public void invokePreDestroyMethods(Object managedBean, boolean preferPreDestroy) {
+		getWrapped().invokePreDestroyMethods(managedBean, preferPreDestroy);
+	}
+
+	public boolean isManagedBean(String name, Object value) {
+		return getWrapped().isManagedBean(name, value);
+	}
+
+	public abstract BeanManager getWrapped();
+}
