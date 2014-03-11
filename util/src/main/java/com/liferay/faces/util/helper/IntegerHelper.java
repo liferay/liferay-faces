bdb76bf6b564dd8a381fb28f66cc6918c34170d8
Neil Griffin <neil.griffin.scm@gmail.com>
2014-03-11 11:51:06 -0400

    FACES-1899 ShortHelper, IntegerHelper, and LongHelper ignore default values

--- util/src/main/java/com/liferay/faces/util/helper/IntegerHelper.java
+++ util/src/main/java/com/liferay/faces/util/helper/IntegerHelper.java
@@ -24,7 +24,7 @@ public class IntegerHelper {
 
 	public static int toInteger(String value, int defaultValue) {
 
-		int valueAsInt = 0;
+		int valueAsInt = defaultValue;
 
 		try {
 			valueAsInt = Integer.parseInt(value);
