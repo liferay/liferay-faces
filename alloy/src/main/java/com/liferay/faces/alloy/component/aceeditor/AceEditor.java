5e92345049cf2039731b4695d2a1612de49aea6e
Neil Griffin <neil.griffin.scm@gmail.com>
2014-03-17 12:26:50 -0400

    FACES-1903 Develop aui:aceEditor component

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/aceeditor/AceEditor.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.aceeditor;
+
+import javax.faces.component.FacesComponent;
+
+import com.liferay.faces.util.component.ComponentUtil;
+import com.liferay.faces.util.lang.StringPool;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@FacesComponent(value = AceEditor.COMPONENT_TYPE)
+public class AceEditor extends AceEditorBase {
+
+	// Public Constants
+	public static final String COMPONENT_FAMILY = "com.liferay.faces.alloy.component.aceeditor";
+	public static final String COMPONENT_TYPE = "com.liferay.faces.alloy.component.aceeditor.AceEditor";
+	public static final String RENDERER_TYPE = "com.liferay.faces.alloy.component.aceeditor.AceEditorRenderer";
+
+	public AceEditor() {
+		super();
+		setRendererType(RENDERER_TYPE);
+	}
+
+	@Override
+	public String getBoundingBox() {
+
+		String boundingBox = super.getBoundingBox();
+
+		if (boundingBox == null) {
+			String defaultValue = StringPool.POUND + ComponentUtil.escapeClientId(getClientId());
+			boundingBox = (java.lang.String) getStateHelper().eval(BOUNDING_BOX, defaultValue);
+		}
+
+		return boundingBox;
+	}
+
+	@Override
+	public String getFamily() {
+		return COMPONENT_FAMILY;
+	}
+
+	@Override
+	public Object getRender() {
+		return (Object) getStateHelper().eval(RENDER, true);
+	}
+}
---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/aceeditor/AceEditorComponentWrapper.java
@@ -0,0 +1,759 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.aceeditor;
+//J-
+
+import javax.annotation.Generated;
+import javax.faces.FacesWrapper;
+
+/**
+ * @author  Bruno Basto
+ * @author  Kyle Stiemann
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public class AceEditorComponentWrapper implements AceEditorComponent, FacesWrapper<AceEditorComponent> {
+
+	// Private Data Members
+	private AceEditorComponent wrappedAceEditorComponent;
+
+	public AceEditorComponentWrapper(AceEditorComponent aceEditorComponent) {
+		this.wrappedAceEditorComponent = aceEditorComponent;
+	}
+
+	@Override
+	public AceEditorComponent getWrapped() {
+		return wrappedAceEditorComponent;
+	}
+
+	@Override
+	public Boolean isAceEditorDisabled() {
+		return getWrapped().isAceEditorDisabled();
+	}
+
+	@Override
+	public void setAceEditorDisabled(Boolean aceEditorDisabled) {
+		getWrapped().setAceEditorDisabled(aceEditorDisabled);
+	}
+
+	@Override
+	public String getAceEditorId() {
+		return getWrapped().getAceEditorId();
+	}
+
+	@Override
+	public void setAceEditorId(String aceEditorId) {
+		getWrapped().setAceEditorId(aceEditorId);
+	}
+
+	@Override
+	public Boolean isAceEditorRendered() {
+		return getWrapped().isAceEditorRendered();
+	}
+
+	@Override
+	public void setAceEditorRendered(Boolean aceEditorRendered) {
+		getWrapped().setAceEditorRendered(aceEditorRendered);
+	}
+
+	@Override
+	public String getAceEditorValue() {
+		return getWrapped().getAceEditorValue();
+	}
+
+	@Override
+	public void setAceEditorValue(String aceEditorValue) {
+		getWrapped().setAceEditorValue(aceEditorValue);
+	}
+
+	@Override
+	public String getAfterBoundingBoxChange() {
+		return getWrapped().getAfterBoundingBoxChange();
+	}
+
+	@Override
+	public void setAfterBoundingBoxChange(String afterBoundingBoxChange) {
+		getWrapped().setAfterBoundingBoxChange(afterBoundingBoxChange);
+	}
+
+	@Override
+	public String getAfterContentBoxChange() {
+		return getWrapped().getAfterContentBoxChange();
+	}
+
+	@Override
+	public void setAfterContentBoxChange(String afterContentBoxChange) {
+		getWrapped().setAfterContentBoxChange(afterContentBoxChange);
+	}
+
+	@Override
+	public String getAfterDestroyedChange() {
+		return getWrapped().getAfterDestroyedChange();
+	}
+
+	@Override
+	public void setAfterDestroyedChange(String afterDestroyedChange) {
+		getWrapped().setAfterDestroyedChange(afterDestroyedChange);
+	}
+
+	@Override
+	public String getAfterDisabledChange() {
+		return getWrapped().getAfterDisabledChange();
+	}
+
+	@Override
+	public void setAfterDisabledChange(String afterDisabledChange) {
+		getWrapped().setAfterDisabledChange(afterDisabledChange);
+	}
+
+	@Override
+	public String getAfterFocusedChange() {
+		return getWrapped().getAfterFocusedChange();
+	}
+
+	@Override
+	public void setAfterFocusedChange(String afterFocusedChange) {
+		getWrapped().setAfterFocusedChange(afterFocusedChange);
+	}
+
+	@Override
+	public String getAfterHeightChange() {
+		return getWrapped().getAfterHeightChange();
+	}
+
+	@Override
+	public void setAfterHeightChange(String afterHeightChange) {
+		getWrapped().setAfterHeightChange(afterHeightChange);
+	}
+
+	@Override
+	public String getAfterHighlightActiveLineChange() {
+		return getWrapped().getAfterHighlightActiveLineChange();
+	}
+
+	@Override
+	public void setAfterHighlightActiveLineChange(String afterHighlightActiveLineChange) {
+		getWrapped().setAfterHighlightActiveLineChange(afterHighlightActiveLineChange);
+	}
+
+	@Override
+	public String getAfterIdChange() {
+		return getWrapped().getAfterIdChange();
+	}
+
+	@Override
+	public void setAfterIdChange(String afterIdChange) {
+		getWrapped().setAfterIdChange(afterIdChange);
+	}
+
+	@Override
+	public String getAfterInitializedChange() {
+		return getWrapped().getAfterInitializedChange();
+	}
+
+	@Override
+	public void setAfterInitializedChange(String afterInitializedChange) {
+		getWrapped().setAfterInitializedChange(afterInitializedChange);
+	}
+
+	@Override
+	public String getAfterLocaleChange() {
+		return getWrapped().getAfterLocaleChange();
+	}
+
+	@Override
+	public void setAfterLocaleChange(String afterLocaleChange) {
+		getWrapped().setAfterLocaleChange(afterLocaleChange);
+	}
+
+	@Override
+	public String getAfterModeChange() {
+		return getWrapped().getAfterModeChange();
+	}
+
+	@Override
+	public void setAfterModeChange(String afterModeChange) {
+		getWrapped().setAfterModeChange(afterModeChange);
+	}
+
+	@Override
+	public String getAfterReadOnlyChange() {
+		return getWrapped().getAfterReadOnlyChange();
+	}
+
+	@Override
+	public void setAfterReadOnlyChange(String afterReadOnlyChange) {
+		getWrapped().setAfterReadOnlyChange(afterReadOnlyChange);
+	}
+
+	@Override
+	public String getAfterRenderChange() {
+		return getWrapped().getAfterRenderChange();
+	}
+
+	@Override
+	public void setAfterRenderChange(String afterRenderChange) {
+		getWrapped().setAfterRenderChange(afterRenderChange);
+	}
+
+	@Override
+	public String getAfterRenderedChange() {
+		return getWrapped().getAfterRenderedChange();
+	}
+
+	@Override
+	public void setAfterRenderedChange(String afterRenderedChange) {
+		getWrapped().setAfterRenderedChange(afterRenderedChange);
+	}
+
+	@Override
+	public String getAfterShowPrintMarginChange() {
+		return getWrapped().getAfterShowPrintMarginChange();
+	}
+
+	@Override
+	public void setAfterShowPrintMarginChange(String afterShowPrintMarginChange) {
+		getWrapped().setAfterShowPrintMarginChange(afterShowPrintMarginChange);
+	}
+
+	@Override
+	public String getAfterSrcNodeChange() {
+		return getWrapped().getAfterSrcNodeChange();
+	}
+
+	@Override
+	public void setAfterSrcNodeChange(String afterSrcNodeChange) {
+		getWrapped().setAfterSrcNodeChange(afterSrcNodeChange);
+	}
+
+	@Override
+	public String getAfterStringsChange() {
+		return getWrapped().getAfterStringsChange();
+	}
+
+	@Override
+	public void setAfterStringsChange(String afterStringsChange) {
+		getWrapped().setAfterStringsChange(afterStringsChange);
+	}
+
+	@Override
+	public String getAfterTabIndexChange() {
+		return getWrapped().getAfterTabIndexChange();
+	}
+
+	@Override
+	public void setAfterTabIndexChange(String afterTabIndexChange) {
+		getWrapped().setAfterTabIndexChange(afterTabIndexChange);
+	}
+
+	@Override
+	public String getAfterTabSizeChange() {
+		return getWrapped().getAfterTabSizeChange();
+	}
+
+	@Override
+	public void setAfterTabSizeChange(String afterTabSizeChange) {
+		getWrapped().setAfterTabSizeChange(afterTabSizeChange);
+	}
+
+	@Override
+	public String getAfterUseSoftTabsChange() {
+		return getWrapped().getAfterUseSoftTabsChange();
+	}
+
+	@Override
+	public void setAfterUseSoftTabsChange(String afterUseSoftTabsChange) {
+		getWrapped().setAfterUseSoftTabsChange(afterUseSoftTabsChange);
+	}
+
+	@Override
+	public String getAfterUseWrapModeChange() {
+		return getWrapped().getAfterUseWrapModeChange();
+	}
+
+	@Override
+	public void setAfterUseWrapModeChange(String afterUseWrapModeChange) {
+		getWrapped().setAfterUseWrapModeChange(afterUseWrapModeChange);
+	}
+
+	@Override
+	public String getAfterValueChange() {
+		return getWrapped().getAfterValueChange();
+	}
+
+	@Override
+	public void setAfterValueChange(String afterValueChange) {
+		getWrapped().setAfterValueChange(afterValueChange);
+	}
+
+	@Override
+	public String getAfterVisibleChange() {
+		return getWrapped().getAfterVisibleChange();
+	}
+
+	@Override
+	public void setAfterVisibleChange(String afterVisibleChange) {
+		getWrapped().setAfterVisibleChange(afterVisibleChange);
+	}
+
+	@Override
+	public String getAfterWidthChange() {
+		return getWrapped().getAfterWidthChange();
+	}
+
+	@Override
+	public void setAfterWidthChange(String afterWidthChange) {
+		getWrapped().setAfterWidthChange(afterWidthChange);
+	}
+
+	@Override
+	public String getBoundingBox() {
+		return getWrapped().getBoundingBox();
+	}
+
+	@Override
+	public void setBoundingBox(String boundingBox) {
+		getWrapped().setBoundingBox(boundingBox);
+	}
+
+	@Override
+	public String getContentBox() {
+		return getWrapped().getContentBox();
+	}
+
+	@Override
+	public void setContentBox(String contentBox) {
+		getWrapped().setContentBox(contentBox);
+	}
+
+	@Override
+	public Boolean isDestroyed() {
+		return getWrapped().isDestroyed();
+	}
+
+	@Override
+	public void setDestroyed(Boolean destroyed) {
+		getWrapped().setDestroyed(destroyed);
+	}
+
+	@Override
+	public Boolean isFocused() {
+		return getWrapped().isFocused();
+	}
+
+	@Override
+	public void setFocused(Boolean focused) {
+		getWrapped().setFocused(focused);
+	}
+
+	@Override
+	public Object getHeight() {
+		return getWrapped().getHeight();
+	}
+
+	@Override
+	public void setHeight(Object height) {
+		getWrapped().setHeight(height);
+	}
+
+	@Override
+	public Boolean isHighlightActiveLine() {
+		return getWrapped().isHighlightActiveLine();
+	}
+
+	@Override
+	public void setHighlightActiveLine(Boolean highlightActiveLine) {
+		getWrapped().setHighlightActiveLine(highlightActiveLine);
+	}
+
+	@Override
+	public Boolean isInitialized() {
+		return getWrapped().isInitialized();
+	}
+
+	@Override
+	public void setInitialized(Boolean initialized) {
+		getWrapped().setInitialized(initialized);
+	}
+
+	@Override
+	public String getLocale() {
+		return getWrapped().getLocale();
+	}
+
+	@Override
+	public void setLocale(String locale) {
+		getWrapped().setLocale(locale);
+	}
+
+	@Override
+	public String getMode() {
+		return getWrapped().getMode();
+	}
+
+	@Override
+	public void setMode(String mode) {
+		getWrapped().setMode(mode);
+	}
+
+	@Override
+	public String getOnBoundingBoxChange() {
+		return getWrapped().getOnBoundingBoxChange();
+	}
+
+	@Override
+	public void setOnBoundingBoxChange(String onBoundingBoxChange) {
+		getWrapped().setOnBoundingBoxChange(onBoundingBoxChange);
+	}
+
+	@Override
+	public String getOnContentBoxChange() {
+		return getWrapped().getOnContentBoxChange();
+	}
+
+	@Override
+	public void setOnContentBoxChange(String onContentBoxChange) {
+		getWrapped().setOnContentBoxChange(onContentBoxChange);
+	}
+
+	@Override
+	public String getOnDestroyedChange() {
+		return getWrapped().getOnDestroyedChange();
+	}
+
+	@Override
+	public void setOnDestroyedChange(String onDestroyedChange) {
+		getWrapped().setOnDestroyedChange(onDestroyedChange);
+	}
+
+	@Override
+	public String getOnDisabledChange() {
+		return getWrapped().getOnDisabledChange();
+	}
+
+	@Override
+	public void setOnDisabledChange(String onDisabledChange) {
+		getWrapped().setOnDisabledChange(onDisabledChange);
+	}
+
+	@Override
+	public String getOnFocusedChange() {
+		return getWrapped().getOnFocusedChange();
+	}
+
+	@Override
+	public void setOnFocusedChange(String onFocusedChange) {
+		getWrapped().setOnFocusedChange(onFocusedChange);
+	}
+
+	@Override
+	public String getOnHeightChange() {
+		return getWrapped().getOnHeightChange();
+	}
+
+	@Override
+	public void setOnHeightChange(String onHeightChange) {
+		getWrapped().setOnHeightChange(onHeightChange);
+	}
+
+	@Override
+	public String getOnHighlightActiveLineChange() {
+		return getWrapped().getOnHighlightActiveLineChange();
+	}
+
+	@Override
+	public void setOnHighlightActiveLineChange(String onHighlightActiveLineChange) {
+		getWrapped().setOnHighlightActiveLineChange(onHighlightActiveLineChange);
+	}
+
+	@Override
+	public String getOnIdChange() {
+		return getWrapped().getOnIdChange();
+	}
+
+	@Override
+	public void setOnIdChange(String onIdChange) {
+		getWrapped().setOnIdChange(onIdChange);
+	}
+
+	@Override
+	public String getOnInitializedChange() {
+		return getWrapped().getOnInitializedChange();
+	}
+
+	@Override
+	public void setOnInitializedChange(String onInitializedChange) {
+		getWrapped().setOnInitializedChange(onInitializedChange);
+	}
+
+	@Override
+	public String getOnLocaleChange() {
+		return getWrapped().getOnLocaleChange();
+	}
+
+	@Override
+	public void setOnLocaleChange(String onLocaleChange) {
+		getWrapped().setOnLocaleChange(onLocaleChange);
+	}
+
+	@Override
+	public String getOnModeChange() {
+		return getWrapped().getOnModeChange();
+	}
+
+	@Override
+	public void setOnModeChange(String onModeChange) {
+		getWrapped().setOnModeChange(onModeChange);
+	}
+
+	@Override
+	public String getOnReadOnlyChange() {
+		return getWrapped().getOnReadOnlyChange();
+	}
+
+	@Override
+	public void setOnReadOnlyChange(String onReadOnlyChange) {
+		getWrapped().setOnReadOnlyChange(onReadOnlyChange);
+	}
+
+	@Override
+	public String getOnRenderChange() {
+		return getWrapped().getOnRenderChange();
+	}
+
+	@Override
+	public void setOnRenderChange(String onRenderChange) {
+		getWrapped().setOnRenderChange(onRenderChange);
+	}
+
+	@Override
+	public String getOnRenderedChange() {
+		return getWrapped().getOnRenderedChange();
+	}
+
+	@Override
+	public void setOnRenderedChange(String onRenderedChange) {
+		getWrapped().setOnRenderedChange(onRenderedChange);
+	}
+
+	@Override
+	public String getOnShowPrintMarginChange() {
+		return getWrapped().getOnShowPrintMarginChange();
+	}
+
+	@Override
+	public void setOnShowPrintMarginChange(String onShowPrintMarginChange) {
+		getWrapped().setOnShowPrintMarginChange(onShowPrintMarginChange);
+	}
+
+	@Override
+	public String getOnSrcNodeChange() {
+		return getWrapped().getOnSrcNodeChange();
+	}
+
+	@Override
+	public void setOnSrcNodeChange(String onSrcNodeChange) {
+		getWrapped().setOnSrcNodeChange(onSrcNodeChange);
+	}
+
+	@Override
+	public String getOnStringsChange() {
+		return getWrapped().getOnStringsChange();
+	}
+
+	@Override
+	public void setOnStringsChange(String onStringsChange) {
+		getWrapped().setOnStringsChange(onStringsChange);
+	}
+
+	@Override
+	public String getOnTabIndexChange() {
+		return getWrapped().getOnTabIndexChange();
+	}
+
+	@Override
+	public void setOnTabIndexChange(String onTabIndexChange) {
+		getWrapped().setOnTabIndexChange(onTabIndexChange);
+	}
+
+	@Override
+	public String getOnTabSizeChange() {
+		return getWrapped().getOnTabSizeChange();
+	}
+
+	@Override
+	public void setOnTabSizeChange(String onTabSizeChange) {
+		getWrapped().setOnTabSizeChange(onTabSizeChange);
+	}
+
+	@Override
+	public String getOnUseSoftTabsChange() {
+		return getWrapped().getOnUseSoftTabsChange();
+	}
+
+	@Override
+	public void setOnUseSoftTabsChange(String onUseSoftTabsChange) {
+		getWrapped().setOnUseSoftTabsChange(onUseSoftTabsChange);
+	}
+
+	@Override
+	public String getOnUseWrapModeChange() {
+		return getWrapped().getOnUseWrapModeChange();
+	}
+
+	@Override
+	public void setOnUseWrapModeChange(String onUseWrapModeChange) {
+		getWrapped().setOnUseWrapModeChange(onUseWrapModeChange);
+	}
+
+	@Override
+	public String getOnValueChange() {
+		return getWrapped().getOnValueChange();
+	}
+
+	@Override
+	public void setOnValueChange(String onValueChange) {
+		getWrapped().setOnValueChange(onValueChange);
+	}
+
+	@Override
+	public String getOnVisibleChange() {
+		return getWrapped().getOnVisibleChange();
+	}
+
+	@Override
+	public void setOnVisibleChange(String onVisibleChange) {
+		getWrapped().setOnVisibleChange(onVisibleChange);
+	}
+
+	@Override
+	public String getOnWidthChange() {
+		return getWrapped().getOnWidthChange();
+	}
+
+	@Override
+	public void setOnWidthChange(String onWidthChange) {
+		getWrapped().setOnWidthChange(onWidthChange);
+	}
+
+	@Override
+	public Boolean isReadOnly() {
+		return getWrapped().isReadOnly();
+	}
+
+	@Override
+	public void setReadOnly(Boolean readOnly) {
+		getWrapped().setReadOnly(readOnly);
+	}
+
+	@Override
+	public Object getRender() {
+		return getWrapped().getRender();
+	}
+
+	@Override
+	public void setRender(Object render) {
+		getWrapped().setRender(render);
+	}
+
+	@Override
+	public Boolean isShowPrintMargin() {
+		return getWrapped().isShowPrintMargin();
+	}
+
+	@Override
+	public void setShowPrintMargin(Boolean showPrintMargin) {
+		getWrapped().setShowPrintMargin(showPrintMargin);
+	}
+
+	@Override
+	public String getSrcNode() {
+		return getWrapped().getSrcNode();
+	}
+
+	@Override
+	public void setSrcNode(String srcNode) {
+		getWrapped().setSrcNode(srcNode);
+	}
+
+	@Override
+	public Object getStrings() {
+		return getWrapped().getStrings();
+	}
+
+	@Override
+	public void setStrings(Object strings) {
+		getWrapped().setStrings(strings);
+	}
+
+	@Override
+	public Object getTabIndex() {
+		return getWrapped().getTabIndex();
+	}
+
+	@Override
+	public void setTabIndex(Object tabIndex) {
+		getWrapped().setTabIndex(tabIndex);
+	}
+
+	@Override
+	public Object getTabSize() {
+		return getWrapped().getTabSize();
+	}
+
+	@Override
+	public void setTabSize(Object tabSize) {
+		getWrapped().setTabSize(tabSize);
+	}
+
+	@Override
+	public Boolean isUseSoftTabs() {
+		return getWrapped().isUseSoftTabs();
+	}
+
+	@Override
+	public void setUseSoftTabs(Boolean useSoftTabs) {
+		getWrapped().setUseSoftTabs(useSoftTabs);
+	}
+
+	@Override
+	public Boolean isUseWrapMode() {
+		return getWrapped().isUseWrapMode();
+	}
+
+	@Override
+	public void setUseWrapMode(Boolean useWrapMode) {
+		getWrapped().setUseWrapMode(useWrapMode);
+	}
+
+	@Override
+	public Boolean isVisible() {
+		return getWrapped().isVisible();
+	}
+
+	@Override
+	public void setVisible(Boolean visible) {
+		getWrapped().setVisible(visible);
+	}
+
+	@Override
+	public Object getWidth() {
+		return getWrapped().getWidth();
+	}
+
+	@Override
+	public void setWidth(Object width) {
+		getWrapped().setWidth(width);
+	}
+}
+//J+
