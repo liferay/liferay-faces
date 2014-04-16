69e2701b5f8e88faf1af22cc33e556db7c0f8ebe
Neil Griffin <neil.griffin.scm@gmail.com>
2014-04-16 18:35:28 -0400

    FACES-1714 Develop Liferay Faces Showcase (added portlet:actionURL)

---
+++ demos/showcase/showcase-portlet/src/main/java/com/liferay/faces/demos/portlet/ActionURLDemoPortlet.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.portlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.faces.render.ResponseStateManager;
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.faces.GenericFacesPortlet;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class ActionURLDemoPortlet extends GenericFacesPortlet {
+
+	// Private Constants
+	private static final String NON_FACES_POSTBACK = "Non-Faces-Postback";
+
+	@Override
+	public void processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortletException,
+		IOException {
+
+		String viewState = actionRequest.getParameter(ResponseStateManager.VIEW_STATE_PARAM);
+
+		if (viewState == null) {
+			actionResponse.setRenderParameter(NON_FACES_POSTBACK, Boolean.TRUE.toString());
+			actionResponse.setRenderParameter("foo", actionRequest.getParameter("foo"));
+		}
+		else {
+			super.processAction(actionRequest, actionResponse);
+		}
+	}
+
+	@Override
+	protected void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException,
+		IOException {
+
+		String nonFacesPostback = renderRequest.getParameter(NON_FACES_POSTBACK);
+
+		if (nonFacesPostback == null) {
+			super.doView(renderRequest, renderResponse);
+		}
+		else {
+			PrintWriter writer = renderResponse.getWriter();
+			writer.write("<p>");
+			writer.write("Successfully executed ");
+			writer.write("<strong>non-JSF postback</strong> in ");
+			writer.write(ActionURLDemoPortlet.class.getName());
+			writer.write("</p>");
+
+			String foo = renderRequest.getParameter("foo");
+			writer.write("<p>");
+			writer.write("Action Parameter <strong>foo</strong>=" + foo);
+			writer.write("</p>");
+
+			PortletURL renderURL = renderResponse.createRenderURL();
+			renderURL.setParameter("componentPrefix", "portlet");
+			renderURL.setParameter("componentName", "actionurl");
+			renderURL.setParameter("componentUseCase", "general");
+			writer.write("<p>");
+			writer.write("<a href=\"" + renderURL.toString() + "\">");
+			writer.write("Return to portlet:actionURL in the Liferay Faces Showcase");
+			writer.write("</a>");
+			writer.write("</p>");
+		}
+	}
+}
