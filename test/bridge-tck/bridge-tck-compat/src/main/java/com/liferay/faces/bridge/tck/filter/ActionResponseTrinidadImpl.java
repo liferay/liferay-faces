df0f29e076b1a94af855e8ba91166ffd37b93e00
Neil Griffin <neil.griffin.scm@gmail.com>
2014-04-22 17:09:55 -0400

    FACES-1920 IllegalStateException when executing JSR 329 TCK TestPage039 (requestNoScopeOnRedirectTest) and TestPage176 (redirectActionTest)

---
+++ test/bridge-tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/filter/ActionResponseTrinidadImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.tck.filter;
+
+import javax.portlet.ActionResponse;
+import javax.portlet.filter.ActionResponseWrapper;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class ActionResponseTrinidadImpl extends ActionResponseWrapper {
+
+	public ActionResponseTrinidadImpl(ActionResponse response) {
+		super(response);
+	}
+
+	@Override
+	public void setRenderParameter(String key, String value) {
+
+		try {
+			super.setRenderParameter(key, value);
+		}
+		catch (IllegalStateException e) {
+			// Ignore -- workaround for https://issues.liferay.com/browse/FACES-1920
+		}
+	}
+}
