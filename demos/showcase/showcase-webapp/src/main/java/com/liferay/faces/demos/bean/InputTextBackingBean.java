25bc88d1af76959a94afd1a6491a17b838691876
Neil Griffin <neil.griffin.scm@gmail.com>
2014-05-01 18:34:54 -0400

    FACES-1714 Develop Liferay Faces Showcase (added use cases for h:inputTextarea)

---
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/InputTextBackingBean.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@ManagedBean
+@RequestScoped
+public class InputTextBackingBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(InputTextBackingBean.class);
+
+	@ManagedProperty(value = "#{inputTextModelBean}")
+	private InputTextModelBean inputTextModelBean;
+
+	public void emailAddressValidator(FacesContext facesContext, UIComponent uiComponent, Object value)
+		throws ValidatorException {
+
+		if (value != null) {
+
+			if (!value.toString().matches(".+[@].+[.].+")) {
+				FacesMessage facesMessage = new FacesMessage();
+				facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+				throw new ValidatorException(facesMessage);
+			}
+		}
+	}
+
+	public void submit() {
+
+		Object value = inputTextModelBean.getText();
+
+		if (value == null) {
+			value = inputTextModelBean.getInteger();
+		}
+
+		logger.info("You entered: " + value);
+	}
+
+	public void setInputTextModelBean(InputTextModelBean inputTextModelBean) {
+		this.inputTextModelBean = inputTextModelBean;
+	}
+}
