952f08a70e0e1cdb6469b56c6192c5606e794108
Neil Griffin <neil.griffin.scm@gmail.com>
2014-05-09 10:37:09 -0400

    FACES-1928 Remove compile-time dependency on portal-service.jar from Liferay Faces Alloy

---
+++ util/src/main/java/com/liferay/faces/util/client/ClientScriptFactory.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.client;
+
+import javax.faces.FacesException;
+import javax.faces.context.ExternalContext;
+
+import com.liferay.faces.util.factory.FactoryExtension;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class ClientScriptFactory implements FactoryExtension<ClientScriptFactory> {
+
+	public abstract ClientScript getClientScript(ExternalContext externalContext) throws FacesException;
+}
--- util/src/main/java/com/liferay/faces/util/client/ClientScriptLiferayImpl.java
+++ util/src/main/java/com/liferay/faces/util/client/ClientScriptLiferayImpl.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.util.portal;
+package com.liferay.faces.util.client;
 
 import java.lang.reflect.Method;
 
@@ -21,30 +21,30 @@ import com.liferay.faces.util.product.Product;
 import com.liferay.faces.util.product.ProductConstants;
 import com.liferay.faces.util.product.ProductMap;
 
+import com.liferay.portal.kernel.servlet.taglib.aui.ScriptData;
+
 
 /**
- * This class serves as a facade for instances of the Liferay Portal {@link ScriptData} class. Some of the method
- * signatures of the class have undergone changes from Liferay Portal 6.1.1 -> 6.1.2 which requires the use of version
- * detection and reflection in this class.
- *
  * @author  Neil Griffin
  */
-public class ScriptDataUtil {
+public class ClientScriptLiferayImpl implements ClientScript {
 
 	// Logger
-	private static final Logger logger = LoggerFactory.getLogger(ScriptDataUtil.class);
+	private static final Logger logger = LoggerFactory.getLogger(ClientScriptLiferayImpl.class);
 
 	// Private Constants
 	private static final String APPEND = "append";
 	private static final Method APPEND_METHOD_LEGACY;
 	private static final Method APPEND_METHOD_CURRENT;
-	private static final String SCRIPT_DATA_FQCN = "com.liferay.portal.kernel.servlet.taglib.aui.ScriptData";
+	private static final boolean LIFERAY_PORTAL_DETECTED;
 
 	static {
 
 		Product liferayPortal = ProductMap.getInstance().get(ProductConstants.LIFERAY_PORTAL);
 
-		if (liferayPortal.isDetected()) {
+		LIFERAY_PORTAL_DETECTED = liferayPortal.isDetected();
+
+		if (LIFERAY_PORTAL_DETECTED) {
 
 			int buildNumber = liferayPortal.getBuildId();
 
@@ -53,8 +53,7 @@ public class ScriptDataUtil {
 				Method appendMethodLegacy = null;
 
 				try {
-					Class<?> scriptDataClass = Class.forName(SCRIPT_DATA_FQCN);
-					appendMethodLegacy = scriptDataClass.getMethod(APPEND, new Class[] { String.class, String.class });
+					appendMethodLegacy = ScriptData.class.getMethod(APPEND, new Class[] { String.class, String.class });
 				}
 				catch (Exception e) {
 					logger.error(e);
@@ -67,8 +66,7 @@ public class ScriptDataUtil {
 				Method appendMethodCurrent = null;
 
 				try {
-					Class<?> scriptDataClass = Class.forName(SCRIPT_DATA_FQCN);
-					appendMethodCurrent = scriptDataClass.getMethod(APPEND,
+					appendMethodCurrent = ScriptData.class.getMethod(APPEND,
 							new Class[] { String.class, String.class, String.class });
 				}
 				catch (Exception e) {
@@ -85,7 +83,15 @@ public class ScriptDataUtil {
 		}
 	}
 
-	public static void append(Object scriptData, String portletId, String content, String use) {
+	// Private Data Members
+	private Object scriptData;
+
+	public ClientScriptLiferayImpl(Object scriptData) {
+		this.scriptData = scriptData;
+	}
+
+	@Override
+	public void append(String portletId, String content, String use) {
 
 		try {
 
