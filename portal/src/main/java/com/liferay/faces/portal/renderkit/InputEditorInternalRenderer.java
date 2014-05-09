152b8de16b827940841195762cf67c2616573d6a
Neil Griffin <neil.griffin.scm@gmail.com>
2014-05-09 11:41:37 -0400

    FACES-1928 Remove compile-time dependency on portal-service.jar from Liferay Faces Alloy

--- portal/src/main/java/com/liferay/faces/portal/renderkit/InputEditorInternalRenderer.java
+++ portal/src/main/java/com/liferay/faces/portal/renderkit/InputEditorInternalRenderer.java
@@ -43,7 +43,6 @@ import com.liferay.faces.util.lang.StringPool;
 import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
 import com.liferay.faces.util.portal.EditorUtil;
-import com.liferay.faces.util.portal.ScriptDataUtil;
 import com.liferay.faces.util.portal.ScriptTagUtil;
 import com.liferay.faces.util.portal.WebKeys;
 import com.liferay.faces.util.render.CleanupRenderer;
