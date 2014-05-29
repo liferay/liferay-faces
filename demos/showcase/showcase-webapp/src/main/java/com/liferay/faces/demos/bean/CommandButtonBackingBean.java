c67e60a0e618e6262e96dbe341333c91918c5323
kylestiemann <kyle.stiemann@liferay.com>
2014-05-29 16:44:00 -0400

    FACES-1896 Develop alloy:button, alloy:commandButton, and alloy:icon component (Added immediate use case for commandButton.)

--- demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/CommandButtonBackingBean.java
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/CommandButtonBackingBean.java
@@ -13,13 +13,18 @@
  */
 package com.liferay.faces.demos.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.component.UICommand;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.PhaseId;
 
 import com.liferay.faces.demos.dto.Customer;
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
 
 
 /**
@@ -29,9 +34,23 @@ import com.liferay.faces.demos.dto.Customer;
 @RequestScoped
 public class CommandButtonBackingBean {
 
+	private static final Logger logger = LoggerFactory.getLogger(CommandButtonBackingBean.class);
+
 	// Injections
 	@ManagedProperty(value = "#{commandButtonModelBean}")
 	private CommandButtonModelBean commandButtonModelBean;
+
+	public void actionListener(ActionEvent actionEvent) {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		PhaseId phaseId = facesContext.getCurrentPhaseId();
+		logger.debug("actionListener: phaseId=[{0}]", phaseId.toString());
+
+		String phaseName = phaseId.getName();
+		FacesMessage facesMessage = new FacesMessage("The actionListener method was called during the " + phaseName +
+				" phase of the JSF lifecycle.");
+		facesContext.addMessage(null, facesMessage);
+	}
 
 	public void selectionListener(ActionEvent actionEvent) {
 
