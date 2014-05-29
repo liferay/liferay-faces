195022655a0fac7a0bee4a86f6e3ede2230f4979
Neil Griffin <neil.griffin.scm@gmail.com>
2014-05-29 13:38:08 -0400

    FACES-1932 Develop alloy:panelGroup component

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/panelgroup/PanelGroupRenderer.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.panelgroup;
+
+import javax.faces.render.FacesRenderer;
+
+import com.liferay.faces.util.render.DelegatingRendererBase;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+@FacesRenderer(componentFamily = PanelGroup.COMPONENT_FAMILY, rendererType = PanelGroup.RENDERER_TYPE)
+public class PanelGroupRenderer extends DelegatingRendererBase {
+
+	@Override
+	public String getDelegateComponentFamily() {
+		return PanelGroup.DELEGATE_COMPONENT_FAMILY;
+	}
+
+	@Override
+	public String getDelegateRendererType() {
+		return PanelGroup.DELEGATE_RENDERER_TYPE;
+	}
+}
