1ccd1404fc0b533f268cc6069e016bae81126a22
Neil Griffin <neil.griffin.scm@gmail.com>
2014-06-04 18:12:28 -0400

    FACES-1944 Develop alloy:message and alloy:messages components

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/message/Message.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.message;
+
+import javax.faces.component.FacesComponent;
+
+import com.liferay.faces.util.component.ComponentUtil;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@FacesComponent(value = Message.COMPONENT_TYPE)
+public class Message extends MessageBase {
+
+	// Public Constants
+	public static final String COMPONENT_TYPE = "com.liferay.faces.alloy.component.message.Message";
+	public static final String DELEGATE_COMPONENT_FAMILY = COMPONENT_FAMILY;
+	public static final String DELEGATE_RENDERER_TYPE = "javax.faces.Message";
+	public static final String RENDERER_TYPE = "com.liferay.faces.alloy.component.message.MessageRenderer";
+	public static final String STYLE_CLASS_NAME = "help-inline alloy-message";
+
+	public Message() {
+		super();
+		setRendererType(RENDERER_TYPE);
+	}
+
+	@Override
+	public String getStyleClass() {
+
+		// getStateHelper().eval(STYLE_CLASS, null) is called because super.getStyleClass() may return the
+		// STYLE_CLASS_NAME of the super class.
+		String styleClass = (String) getStateHelper().eval(STYLE_CLASS, null);
+
+		return ComponentUtil.concatCssClasses(styleClass, STYLE_CLASS_NAME);
+	}
+}
---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/message/MessageRenderer.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.message;
+
+import javax.faces.render.FacesRenderer;
+
+import com.liferay.faces.util.render.DelegatingRendererBase;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@FacesRenderer(componentFamily = Message.COMPONENT_FAMILY, rendererType = Message.RENDERER_TYPE)
+public class MessageRenderer extends DelegatingRendererBase {
+
+	@Override
+	public String getDelegateComponentFamily() {
+		return Message.DELEGATE_COMPONENT_FAMILY;
+	}
+
+	@Override
+	public String getDelegateRendererType() {
+		return Message.DELEGATE_RENDERER_TYPE;
+	}
+}
