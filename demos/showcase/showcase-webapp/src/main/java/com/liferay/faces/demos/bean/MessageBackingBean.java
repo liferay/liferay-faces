4a1a7ec04ae634514595c391da6bed8a660281e5
Neil Griffin <neil.griffin.scm@gmail.com>
2014-06-06 17:48:20 -0400

    FACES-1714 Develop Liferay Faces Showcase (use cases for alloy:message and alloy:messages)

---
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/MessageBackingBean.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@RequestScoped
+@ManagedBean
+public class MessageBackingBean {
+
+	public void submit() {
+
+		FacesMessage globalFacesMessage = new FacesMessage("Your request processed successfully.");
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		facesContext.addMessage(null, globalFacesMessage);
+	}
+
+	public void valueChangeListener(ValueChangeEvent valueChangeEvent) {
+
+		Object newValue = valueChangeEvent.getNewValue();
+		int totalChars = 0;
+
+		if (newValue != null) {
+			totalChars = newValue.toString().length();
+		}
+
+		FacesMessage globalFacesMessage = new FacesMessage("You typed " + totalChars + " characters.");
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		String componentClientId = valueChangeEvent.getComponent().getClientId();
+		facesContext.addMessage(componentClientId, globalFacesMessage);
+	}
+}
