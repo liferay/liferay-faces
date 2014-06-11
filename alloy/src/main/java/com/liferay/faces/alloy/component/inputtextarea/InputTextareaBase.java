ca08ce5a14e1d68d7c609cc899d3bcc892ac208b
Neil Griffin <neil.griffin.scm@gmail.com>
2014-06-11 14:45:23 -0400

    FACES-1806 Develop alloy:inputTextArea component (extend generated base class for consistency)

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/inputtextarea/InputTextareaBase.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.inputtextarea;
+//J-
+
+import javax.annotation.Generated;
+import javax.faces.component.html.HtmlInputTextarea;
+
+import com.liferay.faces.util.component.Styleable;
+
+/**
+ * @author  Bruno Basto
+ * @author  Kyle Stiemann
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public abstract class InputTextareaBase extends HtmlInputTextarea implements Styleable {
+
+	// Public Constants
+	public static final String STYLE = "style";
+	public static final String STYLE_CLASS = "styleClass";
+
+	@Override
+	public String getStyle() {
+		return (String) getStateHelper().eval(STYLE, null);
+	}
+
+	@Override
+	public void setStyle(String style) {
+		getStateHelper().put(STYLE, style);
+	}
+
+	@Override
+	public String getStyleClass() {
+		return (String) getStateHelper().eval(STYLE_CLASS, null);
+	}
+
+	@Override
+	public void setStyleClass(String styleClass) {
+		getStateHelper().put(STYLE_CLASS, styleClass);
+	}
+}
+//J+
