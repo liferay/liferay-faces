031beccadce17400a0ab305e1dc434823d9361ae
Vernon <vsingleton@gmail.com>
2014-06-26 15:50:22 -0400

    FACES-1807 Please consider these components for the showcase.

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/inputsecret/InputSecretRenderer.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.inputsecret;
+
+import javax.faces.render.FacesRenderer;
+
+
+/**
+ * @author  Vernon Singleton
+ */
+@FacesRenderer(componentFamily = InputSecret.COMPONENT_FAMILY, rendererType = InputSecret.RENDERER_TYPE)
+public class InputSecretRenderer extends InputSecretRendererBase {
+
+	@Override
+	public String getDelegateComponentFamily() {
+		return InputSecret.DELEGATE_COMPONENT_FAMILY;
+	}
+
+	@Override
+	public String getDelegateRendererType() {
+		return InputSecret.DELEGATE_RENDERER_TYPE;
+	}
+}
---
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/InputSecretBackingBean.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.validator.ValidatorException;
+
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
+
+
+/**
+ * @author  Vernon Singleton
+ */
+@ManagedBean
+@RequestScoped
+public class InputSecretBackingBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(InputSecretBackingBean.class);
+
+	@ManagedProperty(value = "#{inputSecretModelBean}")
+	private InputSecretModelBean inputSecretModelBean;
+
+	public void emailAddressValidator(FacesContext facesContext, UIComponent uiComponent, Object value)
+		throws ValidatorException {
+
+		if (value != null) {
+
+			if (!value.toString().matches(".+[@].+[.].+")) {
+				FacesMessage facesMessage = new FacesMessage();
+				facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+				throw new ValidatorException(facesMessage);
+			}
+		}
+	}
+
+	public void submit() {
+
+		Object value = inputSecretModelBean.getText();
+
+		if (value == null) {
+			value = inputSecretModelBean.getDate();
+		}
+
+		logger.info("You entered: " + value);
+	}
+
+	public void valueChangeListener(ValueChangeEvent valueChangeEvent) {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		PhaseId phaseId = facesContext.getCurrentPhaseId();
+		logger.debug("valueChangeListener: phaseId=[{0}]", phaseId.toString());
+
+		String phaseName = phaseId.getName();
+		FacesMessage facesMessage = new FacesMessage("The valueChangeListener method was called during the " + phaseName + " phase of the JSF lifecycle.");
+		facesContext.addMessage(null, facesMessage);
+	}
+
+	public void setInputSecretModelBean(InputSecretModelBean inputSecretModelBean) {
+		this.inputSecretModelBean = inputSecretModelBean;
+	}
+}
