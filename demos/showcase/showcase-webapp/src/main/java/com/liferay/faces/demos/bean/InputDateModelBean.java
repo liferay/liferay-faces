eac4091c5c85e47d17cc555833934119c8c8bb1a
Neil Griffin <neil.griffin.scm@gmail.com>
2014-07-26 14:20:48 -0400

    FACES-1714 Develop Liferay Faces Showcase (removed unused import in InputDateModelBean.java)

--- demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/InputDateModelBean.java
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/InputDateModelBean.java
@@ -17,7 +17,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
-import java.util.TimeZone;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
