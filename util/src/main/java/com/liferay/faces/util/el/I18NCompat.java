bd680315e9c2022568727769004e1b1bf13a0be1
Neil Griffin <neil.griffin.scm@gmail.com>
2014-08-07 13:07:02 -0400

    FACES-1960 i18n EL keyword has an underlying cache that is not synchronized FACES-1961 Enable cache for i18n EL keyword only when ProjectStage != Development

---
+++ util/src/main/java/com/liferay/faces/util/el/I18NCompat.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.el;
+
+import java.util.ResourceBundle;
+
+import javax.faces.application.ProjectStage;
+import javax.faces.context.FacesContext;
+
+
+/**
+ * This class provides a compatibility layer that isolates differences between JSF1 and JSF2.
+ *
+ * @author  Neil Griffin
+ */
+public abstract class I18NCompat extends ResourceBundle {
+
+	// Protected Data Members
+	protected boolean cacheEnabled;
+
+	public I18NCompat() {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		if (facesContext == null) {
+			this.cacheEnabled = true;
+		}
+		else {
+			this.cacheEnabled = !facesContext.isProjectStage(ProjectStage.Development);
+		}
+	}
+}
