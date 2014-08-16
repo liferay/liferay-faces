1628ea7d2da71e0d7cf92da581d5ac620cd73350
Neil Griffin <neil.griffin.scm@gmail.com>
2014-08-16 18:03:21 -0400

    FACES-1894 Add general utilities for Liferay Faces Alloy Components (added interfaces and base classes for delegation of rendering)

---
+++ util/src/main/java/com/liferay/faces/util/render/DelegationResponseWriterBase.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.render;
+
+import javax.faces.context.ResponseWriter;
+import javax.faces.context.ResponseWriterWrapper;
+
+
+/**
+ * This class provides a concrete implementation of {@link ResponseWriterWrapper} that is intended to be used (or
+ * extended) by renderers that implement {@link RendererDelegator}.
+ *
+ * @author  Neil Griffin
+ */
+public class DelegationResponseWriterBase extends DelegationResponseWriter {
+
+	// Private Data Members
+	private ResponseWriter wrappedResponseWriter;
+
+	public DelegationResponseWriterBase(ResponseWriter responseWriter) {
+		this.wrappedResponseWriter = responseWriter;
+	}
+
+	@Override
+	public ResponseWriter getWrapped() {
+		return wrappedResponseWriter;
+	}
+}
