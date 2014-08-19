77ded9fd1dd79a72c27a0fe2518b17162fac6718
Neil Griffin <neil.griffin.scm@gmail.com>
2014-08-19 15:31:05 -0400

    FACES-1972 Develop alloy:inputFile component and associated alloy:validateFile validator (refactored to use javascript functions in alloy.js instead of javascript templates)

---
+++ util/src/main/java/com/liferay/faces/util/js/JavaScriptFragment.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.js;
+
+import java.io.Serializable;
+
+
+/**
+ * This is a simple marker class that wraps a String. It marks the fact that the wrapped string is a fragment of
+ * JavaScript code.
+ *
+ * @author  Neil Griffin
+ */
+public final class JavaScriptFragment implements Serializable {
+
+	// serialVersionUID
+	private static final long serialVersionUID = 5918907480864436697L;
+
+	// Private Data Members
+	private String value;
+
+	public JavaScriptFragment(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+}
