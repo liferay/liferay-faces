367ada9df410dcb46778f8592a7286ce54e6c464
Neil Griffin <neil.griffin.scm@gmail.com>
2014-08-20 14:45:19 -0400

    FACES-1974 Develop alloy:head component FACES-1714 Develop Liferay Faces Showcase (added use-case for alloy:head and using alloy:head within the showcase itself)

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/head/HeadBase.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.head;
+//J-
+
+import javax.annotation.Generated;
+import javax.faces.component.html.HtmlHead;
+
+import com.liferay.faces.util.component.Styleable;
+
+/**
+ * @author	Bruno Basto
+ * @author	Kyle Stiemann
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public abstract class HeadBase extends HtmlHead implements Styleable {
+
+	// Protected Enumerations
+	protected enum HeadPropertyKeys {
+		style,
+		styleClass
+	}
+
+	@Override
+	public String getStyle() {
+		return (String) getStateHelper().eval(HeadPropertyKeys.style, null);
+	}
+
+	@Override
+	public void setStyle(String style) {
+		getStateHelper().put(HeadPropertyKeys.style, style);
+	}
+
+	@Override
+	public String getStyleClass() {
+		return (String) getStateHelper().eval(HeadPropertyKeys.styleClass, null);
+	}
+
+	@Override
+	public void setStyleClass(String styleClass) {
+		getStateHelper().put(HeadPropertyKeys.styleClass, styleClass);
+	}
+}
+//J+
---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/head/HeadRenderer.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.head;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.render.FacesRenderer;
+
+
+/**
+ * @author  Neil Griffin
+ */
+//J-
+@FacesRenderer(componentFamily = Head.COMPONENT_FAMILY, rendererType = Head.RENDERER_TYPE)
+@ResourceDependencies(
+		{
+			@ResourceDependency(library = "liferay-faces-alloy", name = "alloy.css"),
+			@ResourceDependency(library = "liferay-faces-alloy", name = "alloy.js"),
+			@ResourceDependency(library = "liferay-faces-alloy", name = "build/aui-css/css/bootstrap.min.css"),
+			@ResourceDependency(library = "liferay-faces-alloy", name = "build/aui/aui-min.js"),
+			@ResourceDependency(library = "liferay-faces-alloy", name = "liferay.js")
+		}
+	)
+//J+
+public class HeadRenderer extends HeadRendererBase {
+
+	@Override
+	public String getDelegateComponentFamily() {
+		return Head.DELEGATE_COMPONENT_FAMILY;
+	}
+
+	@Override
+	public String getDelegateRendererType() {
+		return Head.DELEGATE_RENDERER_TYPE;
+	}
+}
