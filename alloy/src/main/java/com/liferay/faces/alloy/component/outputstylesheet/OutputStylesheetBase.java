ce59ffb481934fbfc2b82fa8a6a8860941c01ed1
Neil Griffin <neil.griffin.scm@gmail.com>
2014-08-27 17:15:32 -0400

    FACES-1957 Develop alloy:outputStylesheet component FACES-1714 Develop Liferay Faces Showcase (added use-case for alloy:outputStylesheet)

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/outputstylesheet/OutputStylesheetBase.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.component.outputstylesheet;
+//J-
+
+import javax.annotation.Generated;
+import javax.faces.component.UIOutput;
+
+
+/**
+ * @author	Bruno Basto
+ * @author	Kyle Stiemann
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public abstract class OutputStylesheetBase extends UIOutput {
+
+	// Protected Enumerations
+	protected enum OutputStylesheetPropertyKeys {
+		library,
+		media,
+		name
+	}
+
+	public String getLibrary() {
+		return (String) getStateHelper().eval(OutputStylesheetPropertyKeys.library, null);
+	}
+
+	public void setLibrary(String library) {
+		getStateHelper().put(OutputStylesheetPropertyKeys.library, library);
+	}
+
+	public String getMedia() {
+		return (String) getStateHelper().eval(OutputStylesheetPropertyKeys.media, null);
+	}
+
+	public void setMedia(String media) {
+		getStateHelper().put(OutputStylesheetPropertyKeys.media, media);
+	}
+
+	public String getName() {
+		return (String) getStateHelper().eval(OutputStylesheetPropertyKeys.name, null);
+	}
+
+	public void setName(String name) {
+		getStateHelper().put(OutputStylesheetPropertyKeys.name, name);
+	}
+}
+//J+
