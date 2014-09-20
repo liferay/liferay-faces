92dbe63d547d41730dbb1edc7c41887832503fc5
Neil Griffin <neil.griffin.scm@gmail.com>
2014-09-20 17:51:46 -0400

    FACES-1989 Develop RichText interface and corresponding RichTextFactory for representing BBCode, Creole, and HTML rich text

---
+++ util/src/main/java/com/liferay/faces/util/text/internal/RichTextBaseImpl.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.text.internal;
+
+import com.liferay.faces.util.text.RichText;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class RichTextBaseImpl implements RichText {
+
+	// Private Data Members
+	private String value;
+
+	public RichTextBaseImpl(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	@Override
+	public int getPlainTextLength() {
+
+		int count = 0;
+
+		if (value != null) {
+			count = value.length();
+		}
+
+		return count;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
