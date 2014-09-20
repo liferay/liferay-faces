86e3c3c5a7fc0b71c575fc1f98089dd44e2f6f07
Neil Griffin <neil.griffin.scm@gmail.com>
2014-09-20 17:52:12 -0400

    FACES-1989 Develop RichText interface and corresponding RichTextFactory for representing BBCode, Creole, and HTML rich text

---
+++ util/src/main/java/com/liferay/faces/util/text/internal/RichTextFactoryImpl.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.text.internal;
+
+import com.liferay.faces.util.text.RichText;
+import com.liferay.faces.util.text.RichTextFactory;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class RichTextFactoryImpl extends RichTextFactory {
+
+	@Override
+	public RichText getRichText(RichText.Type type, String value) {
+
+		switch (type) {
+
+		case BBCODE: {
+			return new RichTextBBCodeImpl(value);
+		}
+
+		case CREOLE: {
+			return new RichTextCreoleImpl(value);
+		}
+
+		default: {
+			return new RichTextHTMLImpl(value);
+		}
+		}
+	}
+
+	@Override
+	public RichTextFactory getWrapped() {
+
+		// Since this is the factory instance provided by default, it will never wrap another factory.
+		return null;
+	}
+}
