47bc119117ff5223fb6ca0a6d30f4ba424795c63
Neil Griffin <neil.griffin.scm@gmail.com>
2014-09-20 17:52:25 -0400

    FACES-1989 Develop RichText interface and corresponding RichTextFactory for representing BBCode, Creole, and HTML rich text

---
+++ util/src/main/java/com/liferay/faces/util/text/internal/RichTextHTMLImpl.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.text.internal;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.liferay.faces.util.lang.StringPool;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class RichTextHTMLImpl extends RichTextBaseImpl {
+
+	// Private Constants
+	private static final Pattern TAG_PATTERN = Pattern.compile("<.+?>");
+	private static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\s+");
+
+	public RichTextHTMLImpl(String value) {
+		super(value);
+	}
+
+	@Override
+	public int getPlainTextLength() {
+
+		int count = 0;
+
+		String value = getValue();
+
+		if (value != null) {
+			String cleanString = null;
+
+			if (value instanceof String) {
+				cleanString = (String) value;
+			}
+			else {
+				cleanString = value.toString();
+			}
+
+			Matcher tagMatcher = TAG_PATTERN.matcher(cleanString);
+
+			cleanString = tagMatcher.replaceAll(StringPool.BLANK);
+			cleanString = cleanString.replaceAll("&nbsp;", StringPool.SPACE);
+			cleanString = WHITESPACE_PATTERN.matcher(cleanString).replaceAll(StringPool.SPACE);
+			cleanString = cleanString.trim();
+			count = cleanString.length();
+		}
+
+		return count;
+	}
+
+	@Override
+	public Type getType() {
+		return Type.HTML;
+	}
+}
