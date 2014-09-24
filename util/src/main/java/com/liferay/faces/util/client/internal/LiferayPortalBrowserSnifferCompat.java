ac69c7800ffa3119abbc3a86a67d5fb4a8ff42a4
Neil Griffin <neil.griffin.scm@gmail.com>
2014-09-24 17:01:39 -0400

    FACES-287 Develop browserSniffer EL keyword (fixed bug in isNotNull)

--- util/src/main/java/com/liferay/faces/util/client/internal/LiferayPortalBrowserSnifferCompat.java
+++ util/src/main/java/com/liferay/faces/util/client/internal/LiferayPortalBrowserSnifferCompat.java
@@ -41,6 +41,9 @@ public class LiferayPortalBrowserSnifferCompat {
 		public static final char BACK_SLASH = '\\';
 		public static final char COLON = ':';
 		public static final char FORWARD_SLASH = '/';
+		public static final char LOWER_CASE_L = 'l';
+		public static final char LOWER_CASE_N = 'n';
+		public static final char LOWER_CASE_U = 'u';
 		public static final char SLASH = FORWARD_SLASH;
 		public static final char SPACE = ' ';
 	}
@@ -71,8 +74,55 @@ public class LiferayPortalBrowserSnifferCompat {
 
 	protected static class Validator {
 
-		public static boolean isNotNull(String value) {
-			return (value != null);
+		public static boolean isNotNull(String s) {
+			return (!isNull(s));
+		}
+
+		public static boolean isNull(String s) {
+
+			if (s == null) {
+				return true;
+			}
+
+			int counter = 0;
+
+			for (int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+
+				if (c == CharPool.SPACE) {
+					continue;
+				}
+				else if (counter > 3) {
+					return false;
+				}
+
+				if (counter == 0) {
+
+					if (c != CharPool.LOWER_CASE_N) {
+						return false;
+					}
+				}
+				else if (counter == 1) {
+
+					if (c != CharPool.LOWER_CASE_U) {
+						return false;
+					}
+				}
+				else if ((counter == 2) || (counter == 3)) {
+
+					if (c != CharPool.LOWER_CASE_L) {
+						return false;
+					}
+				}
+
+				counter++;
+			}
+
+			if ((counter == 0) || (counter == 4)) {
+				return true;
+			}
+
+			return false;
 		}
 	}
 
