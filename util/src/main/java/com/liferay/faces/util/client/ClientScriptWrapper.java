2d816832a38b537c7d6cd8015cf7bcf235d786fa
Neil Griffin <neil.griffin.scm@gmail.com>
2014-09-24 14:31:20 -0400

    FACES-1928 Remove compile-time dependency on portal-service.jar from Liferay Faces Alloy (Added ClientScriptWrapper and moved ClientScript implementation classes to internal sub-package)

---
+++ util/src/main/java/com/liferay/faces/util/client/ClientScriptWrapper.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.client;
+
+import javax.faces.FacesWrapper;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class ClientScriptWrapper implements ClientScript, FacesWrapper<ClientScript> {
+
+	@Override
+	public void append(String portletId, String content, String use) {
+		getWrapped().append(portletId, content, use);
+	}
+
+	public abstract ClientScript getWrapped();
+}
