8ef1af9dd6d2eb7892af44618190d08e1630d06e
Neil Griffin <neil.griffin.scm@gmail.com>
2014-09-24 14:12:51 -0400

    FACES-287 Develop browserSniffer EL keyword (added BrowserSnifferFactory and moved implementation-specific classes to internal sub-package)

---
+++ util/src/main/java/com/liferay/faces/util/client/BrowserSnifferFactory.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.client;
+
+import javax.faces.FacesException;
+import javax.faces.context.ExternalContext;
+
+import com.liferay.faces.util.factory.FactoryExtension;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class BrowserSnifferFactory implements FactoryExtension<BrowserSnifferFactory> {
+
+	public abstract BrowserSniffer getBrowserSniffer(ExternalContext externalContext) throws FacesException;
+}
