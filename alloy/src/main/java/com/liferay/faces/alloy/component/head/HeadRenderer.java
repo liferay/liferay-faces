9f0e9dcc0aff0a813424c3b8d0612464eeb4b19a
Kyle Stiemann <kyle.stiemann@liferay.com>
2014-09-24 14:11:01 -0400

    FACES-1974 Develop alloy:head component (Write <meta name="viewport" content="width=device-width, initial-scale=1"></meta> as a child of the <head> tag when not running in the portal in order to enable responsive CSS for mobile devices.)

--- alloy/src/main/java/com/liferay/faces/alloy/component/head/HeadRenderer.java
+++ alloy/src/main/java/com/liferay/faces/alloy/component/head/HeadRenderer.java
@@ -13,9 +13,22 @@
  */
 package com.liferay.faces.alloy.component.head;
 
+import java.io.IOException;
+import java.util.List;
+
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 import javax.faces.render.FacesRenderer;
+
+import com.liferay.faces.util.application.ComponentResource;
+import com.liferay.faces.util.application.ComponentResourceFactory;
+import com.liferay.faces.util.factory.FactoryExtensionFinder;
+import com.liferay.faces.util.product.ProductConstants;
+import com.liferay.faces.util.product.ProductMap;
 
 
 /**
@@ -35,6 +48,43 @@ import javax.faces.render.FacesRenderer;
 //J+
 public class HeadRenderer extends HeadRendererBase {
 
+	// Private Constants
+	private static final boolean LIFERAY_PORTAL_DETECTED = ProductMap.getInstance().get(ProductConstants.LIFERAY_PORTAL)
+		.isDetected();
+
+	@Override
+	public void encodeChildren(FacesContext facesContext, UIComponent uiComponent) throws IOException {
+
+		if (!LIFERAY_PORTAL_DETECTED) {
+
+			UIViewRoot uiViewRoot = facesContext.getViewRoot();
+			List<UIComponent> componentRespources = uiViewRoot.getComponentResources(facesContext, "head");
+
+			for (UIComponent resource : componentRespources) {
+
+				ComponentResourceFactory componentResourceFactory = (ComponentResourceFactory) FactoryExtensionFinder
+					.getFactory(ComponentResourceFactory.class);
+				ComponentResource componentResource = componentResourceFactory.getComponentResource(resource);
+				String library = componentResource.getLibrary();
+				String name = componentResource.getName();
+
+				if ("build/aui-css/css/bootstrap-responsive.min.css".equals(name) &&
+						"liferay-faces-reslib".equals(library)) {
+
+					ResponseWriter responseWriter = facesContext.getResponseWriter();
+					responseWriter.startElement("meta", null);
+					responseWriter.writeAttribute("name", "viewport", null);
+					responseWriter.writeAttribute("content", "width=device-width, initial-scale=1", null);
+					responseWriter.endElement("meta");
+
+					break;
+				}
+			}
+		}
+
+		super.encodeChildren(facesContext, uiComponent);
+	}
+
 	@Override
 	public String getDelegateComponentFamily() {
 		return Head.DELEGATE_COMPONENT_FAMILY;
@@ -43,5 +93,10 @@ public class HeadRenderer extends HeadRendererBase {
 	@Override
 	public String getDelegateRendererType() {
 		return Head.DELEGATE_RENDERER_TYPE;
+	}
+
+	@Override
+	public boolean getRendersChildren() {
+		return true;
 	}
 }
