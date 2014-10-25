e6e1135a9f64d95006eca24dcc44a3578ab222a4
Neil Griffin <neil.griffin.scm@gmail.com>
2014-10-25 18:23:24 -0400

    FACES-2021 Prepare for OSGi by moving impl classes to "internal" sub-packages in order to clearly distinguish API from implementation

--- alloy/src/main/java/com/liferay/faces/alloy/event/com/liferay/faces/alloy/event/internal/ApplicationStartupListener.java
+++ alloy/src/main/java/com/liferay/faces/alloy/event/com/liferay/faces/alloy/event/internal/ApplicationStartupListener.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.alloy.event;
+package com.liferay.faces.alloy.event.com.liferay.faces.alloy.event.internal;
 
 import javax.faces.application.Application;
 import javax.faces.event.AbortProcessingException;
