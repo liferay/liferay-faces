2cef5853e003a4f534114c2863ef983a6bc9aab9
Neil Griffin <neil.griffin.scm@gmail.com>
2014-10-27 13:24:51 -0400

    FACES-2018 Develop new PostConstructApplicationConfigEvent (backport compatibility layer)

---
+++ util/src/main/java/com/liferay/faces/util/event/internal/ApplicationStartupListenerCompat.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.event.internal;
+
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
+import javax.faces.event.SystemEventListener;
+
+import com.liferay.faces.util.config.ApplicationConfig;
+import com.liferay.faces.util.event.PostConstructApplicationConfigEvent;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class ApplicationStartupListenerCompat implements SystemEventListener {
+
+	protected void publishEvent(Application application, FacesContext facesContext,
+		ApplicationConfig applicationConfig) {
+
+		application.publishEvent(facesContext, PostConstructApplicationConfigEvent.class, ApplicationConfig.class,
+			applicationConfig);
+	}
+}
