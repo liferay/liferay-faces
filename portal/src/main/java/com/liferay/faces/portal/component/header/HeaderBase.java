2f9e18e2bb2fbf30195d5c307d97a46569c2c427
Neil Griffin <neil.griffin.scm@gmail.com>
2015-01-28 19:51:41 -0500

    FACES-2039 portal:header and portal:buttonRow components and use cases

---
+++ portal/src/main/java/com/liferay/faces/portal/component/header/HeaderBase.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portal.component.header;
+//J-
+
+import javax.annotation.Generated;
+import javax.faces.component.html.HtmlPanelGroup;
+
+import com.liferay.faces.util.component.Styleable;
+
+/**
+ * @author	Neil Griffin
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public abstract class HeaderBase extends HtmlPanelGroup implements Styleable {
+
+	// Protected Enumerations
+	protected enum HeaderPropertyKeys {
+		backLabel,
+		backURL,
+		escapeXml,
+		showBackURL,
+		style,
+		styleClass,
+		title
+	}
+
+	public String getBackLabel() {
+		return (String) getStateHelper().eval(HeaderPropertyKeys.backLabel, null);
+	}
+
+	public void setBackLabel(String backLabel) {
+		getStateHelper().put(HeaderPropertyKeys.backLabel, backLabel);
+	}
+
+	public String getBackURL() {
+		return (String) getStateHelper().eval(HeaderPropertyKeys.backURL, null);
+	}
+
+	public void setBackURL(String backURL) {
+		getStateHelper().put(HeaderPropertyKeys.backURL, backURL);
+	}
+
+	public boolean isEscapeXml() {
+		return (Boolean) getStateHelper().eval(HeaderPropertyKeys.escapeXml, true);
+	}
+
+	public void setEscapeXml(boolean escapeXml) {
+		getStateHelper().put(HeaderPropertyKeys.escapeXml, escapeXml);
+	}
+
+	public boolean isShowBackURL() {
+		return (Boolean) getStateHelper().eval(HeaderPropertyKeys.showBackURL, true);
+	}
+
+	public void setShowBackURL(boolean showBackURL) {
+		getStateHelper().put(HeaderPropertyKeys.showBackURL, showBackURL);
+	}
+
+	@Override
+	public String getStyle() {
+		return (String) getStateHelper().eval(HeaderPropertyKeys.style, null);
+	}
+
+	@Override
+	public void setStyle(String style) {
+		getStateHelper().put(HeaderPropertyKeys.style, style);
+	}
+
+	@Override
+	public String getStyleClass() {
+		return (String) getStateHelper().eval(HeaderPropertyKeys.styleClass, null);
+	}
+
+	@Override
+	public void setStyleClass(String styleClass) {
+		getStateHelper().put(HeaderPropertyKeys.styleClass, styleClass);
+	}
+
+	public String getTitle() {
+		return (String) getStateHelper().eval(HeaderPropertyKeys.title, null);
+	}
+
+	public void setTitle(String title) {
+		getStateHelper().put(HeaderPropertyKeys.title, title);
+	}
+}
+//J+
---
+++ portal/src/main/java/com/liferay/faces/portal/component/header/internal/HeaderRendererBase.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portal.component.header.internal;
+//J-
+
+
+import com.liferay.faces.portal.component.header.Header;
+import com.liferay.faces.portal.render.internal.PortalTagRenderer;
+import com.liferay.taglib.ui.HeaderTag;
+
+import javax.annotation.Generated;
+
+/**
+ * @author	Neil Griffin
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public abstract class HeaderRendererBase extends PortalTagRenderer<Header, HeaderTag> {
+
+	// Protected Constants
+	protected static final String BACK_LABEL = "backLabel";
+	protected static final String BACK_URL = "backURL";
+	protected static final String CSS_CLASS = "cssClass";
+	protected static final String ESCAPE_XML = "escapeXml";
+	protected static final String LOCALIZE_TITLE = "localizeTitle";
+	protected static final String SHOW_BACK_URL = "showBackURL";
+	protected static final String STYLE = "style";
+	protected static final String STYLE_CLASS = "styleClass";
+	protected static final String TITLE = "title";
+}
+//J+
