306c60a8ece838714fcc20b54f9a0fbdf7f27d8d
Neil Griffin <neil.griffin.scm@gmail.com>
2015-02-10 16:10:34 -0500

    FACES-2021 Prepare for OSGi by moving impl classes to internal sub-packages in order to clearly distinguish API from implementation (remove bridge-api dependency on liferay-faces-util and streamine factory APIs)

--- bridge-api/src/main/java/com/liferay/faces/bridge/scope/BridgeRequestScopeFactory.java
+++ bridge-api/src/main/java/com/liferay/faces/bridge/scope/BridgeRequestScopeFactory.java
@@ -16,7 +16,6 @@ package com.liferay.faces.bridge.scope;
 import javax.faces.FacesException;
 import javax.faces.FacesWrapper;
 import javax.portlet.PortletConfig;
-import javax.portlet.PortletContext;
 import javax.portlet.PortletRequest;
 
 import com.liferay.faces.bridge.config.BridgeConfig;
