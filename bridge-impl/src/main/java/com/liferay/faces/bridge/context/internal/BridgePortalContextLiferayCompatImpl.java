b73203efb52567c76ddf4644bfbc0fa8a22cf2cb
Neil Griffin <neil.griffin.scm@gmail.com>
2015-02-11 17:42:24 -0500

    FACES-2118 Develop new BridgePortalContext interface

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/internal/BridgePortalContextLiferayCompatImpl.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.context.internal;
+
+import javax.portlet.PortalContext;
+import javax.portlet.PortletRequest;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class BridgePortalContextLiferayCompatImpl extends BridgePortalContextImpl {
+
+	public BridgePortalContextLiferayCompatImpl(PortalContext portalContext) {
+		super(portalContext);
+	}
+
+	protected boolean isLiferayNamingspacingParameters(PortletRequest portletRequest) {
+
+		// Versions of Liferay Portal prior to 6.2 do not support strict namespacing of parameters.
+		return false;
+	}
+}
