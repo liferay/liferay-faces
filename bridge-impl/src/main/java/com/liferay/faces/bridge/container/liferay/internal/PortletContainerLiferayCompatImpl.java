c346ed4b464371780417efc6ff6a79a9eca1fa72
Neil Griffin <neil.griffin.scm@gmail.com>
2015-02-11 16:14:35 -0500

    FACES-2116 Remove "extends PhaseListener" from PortletContainer interface

--- bridge-impl/src/main/java/com/liferay/faces/bridge/container/liferay/internal/PortletContainerLiferayCompatImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/container/liferay/internal/PortletContainerLiferayCompatImpl.java
@@ -13,8 +13,6 @@
  */
 package com.liferay.faces.bridge.container.liferay.internal;
 
-import javax.faces.event.PhaseEvent;
-import javax.faces.event.PhaseId;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletRequest;
 
@@ -36,34 +34,11 @@ import com.liferay.portal.theme.ThemeDisplay;
  */
 public class PortletContainerLiferayCompatImpl extends PortletContainerImpl {
 
-	// serialVersionUID
-	private static final long serialVersionUID = 8713570232856573935L;
-
 	// Logger
 	private static final Logger logger = LoggerFactory.getLogger(PortletContainerLiferayCompatImpl.class);
 
 	public PortletContainerLiferayCompatImpl(PortletRequest portletRequest, PortletConfig portletConfig) {
 		super(portletRequest, portletConfig);
-	}
-
-	@Override
-	public void afterPhase(PhaseEvent phaseEvent) {
-		// no-op for JSF 1.2
-	}
-
-	/**
-	 * This method is called prior to the {@link PhaseId#RENDER_RESPONSE} phase of the JSF lifecycle. It's purpose is to
-	 * determine if there are any resources in the LIFERAY_SHARED_PAGE_TOP request attribute, so that execution of the
-	 * {@link #afterPhase(PhaseEvent)} can be optimized.
-	 */
-	@Override
-	public void beforePhase(PhaseEvent phaseEvent) {
-		// no-op for JSF 1.2
-	}
-
-	@Override
-	public PhaseId getPhaseId() {
-		return PhaseId.RENDER_RESPONSE;
 	}
 
 	protected boolean isPortletRequiresNamespacedParameters(PortletRequest portletRequest, ThemeDisplay themeDisplay) {
