9f0d34b4f7918ca0430ef2a8dfc4d1184388ff70
Vernon <vsingleton@gmail.com>
2015-02-13 13:25:20 -0500

    added tester for icefaces4

--- test/integration/demos/bridge/gen.pl
+++ test/integration/demos/bridge/gen.pl
@@ -55,11 +55,10 @@ my %in = (
    Jsf2CdiPortletTest => "./jsf2-cdi-portlet/src/test/java/com/liferay/faces/test/Jsf2Cdi.java",
    Jsf2Html5PortletTest => "./jsf2-html5-portlet/src/test/java/com/liferay/faces/test/Jsf2Html5.java",
    Jsf2SpringPortletTest => "./jsf2-spring-portlet/src/test/java/com/liferay/faces/test/Jsf2Spring.java",
-   Icefaces3PortletTest => "./icefaces3-portlet/src/test/java/com/liferay/faces/test/Icefaces3.java",
+   Icefaces4PortletTest => "./icefaces4-portlet/src/test/java/com/liferay/faces/test/Icefaces4.java",
    Richfaces4PortletTest => "./richfaces4-portlet/src/test/java/com/liferay/faces/test/Richfaces4.java",
    Primefaces4PortletTest => "./primefaces4-portlet/src/test/java/com/liferay/faces/test/Primefaces4.java",
    Primefaces5PortletTest => "./primefaces5-portlet/src/test/java/com/liferay/faces/test/Primefaces5.java",
-   Icefaces3CompatPortletTest => "./icefaces3-compat-portlet/src/test/java/com/liferay/faces/test/Icefaces3Compat.java",
 );
 my %variables = ();
 my %xpaths = ();
