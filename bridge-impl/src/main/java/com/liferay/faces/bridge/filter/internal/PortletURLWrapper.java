de3d49effeb838924d104187ebf991213f57162f
Neil Griffin <neil.griffin.scm@gmail.com>
2015-02-27 16:55:16 -0500

    FACES-2146 Move the creation of ActionURLs, RenderURLs, and ResourceURLs from PortletContainer to MimeResponse

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/filter/internal/PortletURLWrapper.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.bridge.filter.internal;
+
+import javax.portlet.PortletMode;
+import javax.portlet.PortletModeException;
+import javax.portlet.PortletURL;
+import javax.portlet.WindowState;
+import javax.portlet.WindowStateException;
+
+import com.liferay.faces.util.helper.Wrapper;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class PortletURLWrapper extends BaseURLWrapper implements PortletURL, Wrapper<PortletURL> {
+
+	public void removePublicRenderParameter(String name) {
+		getWrapped().removePublicRenderParameter(name);
+	}
+
+	public PortletMode getPortletMode() {
+		return getWrapped().getPortletMode();
+	}
+
+	public void setPortletMode(PortletMode portletMode) throws PortletModeException {
+		getWrapped().setPortletMode(portletMode);
+	}
+
+	public WindowState getWindowState() {
+		return getWrapped().getWindowState();
+	}
+
+	public void setWindowState(WindowState windowState) throws WindowStateException {
+		getWrapped().setWindowState(windowState);
+	}
+
+	@Override
+	public abstract PortletURL getWrapped();
+
+}
