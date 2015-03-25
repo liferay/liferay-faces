fe45a85bfb57ebc68be288ec891eacb4ec7f65ba
Neil Griffin <neil.griffin.scm@gmail.com>
2015-03-25 12:53:48 -0400

    FACES-2180 Liferay Faces Bridge is unable to add <meta> tags to the <head>...</head> section of the portal page

--- bridge-impl/src/main/java/com/liferay/faces/bridge/renderkit/html_basic/internal/ElementWriterStack.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/renderkit/html_basic/internal/ElementWriterStack.java
@@ -51,9 +51,21 @@ public class ElementWriterStack extends Stack<ElementWriter> {
 			Element peekedElement = peek().getElement();
 
 			if (peekedElement instanceof ElementBlankImpl) {
+
 				StringBuilder prependedTextContent = new StringBuilder();
-				prependedTextContent.append(peekedElement.getTextContent());
-				prependedTextContent.append(topElement.getTextContent());
+
+				String peekedTextContent = peekedElement.getTextContent();
+
+				if (peekedTextContent != null) {
+					prependedTextContent.append(peekedTextContent);
+				}
+
+				String topTextContent = topElement.getTextContent();
+
+				if (topTextContent != null) {
+					prependedTextContent.append(topTextContent);
+				}
+
 				topElement.setTextContent(prependedTextContent.toString());
 				super.pop();
 				done = isEmpty();
