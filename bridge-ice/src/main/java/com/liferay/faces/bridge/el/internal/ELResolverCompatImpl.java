da0a52ec7cc02829c4746e8c8e8c51dc15b8825e
Neil Griffin <neil.griffin.scm@gmail.com>
2015-05-08 15:34:53 -0400

    FACES-2205 Icefaces1 portlets fail to deploy

--- bridge-ice/src/main/java/com/liferay/faces/bridge/el/internal/ELResolverCompatImpl.java
+++ bridge-ice/src/main/java/com/liferay/faces/bridge/el/internal/ELResolverCompatImpl.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.bridge.el;
+package com.liferay.faces.bridge.el.internal;
 
 import javax.el.ELResolver;
 import javax.faces.context.FacesContext;
--- bridge-ice/src/main/java/com/liferay/faces/bridge/el/internal/ELResolverImpl.java
+++ bridge-ice/src/main/java/com/liferay/faces/bridge/el/internal/ELResolverImpl.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.bridge.el;
+package com.liferay.faces.bridge.el.internal;
 
 import java.beans.FeatureDescriptor;
 import java.util.ArrayList;
