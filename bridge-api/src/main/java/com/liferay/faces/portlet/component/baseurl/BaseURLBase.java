629042b0ba5a26673e12e10d23d37d6a46c42a04
Kyle Stiemann <kyle.stiemann@liferay.com>
2015-05-19 15:07:33 -0400

    FACES-2253 Refactor portlet namespaced components to comply with portlet 2.0 spec and allow them to be generated by alloy-generator

---
+++ bridge-api/src/main/java/com/liferay/faces/portlet/component/baseurl/BaseURLBase.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portlet.component.baseurl;
+//J-
+
+import javax.annotation.Generated;
+import javax.faces.component.UIComponentBase;
+
+
+/**
+ * @author	Neil Griffin
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public abstract class BaseURLBase extends UIComponentBase {
+
+	// Public Constants
+	public static final String COMPONENT_TYPE = "com.liferay.faces.portlet.component.baseurl.BaseURL";
+	public static final String RENDERER_TYPE = "com.liferay.faces.portlet.component.baseurl.internal.BaseURLRenderer";
+
+	// Protected Enumerations
+	protected enum BaseURLPropertyKeys {
+		escapeXml,
+		secure,
+		var
+	}
+
+	public BaseURLBase() {
+		super();
+		setRendererType(RENDERER_TYPE);
+	}
+
+	public boolean isEscapeXml() {
+		return (Boolean) getStateHelper().eval(BaseURLPropertyKeys.escapeXml, true);
+	}
+
+	public void setEscapeXml(boolean escapeXml) {
+		getStateHelper().put(BaseURLPropertyKeys.escapeXml, escapeXml);
+	}
+
+	public Boolean getSecure() {
+		return (Boolean) getStateHelper().eval(BaseURLPropertyKeys.secure, null);
+	}
+
+	public void setSecure(Boolean secure) {
+		getStateHelper().put(BaseURLPropertyKeys.secure, secure);
+	}
+
+	public String getVar() {
+		return (String) getStateHelper().eval(BaseURLPropertyKeys.var, null);
+	}
+
+	public void setVar(String var) {
+		getStateHelper().put(BaseURLPropertyKeys.var, var);
+	}
+}
+//J+
---
+++ bridge-impl/src/main/java/com/liferay/faces/portlet/component/namespace/internal/NamespaceRenderer.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portlet.component.namespace.internal;
+
+import javax.faces.render.FacesRenderer;
+
+import com.liferay.faces.portlet.component.namespace.Namespace;
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+
+/**
+ * @author	Neil Griffin
+ */
+//J-
+@FacesRenderer(componentFamily = Namespace.COMPONENT_FAMILY, rendererType = Namespace.RENDERER_TYPE)
+//J+
+public class NamespaceRenderer extends NamespaceRendererBase {
+
+	@Override
+	public boolean getRendersChildren() {
+		return true;
+	}
+
+	@Override
+	public void encodeEnd(FacesContext facesContext, UIComponent uiComponent) throws IOException {
+
+		String namespace = facesContext.getExternalContext().encodeNamespace("");
+		Namespace namespaceComponent = (Namespace) uiComponent;
+		String var = namespaceComponent.getVar();
+
+		if (var == null) {
+
+			ResponseWriter responseWriter = facesContext.getResponseWriter();
+			responseWriter.write(namespace);
+		}
+		else {
+
+			ExternalContext externalContext = facesContext.getExternalContext();
+			externalContext.getRequestMap().put(var, namespace);
+		}
+	}
+
+	@Override
+	public void encodeChildren(FacesContext facesContext, UIComponent uiComponent) throws IOException {
+		// no-op
+	}
+
+	@Override
+	public void encodeBegin(FacesContext facesContext, UIComponent uiComponent) throws IOException {
+		// no-op
+	}
+}
