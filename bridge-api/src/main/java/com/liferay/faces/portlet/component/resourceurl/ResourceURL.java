e5ac4ce7a72f4a570efe1054932056e9499ec261
Neil Griffin <neil.griffin.scm@gmail.com>
2015-05-27 18:13:24 -0400

    FACES-2253 Refactor portlet namespaced components to comply with portlet 2.0 spec and allow them to be generated by alloy-generator

---
+++ bridge-api/src/main/java/com/liferay/faces/portlet/component/resourceurl/ResourceURL.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portlet.component.resourceurl;
+
+import javax.faces.component.FacesComponent;
+
+/**
+ * @author	Neil Griffin
+ */
+@FacesComponent(value = ResourceURL.COMPONENT_TYPE)
+public class ResourceURL extends ResourceURLBase {
+	// Initial Generation
+}
---
+++ bridge-impl/src/main/java/com/liferay/faces/portlet/component/renderurl/internal/RenderURLRendererBase.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.portlet.component.renderurl.internal;
+//J-
+
+
+import javax.annotation.Generated;
+
+import com.liferay.faces.portlet.component.baseurl.internal.BaseURLRenderer;
+
+
+/**
+ * @author	Neil Griffin
+ */
+@Generated(value = "com.liferay.alloy.tools.builder.FacesBuilder")
+public abstract class RenderURLRendererBase extends BaseURLRenderer {
+
+	// Protected Constants
+	protected static final String COPY_CURRENT_RENDER_PARAMETERS = "copyCurrentRenderParameters";
+	protected static final String PORTLET_MODE = "portletMode";
+	protected static final String WINDOW_STATE = "windowState";
+}
+//J+
