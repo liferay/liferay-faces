a3aaca6a3cd2bf4b7ccbad89c161b8d33af6da59
Kyle Stiemann <kyle.stiemann@liferay.com>
2015-06-15 16:55:29 -0400

    FACES-2299 Remove the dependency that liferay-faces-util.jar has on portal-service.jar (In the 3.0.x-legacy branch, moved StringBundler to bridge-impl and deleted PropsKeys and PropsUtil)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/event/internal/liferay/StringBundler.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/event/internal/liferay/StringBundler.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.util.portal;
+package com.liferay.faces.bridge.event.internal.liferay;
 
 import java.lang.reflect.Method;
 
