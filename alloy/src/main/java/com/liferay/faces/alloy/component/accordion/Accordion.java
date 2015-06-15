05d1af2b5202b427e3fc628b661ae264f654a427
pcwhite <philip.white@liferay.com>
2015-06-15 10:41:53 -0400

    FACES-2308 Delete com.liferay.faces.util.lang.FacesConstants

--- alloy/src/main/java/com/liferay/faces/alloy/component/accordion/Accordion.java
+++ alloy/src/main/java/com/liferay/faces/alloy/component/accordion/Accordion.java
@@ -33,8 +33,8 @@ import com.liferay.faces.alloy.component.tab.TabCollapseEvent;
 import com.liferay.faces.alloy.component.tab.TabExpandEvent;
 import com.liferay.faces.alloy.component.tab.TabUtil;
 import com.liferay.faces.util.helper.IntegerHelper;
-import com.liferay.faces.util.lang.FacesConstants;
 import com.liferay.faces.util.lang.StringPool;
+import com.liferay.faces.util.render.RendererUtil;
 
 
 /**
@@ -99,7 +99,7 @@ public class Accordion extends AccordionBase implements ClientBehaviorHolder {
 			// Queue an accordion tab event rather than the specified faces event.
 			AjaxBehaviorEvent behaviorEvent = (AjaxBehaviorEvent) facesEvent;
 			Behavior behavior = behaviorEvent.getBehavior();
-			String eventName = requestParameterMap.get(FacesConstants.JAVAX_FACES_BEHAVIOR_EVENT);
+			String eventName = requestParameterMap.get(RendererUtil.JAVAX_FACES_BEHAVIOR_EVENT);
 
 			if (TabCollapseEvent.TAB_COLLAPSE.equals(eventName)) {
 				TabCollapseEvent tabCollapseEvent = new TabCollapseEvent(this, behavior, tab, rowData);
