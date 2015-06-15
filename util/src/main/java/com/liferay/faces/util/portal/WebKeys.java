cd7e8a10125d57a904307222cf5bc1711592bf74
Neil Griffin <neil.griffin.scm@gmail.com>
2015-06-15 15:52:20 -0400

    FACES-2304 Delete com.liferay.faces.util.portal.WebKeys from liferay-faces-util.jar

---
+++ util/src/main/java/com/liferay/faces/util/portal/WebKeys.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.util.portal;
+
+/**
+ * This interface provides a means of backporting Liferay Portal 6.1.x constants to branches of Liferay Faces that are
+ * designed to be used with older versions of the portal.
+ *
+ * @author  Neil Griffin
+ */
+@Deprecated
+public interface WebKeys {
+
+	/**
+	 * @deprecated  Replaced by {@link com.liferay.portal.kernel.util.WebKeys#AUI_SCRIPT_DATA}.
+	 */
+	@Deprecated
+	public static final String AUI_SCRIPT_DATA = "LIFERAY_SHARED_AUI_SCRIPT_DATA";
+
+	/**
+	 * @deprecated  Replaced by {@link com.liferay.portal.kernel.util.WebKeys#RENDER_PORTLET}.
+	 */
+	@Deprecated
+	public static final String RENDER_PORTLET = "RENDER_PORTLET";
+
+	/**
+	 * @deprecated  Replaced by {@link com.liferay.portal.kernel.util.WebKeys#THEME_DISPLAY}.
+	 */
+	@Deprecated
+	public static final String THEME_DISPLAY = "LIFERAY_SHARED_THEME_DISPLAY";
+}
