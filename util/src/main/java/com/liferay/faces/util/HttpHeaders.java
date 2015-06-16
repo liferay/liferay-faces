01d179877f9396438dad3a161d582e534e0e0eb6
Neil Griffin <neil.griffin.scm@gmail.com>
2015-06-16 17:00:40 -0400

    FACES-2320 Move the com.liferay.faces.util.render.HttpHeaders interface to package com.liferay.faces.util

--- util/src/main/java/com/liferay/faces/util/HttpHeaders.java
+++ util/src/main/java/com/liferay/faces/util/HttpHeaders.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.util.render;
+package com.liferay.faces.util;
 
 /**
  * @author  Brian Wing Shun Chan
