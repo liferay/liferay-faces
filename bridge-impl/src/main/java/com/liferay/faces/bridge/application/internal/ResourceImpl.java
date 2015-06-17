cfa93987598fcc35896066856e8598dab1cc06a6
Neil Griffin <neil.griffin.scm@gmail.com>
2015-06-17 09:57:55 -0400

    FACES-2323 Remove the com.liferay.faces.util.application.ResourceConstants class in the master branch and deprecate in older branches

--- bridge-impl/src/main/java/com/liferay/faces/bridge/application/internal/ResourceImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/application/internal/ResourceImpl.java
@@ -35,7 +35,6 @@ import com.liferay.faces.bridge.config.BridgeConfig;
 import com.liferay.faces.bridge.config.internal.BridgeConfigAttributeMap;
 import com.liferay.faces.bridge.context.BridgeContext;
 import com.liferay.faces.util.HttpHeaders;
-import com.liferay.faces.util.application.ResourceConstants;
 import com.liferay.faces.util.config.ConfiguredServletMapping;
 import com.liferay.faces.util.lang.StringPool;
 import com.liferay.faces.util.logging.Logger;
@@ -282,7 +281,7 @@ public class ResourceImpl extends ResourceWrapper implements Serializable {
 							// Note: Both Mojarra and MyFaces construct a requestPath that looks something like
 							// "/javax.faces.resource/jsf.js.faces?ln=javax.faces" and so we look for the ".faces?ln" as
 							// an indicator that ".faces" needs to be removed from the requestPath.
-							String token = extension + StringPool.QUESTION + ResourceConstants.LN;
+							String token = extension + StringPool.QUESTION + StringPool.LN;
 							int pos = wrappedRequestPath.indexOf(token);
 
 							// If the servlet-mapping extension is found, then remove it since this is an implicit
