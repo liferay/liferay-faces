82e0005d72381adb1a7223965294e69a3d6b5262
Neil Griffin <neil.griffin.scm@gmail.com>
2015-07-10 15:27:33 -0400

    FACES-2138 Complete showcase with f, jstl and ui demo examples

--- demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/CommandBackingBean.java
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/CommandBackingBean.java
@@ -69,7 +69,7 @@ public class CommandBackingBean {
 		PhaseId phaseId = facesContext.getCurrentPhaseId();
 		logger.debug("ajaxListener: phaseId=[{0}]", phaseId.toString());
 
-		String phaseName = phaseId.getName();
+		String phaseName = phaseId.toString();
 		FacesMessage facesMessage = new FacesMessage("The ajaxListener method was called during the " + phaseName +
 				" phase of the JSF lifecycle.");
 		facesContext.addMessage(null, facesMessage);
@@ -83,7 +83,7 @@ public class CommandBackingBean {
 
 		String value = (String) actionEvent.getComponent().getAttributes().get("attribute");
 
-		String phaseName = phaseId.getName();
+		String phaseName = phaseId.toString();
 		FacesMessage facesMessage = new FacesMessage("The actionListener method was called during the " + phaseName +
 				" phase of the JSF lifecycle.The attribute value is " + value);
 		facesContext.addMessage(null, facesMessage);
@@ -99,7 +99,7 @@ public class CommandBackingBean {
 		String value1 = (String) attributes.get("attribute1");
 		String value2 = (String) attributes.get("attribute2");
 
-		String phaseName = phaseId.getName();
+		String phaseName = phaseId.toString();
 		FacesMessage facesMessage = new FacesMessage("The actionListener method was called during the " + phaseName +
 				" phase of the JSF lifecycle.The attributes value are " + value1 + " and " + value2);
 		facesContext.addMessage(null, facesMessage);
@@ -134,7 +134,7 @@ public class CommandBackingBean {
 		Map<String, String> requestParameterMap = facesContext.getExternalContext().getRequestParameterMap();
 		String value = requestParameterMap.get("parameter");
 
-		String phaseName = phaseId.getName();
+		String phaseName = phaseId.toString();
 		FacesMessage facesMessage = new FacesMessage("The actionListener method was called during the " + phaseName +
 				" phase of the JSF lifecycle.The parameter value is " + value);
 		facesContext.addMessage(null, facesMessage);
