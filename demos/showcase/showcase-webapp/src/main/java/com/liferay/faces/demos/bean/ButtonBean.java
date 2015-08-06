6372ea090f683d8746c588b3e8828f40f97c2277
Neil Griffin <neil.griffin.scm@gmail.com>
2015-08-06 11:16:23 -0400

    FACES-2404 fix for the bottom edge

---
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/ButtonBean.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.demos.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@ManagedBean
+@RequestScoped
+public class ButtonBean {
+
+	private String menuItem;
+
+	public String getMenuItem() {
+		return menuItem;
+	}
+
+	public void setMenuItem(String menuItem) {
+		this.menuItem = menuItem;
+	}
+}
